3f96ec4cb104d0d798e96e60cc02715a1af8df69
Wellsen <gengshengzhang@gmail.com>
2015-07-14 23:17:51 +0800

    CMS Buildit file for Linux

--- CommonUtilitiesLib/nbproject/Makefile-variables.mk
+++ CommonUtilitiesLib/nbproject/Makefile-variables.mk
@@ -8,17 +8,17 @@ CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=../Debug
+CND_ARTIFACT_DIR_Debug=${AVS_ROOT}/lib
 CND_ARTIFACT_NAME_Debug=libCommonUtilitiesLib.a
-CND_ARTIFACT_PATH_Debug=../Debug/libCommonUtilitiesLib.a
+CND_ARTIFACT_PATH_Debug=${AVS_ROOT}/lib/libCommonUtilitiesLib.a
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=CommonUtilitiesLib.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/CommonUtilitiesLib.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=../Release
+CND_ARTIFACT_DIR_Release=Release
 CND_ARTIFACT_NAME_Release=libCommonUtilitiesLib.a
-CND_ARTIFACT_PATH_Release=../Release/libCommonUtilitiesLib.a
+CND_ARTIFACT_PATH_Release=Release/libCommonUtilitiesLib.a
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=CommonUtilitiesLib.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/CommonUtilitiesLib.tar
