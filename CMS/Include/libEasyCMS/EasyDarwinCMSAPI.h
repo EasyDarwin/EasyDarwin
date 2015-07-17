d2dc32a6502012ec0896624a71a736a1e240cea9
Babosa <mysunpany@gmail.com>
2015-07-17 09:52:42 +0800

    libEasyCMS Inlcude

---
+++ CMS/Include/libEasyCMS/EasyDarwinCMSAPI.h
@@ -0,0 +1,54 @@
+/*
+	Copyright (c) 2013-2015 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.easydarwin.org
+*/
+#ifndef _EasyDarwin_CMS_INTERFACE_H
+#define _EasyDarwin_CMS_INTERFACE_H
+#include "libClientCommondef.h"
+
+#if (defined(_WIN32))
+#ifndef _DLL_
+#define EASYDARWIN_CMS_API
+#else
+#ifdef EASYDARWIN_CMS_API_EXPORTS
+#define EASYDARWIN_CMS_API __declspec(dllexport) 
+#else  
+#define EASYDARWIN_CMS_API __declspec(dllimport)
+#endif
+#endif
+#elif defined(__linux__)
+#define EASYDARWIN_CMS_API
+#endif
+
+namespace EasyDarwin { namespace libEasyCMS
+{
+
+enum EASYDARWIN_EVENT_TYPE
+{
+	EASYDARWIN_EVENT_LOGIN,
+	EASYDARWIN_EVENT_OFFLINE
+};
+
+typedef void (*fEventCallBack)(EASYDARWIN_EVENT_TYPE eEvent, const char* pEventData, unsigned int iDataLen, void* pUserData);
+
+class EASYDARWIN_CMS_API EasyDarwinCMSAPI
+{
+public:
+	EasyDarwinCMSAPI();
+	~EasyDarwinCMSAPI(void);
+
+private:
+	void *m_callCenter;
+
+public:
+	//设置事件回调
+	void SetEventCallBack(fEventCallBack fCallBack, void *pUserData);
+	//登录
+	Easy_Error  Login(const char *szHost, int nPort, const char *szAccess, const char *szPassword);
+};
+
+}
+}
+#endif
