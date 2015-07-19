c5d22107577bce6d5262a0bc4661986dd72294e7
Babosa <mysunpany@gmail.com>
2015-07-19 18:36:01 +0800

    Add EasyDSSProtocol source code

---
+++ CMS/EasyDSSProtocol/EasyDSSProtocol/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=${AVS_ROOT}/lib
+CND_ARTIFACT_NAME_Debug=libEasyDSSProtocol.a
+CND_ARTIFACT_PATH_Debug=${AVS_ROOT}/lib/libEasyDSSProtocol.a
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=EasyDSSProtocol.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/EasyDSSProtocol.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=Release
+CND_ARTIFACT_NAME_Release=libEasyDSSProtocol.a
+CND_ARTIFACT_PATH_Release=Release/libEasyDSSProtocol.a
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=EasyDSSProtocol.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/EasyDSSProtocol.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
