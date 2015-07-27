75c7f777a0ebf78ba5a0a2806dc1dee11e5cf16f
Babosa <mysunpany@gmail.com>
2015-07-27 15:53:45 +0800

    revision v7.0.1 Build/15.0727

--- Include/revision.h
+++ Include/revision.h
@@ -29,8 +29,8 @@
 // Use no http/rtsp tspecial chars in kVersionString and kBuildString defines
 // tspecials =  ()<>@,;:\/"[]?=
 
-#define kVersionString "7.0.0"
-#define kBuildString "15.0310"
+#define kVersionString "7.0.1"
+#define kBuildString "15.0727"
 
 // Use kCommentString for seed or other release info 
 // Do not use '(' or ')' in the kCommentString
