a7810bc83f01ffc95ebd0ce6d10772ec66380a2e
Babosa <mysunpany@gmail.com>
2015-08-06 16:38:19 +0800

    EasyRelaySession加入NVSource

--- EasyDarwin/APIModules/EasyRelayModule/EasyRelaySession.cpp
+++ EasyDarwin/APIModules/EasyRelayModule/EasyRelaySession.cpp
@@ -11,15 +11,34 @@
 #include "EasyRelaySession.h"
 #include <stdlib.h>
 
+/* NVSource从RTSPClient获取数据后回调给上层 */
+int CALLBACK __NVSourceCallBack( int _chid, int *_chPtr, int _mediatype, char *pbuf, NVS_FRAME_INFO *frameinfo)
+{
+	EasyRelaySession* pRelaySession = (EasyRelaySession*)_chPtr;
+
+	if (NULL == pRelaySession)	return -1;
+
+	if (NULL != frameinfo)
+	{
+		if (frameinfo->height==1088)		frameinfo->height=1080;
+		else if (frameinfo->height==544)	frameinfo->height=540;
+	}
+
+	//投递到具体的EasyHLSSession进行处理
+	pRelaySession->ProcessData(_chid, _mediatype, pbuf, frameinfo);
+
+	return 0;
+}
+
 EasyRelaySession::EasyRelaySession(char* inURL, ClientType inClientType, const char* streamName)
-:   fReflectorSession(NULL),
-	fStreamName(NULL),
-	fURL(NULL)
+:   fStreamName(NULL),
+	fURL(NULL),
+	fNVSHandle(NULL),
+	fPusherHandle(NULL)
 {
     this->SetTaskName("EasyRelaySession");
 
 	//建立NVSourceClient
-
     StrPtrLen theURL(inURL);
 
 	fURL = NEW char[::strlen(inURL) + 2];
@@ -59,8 +78,101 @@ SInt64 EasyRelaySession::Run()
 }
 
 
-QTSS_Error EasyRelaySession::SendDescribe()
+QTSS_Error EasyRelaySession::ProcessData(int _chid, int mediatype, char *pbuf, NVS_FRAME_INFO *frameinfo)
 {
-	QTSS_Error theErr = QTSS_NoErr;
-	return theErr;
+	//if(NULL == fHLSHandle) return QTSS_Unimplemented;
+	//if (mediatype == MEDIA_TYPE_VIDEO)
+	//{
+	//	printf("Get %s Video Len:%d tm:%d rtp:%d\n",frameinfo->type==FRAMETYPE_I?"I":"P", frameinfo->length, frameinfo->timestamp_sec, frameinfo->rtptimestamp);
+	//	
+	//	fwrite(pbuf, 1, frameinfo->length, fTest);
+
+	//	if(frameinfo->fps == 0) frameinfo->fps = 25;
+	//	tsTimeStampMSsec += 1000/frameinfo->fps;
+	//	unsigned long long llPts = tsTimeStampMSsec * 90;
+	//
+	//	unsigned int uiFrameType = 0;
+	//	if (frameinfo->type == FRAMETYPE_I)
+	//	{
+	//		uiFrameType = TS_TYPE_PES_VIDEO_I_FRAME;
+	//	}
+	//	else if (frameinfo->type == FRAMETYPE_P)
+	//	{
+	//		uiFrameType = TS_TYPE_PES_VIDEO_P_FRAME;
+	//	}
+	//	else
+	//	{
+	//		return QTSS_OutOfState;
+	//	}
+
+	//	EasyHLS_VideoMux(fHLSHandle, uiFrameType, (unsigned char*)pbuf, frameinfo->length, llPts, llPts, llPts);
+	//}
+	//else if (mediatype == MEDIA_TYPE_AUDIO)
+	//{
+	//	printf("Get Audio Len:%d tm:%d rtp:%d\n", frameinfo->length, frameinfo->timestamp_sec, frameinfo->rtptimestamp);
+	//	// 暂时不对音频进行处理
+	//}
+	//else if (mediatype == MEDIA_TYPE_EVENT)
+	//{
+	//	if (NULL == pbuf && NULL == frameinfo)
+	//	{
+	//		printf("Connecting:%s ...\n", fHLSSessionID.Ptr);
+	//	}
+	//	else if (NULL!=frameinfo && frameinfo->type==0xF1)
+	//	{
+	//		printf("Lose Packet:%s ...\n", fHLSSessionID.Ptr);
+	//	}
+	//}
+
+	return QTSS_NoErr;
+}
+
+/*
+	创建HLS直播Session
+*/
+QTSS_Error	EasyRelaySession::HLSSessionStart(char* rtspUrl)
+{
+	if(NULL == fNVSHandle)
+	{
+		//创建NVSource
+		EasyNVS_Init(&fNVSHandle);
+
+		if (NULL == fNVSHandle) return QTSS_Unimplemented;
+
+		unsigned int mediaType = MEDIA_TYPE_VIDEO;
+		//mediaType |= MEDIA_TYPE_AUDIO;	//换为NVSource, 屏蔽声音
+
+		EasyNVS_SetCallback(fNVSHandle, __NVSourceCallBack);
+		EasyNVS_OpenStream(fNVSHandle, 0, rtspUrl,RTP_OVER_TCP, mediaType, 0, 0, this, 1000, 0);
+	}
+
+	if(NULL == fPusherHandle)
+	{
+		;
+
+	}
+
+	return QTSS_NoErr;
+}
+
+QTSS_Error	EasyRelaySession::HLSSessionRelease()
+{
+	qtss_printf("HLSSession Release....\n");
+	
+	//释放source
+	if(fNVSHandle)
+	{
+		EasyNVS_CloseStream(fNVSHandle);
+		EasyNVS_Deinit(&fNVSHandle);
+		fNVSHandle = NULL;
+	}
+
+	//释放sink
+	if(fPusherHandle)
+	{
+		//EasyHLS_Session_Release(fHLSHandle);
+		//fHLSHandle = NULL;
+	}
+
+	return QTSS_NoErr;
 }
