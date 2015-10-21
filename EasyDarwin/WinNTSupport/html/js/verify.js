f790d993fb022452bf958a26417a760ff84ff5f2
arnowang <272706196@qq.com>
2015-10-21 11:06:29 +0800

    完成基本接口配置和语言国际化功能

--- EasyDarwin/WinNTSupport/html/js/verify.js
+++ EasyDarwin/WinNTSupport/html/js/verify.js
@@ -1,10 +1,20 @@
-﻿function isPort(str)  
+﻿function isPort(str,warnstr)  
 {  
     var parten=/^(\d)+$/g;  
     if(parten.test(str)&&parseInt(str)<=65535&&parseInt(str)>=0){  
         return true;  
      }else{  
-		alert("端口号格式不正确");
+		alert(warnstr);
+        return false;  
+     }   
+}  
+function istime(str,warnstr)  
+{  
+    var parten=/^(\d)+$/g;  
+    if(parten.test(str)&&parseInt(str)>=0){  
+        return true;  
+     }else{  
+		alert(warnstr);
         return false;  
      }   
 }  
