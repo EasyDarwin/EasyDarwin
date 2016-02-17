9a841eebe2e9d3412b9ce910be50ce241495215e
fantasyARM <fantasyARM@users.noreply.github.com>
2016-02-17 23:15:53 +0800

    common.h copyright

--- CommonUtilitiesLib/common.h
+++ CommonUtilitiesLib/common.h
@@ -1,4 +1,10 @@
-
+/*
+	Copyright (c) 2013-2016 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.easydarwin.org
+	Author: Fantasy@EasyDarwin.org
+*/
 #ifndef _COMMON_H__
 #define _COMMON_H__
 #define EV_RE  1
