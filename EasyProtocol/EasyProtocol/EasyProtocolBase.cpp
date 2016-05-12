fdf38e68306b4d050be9bbac058d9ed943a179cb
Babosa <mysunpany@gmail.com>
2016-05-12 16:13:53 +0800

    EasyDarwin Copyright (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.

--- EasyProtocol/EasyProtocol/EasyProtocolBase.cpp
+++ EasyProtocol/EasyProtocol/EasyProtocolBase.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013-2015 EasyDarwin.ORG.  All rights reserved.
+Copyright (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
 Github: https://github.com/EasyDarwin
 WEChat: EasyDarwin
 Website: http://www.easydarwin.org
