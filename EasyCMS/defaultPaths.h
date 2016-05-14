22721395370582503f1e8b4f4b918559313614b5
Babosa <mysunpany@gmail.com>
2016-05-14 11:22:42 +0800

    EasyCMS update DefaultPaths

--- EasyCMS/defaultPaths.h
+++ EasyCMS/defaultPaths.h
@@ -18,31 +18,31 @@
 
 # define DEFAULTPATHS_DIRECTORY_SEPARATOR	"\\"
 
-# define DEFAULTPATHS_ETC_DIR			"c:\\Program Files\\EasyDarwin\\"
-# define DEFAULTPATHS_ETC_DIR_OLD		"c:\\Program Files\\EasyDarwin\\"
-# define DEFAULTPATHS_SSM_DIR			"c:\\Program Files\\EasyDarwin\\QTSSModules\\"
-# define DEFAULTPATHS_LOG_DIR			"c:\\Program Files\\EasyDarwin\\Logs\\"
+# define DEFAULTPATHS_ETC_DIR			"c:\\Program Files\\EasyCMS\\"
+# define DEFAULTPATHS_ETC_DIR_OLD		"c:\\Program Files\\EasyCMS\\"
+# define DEFAULTPATHS_SSM_DIR			"c:\\Program Files\\EasyCMS\\QTSSModules\\"
+# define DEFAULTPATHS_LOG_DIR			"c:\\Program Files\\EasyCMS\\Logs\\"
 # define DEFAULTPATHS_PID_FILE          ""
 # define DEFAULTPATHS_PID_DIR			""
 #elif __MacOSX__
 
 # define DEFAULTPATHS_DIRECTORY_SEPARATOR	"/"
 
-# define DEFAULTPATHS_ETC_DIR			"/opt/streaming/EasyDarwin/Config/"
-# define DEFAULTPATHS_ETC_DIR_OLD		"/opt/EasyDarwin/"
-# define DEFAULTPATHS_SSM_DIR			"/opt/streaming/EasyDarwin/Modules/"
-# define DEFAULTPATHS_LOG_DIR			"/opt/streaming/EasyDarwin/Logs/"
-# define DEFAULTPATHS_PID_DIR           "/var/run/EasyDarwin/"
+# define DEFAULTPATHS_ETC_DIR			"/opt/streaming/EasyCMS/Config/"
+# define DEFAULTPATHS_ETC_DIR_OLD		"/opt/EasyCMS/"
+# define DEFAULTPATHS_SSM_DIR			"/opt/streaming/EasyCMS/Modules/"
+# define DEFAULTPATHS_LOG_DIR			"/opt/streaming/EasyCMS/Logs/"
+# define DEFAULTPATHS_PID_DIR           "/var/run/EasyCMS/"
 
 #else
 
 # define DEFAULTPATHS_DIRECTORY_SEPARATOR	"/"
 
-# define DEFAULTPATHS_ETC_DIR			"/opt/streaming/EasyDarwin/"
-# define DEFAULTPATHS_ETC_DIR_OLD		"/opt/EasyDarwin/"
-# define DEFAULTPATHS_SSM_DIR			"/usr/local/sbin/EasyDarwinModules/"
-# define DEFAULTPATHS_LOG_DIR			"/var/streaming/EasyDarwin/logs/"
-# define DEFAULTPATHS_MOVIES_DIR        "/usr/local/EasyDarwin/movies/"
-# define DEFAULTPATHS_PID_DIR           "/var/run/EasyDarwin/"
+# define DEFAULTPATHS_ETC_DIR			"/opt/streaming/EasyCMS/"
+# define DEFAULTPATHS_ETC_DIR_OLD		"/opt/EasyCMS/"
+# define DEFAULTPATHS_SSM_DIR			"/usr/local/sbin/EasyCMSModules/"
+# define DEFAULTPATHS_LOG_DIR			"/var/streaming/EasyCMS/logs/"
+# define DEFAULTPATHS_MOVIES_DIR        "/usr/local/EasyCMS/movies/"
+# define DEFAULTPATHS_PID_DIR           "/var/run/EasyCMS/"
 
 #endif
