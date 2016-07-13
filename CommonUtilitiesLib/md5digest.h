0ef00783d74b62dcbec3e2bb2a6cb1425bf43b4a
Alex <cai6811376@gmail.com>
2016-07-13 18:06:29 +0800

    Update md5digest

--- CommonUtilitiesLib/md5digest.h
+++ CommonUtilitiesLib/md5digest.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,17 +18,17 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       md5digest.h
+ /*
+	 File:       md5digest.h
 
-    Contains:   Provides a function to calculate the md5 digest 
-                given all the authentication parameters.
-                    
-*/
+	 Contains:   Provides a function to calculate the md5 digest
+				 given all the authentication parameters.
+
+ */
 
 #ifndef _MD5DIGEST_H_
 #define _MD5DIGEST_H_
@@ -36,8 +36,8 @@
 #include "StrPtrLen.h"
 
 enum {
-    kHashHexLen =   32,
-    kHashLen    =   16
+	kHashHexLen = 32,
+	kHashLen = 16
 };
 
 // HashToString allocates memory for hashStr->Ptr 
@@ -47,34 +47,33 @@ void HashToString(unsigned char aHash[kHashLen], StrPtrLen* hashStr);
 void CalcMD5HA1(StrPtrLen* userName, StrPtrLen* realm, StrPtrLen* userPassword, StrPtrLen* hashA1Hex16Bit);
 
 // allocates memory to hA1->Ptr
-void CalcHA1( StrPtrLen* algorithm, 
-              StrPtrLen* userName, 
-              StrPtrLen* realm,
-              StrPtrLen* userPassword, 
-              StrPtrLen* nonce, 
-              StrPtrLen* cNonce,
-              StrPtrLen* hA1
-            );
+void CalcHA1(StrPtrLen* algorithm,
+	StrPtrLen* userName,
+	StrPtrLen* realm,
+	StrPtrLen* userPassword,
+	StrPtrLen* nonce,
+	StrPtrLen* cNonce,
+	StrPtrLen* hA1
+);
 
 // allocates memory to hA1->Ptr
 void CalcHA1Md5Sess(StrPtrLen* hashA1Hex16Bit, StrPtrLen* nonce, StrPtrLen* cNonce, StrPtrLen* hA1);
 
 // allocates memory for requestDigest->Ptr               
-void CalcRequestDigest( StrPtrLen* hA1, 
-                        StrPtrLen* nonce, 
-                        StrPtrLen* nonceCount, 
-                        StrPtrLen* cNonce,
-                        StrPtrLen* qop,
-                        StrPtrLen* method, 
-                        StrPtrLen* digestUri, 
-                        StrPtrLen* hEntity, 
-                        StrPtrLen* requestDigest
-                      );
+void CalcRequestDigest(StrPtrLen* hA1,
+	StrPtrLen* nonce,
+	StrPtrLen* nonceCount,
+	StrPtrLen* cNonce,
+	StrPtrLen* qop,
+	StrPtrLen* method,
+	StrPtrLen* digestUri,
+	StrPtrLen* hEntity,
+	StrPtrLen* requestDigest
+);
 
-
-void to64(register char *s, register SInt32 v, register int n);
+void to64(register char* s, register SInt32 v, register int n);
 
 // Doesn't allocate any memory. The size of the result buffer should be nbytes
-void MD5Encode( char *pw, char *salt, char *result, int nbytes);
+void MD5Encode(char* pw, char* salt, char* result, int nbytes);
 
 #endif
