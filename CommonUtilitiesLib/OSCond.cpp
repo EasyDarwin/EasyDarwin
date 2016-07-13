2fb9fa4c4b013c74cf7217eeba0e14ab2ae9a934
Alex <cai6811376@gmail.com>
2016-07-13 18:25:27 +0800

    Update OSCond

--- CommonUtilitiesLib/OSCond.cpp
+++ CommonUtilitiesLib/OSCond.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,18 +18,18 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       OSCond.cpp
+ /*
+	 File:       OSCond.cpp
 
-    Contains:   Implementation of OSCond class
-    
-    
+	 Contains:   Implementation of OSCond class
 
-*/
+
+
+ */
 
 #include "OSCond.h"
 #include "OSMutex.h"
@@ -42,78 +42,79 @@
 
 
 OSCond::OSCond()
+	: fWaitCount(0)
 {
 #ifdef __Win32__
-    fCondition = ::CreateEvent(NULL, FALSE, FALSE, NULL);
-    Assert(fCondition != NULL);
+	fCondition = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	Assert(fCondition != NULL);
 #elif __PTHREADS_MUTEXES__
-    #if __MacOSX__
-        int ret = pthread_cond_init(&fCondition, NULL);
-        Assert(ret == 0);
-    #else
-        pthread_condattr_t cond_attr;
-        pthread_condattr_init(&cond_attr);
-        int ret = pthread_cond_init(&fCondition, &cond_attr);
-        Assert(ret == 0);
-    #endif
+#if __MacOSX__
+	int ret = pthread_cond_init(&fCondition, NULL);
+	Assert(ret == 0);
 #else
-    fCondition = mycondition_alloc();
-    Assert(fCondition != NULL);
+	pthread_condattr_t cond_attr;
+	pthread_condattr_init(&cond_attr);
+	int ret = pthread_cond_init(&fCondition, &cond_attr);
+	Assert(ret == 0);
+#endif
+#else
+	fCondition = mycondition_alloc();
+	Assert(fCondition != NULL);
 #endif
 }
 
 OSCond::~OSCond()
 {
 #ifdef __Win32__
-    BOOL theErr = ::CloseHandle(fCondition);
-    Assert(theErr == TRUE);
+	BOOL theErr = ::CloseHandle(fCondition);
+	Assert(theErr == TRUE);
 #elif __PTHREADS_MUTEXES__
-    pthread_cond_destroy(&fCondition);
+	pthread_cond_destroy(&fCondition);
 #else
-    Assert(fCondition != NULL);
-    mycondition_free(fCondition);
+	Assert(fCondition != NULL);
+	mycondition_free(fCondition);
 #endif
 }
 
 #if __PTHREADS_MUTEXES__
 void OSCond::TimedWait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs)
 {
-    struct timespec ts;
-    struct timeval tv;
-    struct timezone tz;
-    int sec, usec;
-    
-    //These platforms do refcounting manually, and wait will release the mutex,
-    // so we need to update the counts here
+	struct timespec ts;
+	struct timeval tv;
+	struct timezone tz;
+	int sec, usec;
 
-    inMutex->fHolderCount--;
-    inMutex->fHolder = 0;
+	//These platforms do refcounting manually, and wait will release the mutex,
+	// so we need to update the counts here
 
-    
-    if (inTimeoutInMilSecs == 0)
-        (void)pthread_cond_wait(&fCondition, &inMutex->fMutex);
-    else
-    {
-        gettimeofday(&tv, &tz);
-        sec = inTimeoutInMilSecs / 1000;
-        inTimeoutInMilSecs = inTimeoutInMilSecs - (sec * 1000);
-        Assert(inTimeoutInMilSecs < 1000);
-        usec = inTimeoutInMilSecs * 1000;
-        Assert(tv.tv_usec < 1000000);
-        ts.tv_sec = tv.tv_sec + sec;
-        ts.tv_nsec = (tv.tv_usec + usec) * 1000;
-        Assert(ts.tv_nsec < 2000000000);
-        if(ts.tv_nsec > 999999999)
-        {
-             ts.tv_sec++;
-             ts.tv_nsec -= 1000000000;
-        }
-        (void)pthread_cond_timedwait(&fCondition, &inMutex->fMutex, &ts);
-    }
+	inMutex->fHolderCount--;
+	inMutex->fHolder = 0;
 
 
-    inMutex->fHolderCount++;
-    inMutex->fHolder = pthread_self();
-    
+	if (inTimeoutInMilSecs == 0)
+		(void)pthread_cond_wait(&fCondition, &inMutex->fMutex);
+	else
+	{
+		gettimeofday(&tv, &tz);
+		sec = inTimeoutInMilSecs / 1000;
+		inTimeoutInMilSecs = inTimeoutInMilSecs - (sec * 1000);
+		Assert(inTimeoutInMilSecs < 1000);
+		usec = inTimeoutInMilSecs * 1000;
+		Assert(tv.tv_usec < 1000000);
+		ts.tv_sec = tv.tv_sec + sec;
+		ts.tv_nsec = (tv.tv_usec + usec) * 1000;
+		Assert(ts.tv_nsec < 2000000000);
+		if (ts.tv_nsec > 999999999)
+		{
+			ts.tv_sec++;
+			ts.tv_nsec -= 1000000000;
+		}
+		(void)pthread_cond_timedwait(&fCondition, &inMutex->fMutex, &ts);
+	}
+
+
+	inMutex->fHolderCount++;
+	inMutex->fHolder = pthread_self();
+
 }
 #endif
--- CommonUtilitiesLib/OSCond.h
+++ CommonUtilitiesLib/OSCond.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,111 +18,111 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       OSCond.h
+ /*
+	 File:       OSCond.h
 
-    Contains:   A simple condition variable abstraction
-    
-    
-        
+	 Contains:   A simple condition variable abstraction
 
-*/
+
+
+
+ */
 
 #ifndef _OSCOND_H_
 #define _OSCOND_H_
 
 #ifndef __Win32__
-    #if __PTHREADS_MUTEXES__
-        #include <pthread.h>
-    #else
-        #include "mycondition.h"
-    #endif
+#if __PTHREADS_MUTEXES__
+#include <pthread.h>
+#else
+#include "mycondition.h"
+#endif
 #endif
 
 #include "OSMutex.h"
 #include "MyAssert.h"
 
-class OSCond 
+class OSCond
 {
-    public:
+public:
 
-        OSCond();
-        ~OSCond();
-        
-        inline void     Signal();
-        inline void     Wait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs = 0);
-        inline void     Broadcast();
+	OSCond();
+	~OSCond();
 
-    private:
+	inline void     Signal();
+	inline void     Wait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs = 0);
+	inline void     Broadcast();
+
+private:
 
 #ifdef __Win32__
-        HANDLE              fCondition;
-        UInt32              fWaitCount;
+	HANDLE              fCondition;
+	UInt32              fWaitCount;
 #elif __PTHREADS_MUTEXES__
-        pthread_cond_t      fCondition;
-        void                TimedWait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs);
+	pthread_cond_t      fCondition;
+	void                TimedWait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs);
 #else
-        mycondition_t       fCondition;
+	mycondition_t       fCondition;
 #endif
 };
 
 inline void OSCond::Wait(OSMutex* inMutex, SInt32 inTimeoutInMilSecs)
-{ 
+{
 #ifdef __Win32__
-    DWORD theTimeout = INFINITE;
-    if (inTimeoutInMilSecs > 0)
-        theTimeout = inTimeoutInMilSecs;
-    inMutex->Unlock();
-    fWaitCount++;
-    DWORD theErr = ::WaitForSingleObject(fCondition, theTimeout);
-    fWaitCount--;
-    Assert((theErr == WAIT_OBJECT_0) || (theErr == WAIT_TIMEOUT));
-    inMutex->Lock();
+	DWORD theTimeout = INFINITE;
+	if (inTimeoutInMilSecs > 0)
+		theTimeout = inTimeoutInMilSecs;
+	inMutex->Unlock();
+	fWaitCount++;
+	DWORD theErr = ::WaitForSingleObject(fCondition, theTimeout);
+	fWaitCount--;
+	Assert((theErr == WAIT_OBJECT_0) || (theErr == WAIT_TIMEOUT));
+	inMutex->Lock();
 #elif __PTHREADS_MUTEXES__
-    this->TimedWait(inMutex, inTimeoutInMilSecs);
+	this->TimedWait(inMutex, inTimeoutInMilSecs);
 #else
-    Assert(fCondition != NULL);
-    mycondition_wait(fCondition, inMutex->fMutex, inTimeoutInMilSecs);
+	Assert(fCondition != NULL);
+	mycondition_wait(fCondition, inMutex->fMutex, inTimeoutInMilSecs);
 #endif
 }
 
 inline void OSCond::Signal()
 {
 #ifdef __Win32__
-    BOOL theErr = ::SetEvent(fCondition);
-    Assert(theErr == TRUE);
+	BOOL theErr = ::SetEvent(fCondition);
+	Assert(theErr == TRUE);
 #elif __PTHREADS_MUTEXES__
-    pthread_cond_signal(&fCondition);
+	pthread_cond_signal(&fCondition);
 #else
-    Assert(fCondition != NULL);
-    mycondition_signal(fCondition);
+	Assert(fCondition != NULL);
+	mycondition_signal(fCondition);
 #endif
 }
 
 inline void OSCond::Broadcast()
 {
 #ifdef __Win32__
-    //
-    // There doesn't seem like any more elegant way to
-    // implement Broadcast using events in Win32.
-    // This will work, it may generate spurious wakeups,
-    // but condition variables are allowed to generate
-    // spurious wakeups
-    UInt32 waitCount = fWaitCount;
-    for (UInt32 x = 0; x < waitCount; x++)
-    {
-        BOOL theErr = ::SetEvent(fCondition);
-        Assert(theErr == TRUE);
-    }
+	//
+	// There doesn't seem like any more elegant way to
+	// implement Broadcast using events in Win32.
+	// This will work, it may generate spurious wakeups,
+	// but condition variables are allowed to generate
+	// spurious wakeups
+	UInt32 waitCount = fWaitCount;
+	for (UInt32 x = 0; x < waitCount; x++)
+	{
+		BOOL theErr = ::SetEvent(fCondition);
+		Assert(theErr == TRUE);
+	}
 #elif __PTHREADS_MUTEXES__
-    pthread_cond_broadcast(&fCondition);
+	pthread_cond_broadcast(&fCondition);
 #else
-    Assert(fCondition != NULL);
-    mycondition_broadcast(fCondition);
+	Assert(fCondition != NULL);
+	mycondition_broadcast(fCondition);
 #endif
 }
 
