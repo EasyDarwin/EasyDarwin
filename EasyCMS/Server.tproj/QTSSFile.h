c0e9b0acc192276b70092b068020dec3c276681d
Alex <cai6811376@gmail.com>
2016-07-13 13:39:57 +0800

    Update QTSSFile

--- EasyCMS/Server.tproj/QTSSFile.h
+++ EasyCMS/Server.tproj/QTSSFile.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,25 +18,25 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
-*/
-/*
-    File:       QTSSFile.h
+ /*
+	 Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+	 Github: https://github.com/EasyDarwin
+	 WEChat: EasyDarwin
+	 Website: http://www.EasyDarwin.org
+ */
+ /*
+	 File:       QTSSFile.h
 
-    Contains:    
-                    
-    
-    
-    
-*/
+	 Contains:
+
+
+
+
+ */
 
 #include "QTSSDictionary.h"
 #include "QTSSModule.h"
@@ -46,39 +46,39 @@
 
 class QTSSFile : public QTSSDictionary
 {
-    public:
-    
-        QTSSFile();
-        virtual ~QTSSFile() {}
-        
-        static void     Initialize();
-        
-        //
-        // Opening & Closing
-        QTSS_Error          Open(char* inPath, QTSS_OpenFileFlags inFlags);
-        void                Close();
-        
-        //
-        // Implementation of stream functions.
-        virtual QTSS_Error  Read(void* ioBuffer, UInt32 inLen, UInt32* outLen);
-        
-        virtual QTSS_Error  Seek(UInt64 inNewPosition);
-        
-        virtual QTSS_Error  Advise(UInt64 inPosition, UInt32 inAdviseSize);
-        
-        virtual QTSS_Error  RequestEvent(QTSS_EventType inEventMask);
-        
-    private:
+public:
 
-        QTSSModule* fModule;
-        UInt64      fPosition;
-        QTSSFile*   fThisPtr;
-        
-        //
-        // File attributes
-        UInt64      fLength;
-        time_t      fModDate;
+	QTSSFile();
+	virtual ~QTSSFile() {}
 
-        static QTSSAttrInfoDict::AttrInfo   sAttributes[];
+	static void     Initialize();
+
+	//
+	// Opening & Closing
+	QTSS_Error          Open(char* inPath, QTSS_OpenFileFlags inFlags);
+	void                Close();
+
+	//
+	// Implementation of stream functions.
+	virtual QTSS_Error  Read(void* ioBuffer, UInt32 inLen, UInt32* outLen);
+
+	virtual QTSS_Error  Seek(UInt64 inNewPosition);
+
+	virtual QTSS_Error  Advise(UInt64 inPosition, UInt32 inAdviseSize);
+
+	virtual QTSS_Error  RequestEvent(QTSS_EventType inEventMask);
+
+private:
+
+	QTSSModule* fModule;
+	UInt64      fPosition;
+	QTSSFile*   fThisPtr;
+
+	//
+	// File attributes
+	UInt64      fLength;
+	time_t      fModDate;
+
+	static QTSSAttrInfoDict::AttrInfo   sAttributes[];
 };
 
