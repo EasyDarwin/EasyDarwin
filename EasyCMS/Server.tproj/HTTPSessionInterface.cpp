0db88f22eddd31bbdfc45eaf64f9a77bee534b18
Alex <cai6811376@gmail.com>
2016-07-13 11:22:20 +0800

    Update HTTPSessionInterface

--- EasyCMS/Server.tproj/HTTPSessionInterface.cpp
+++ EasyCMS/Server.tproj/HTTPSessionInterface.cpp
@@ -1,8 +1,8 @@
 /*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
+    Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+    Github: https://github.com/EasyDarwin
+    WEChat: EasyDarwin
+    Website: http://www.EasyDarwin.org
 */
 /*
     File:       HTTPSessionInterface.cpp
@@ -18,53 +18,55 @@
 
 unsigned int HTTPSessionInterface::sSessionIndexCounter = kFirstHTTPSessionID;
 
-QTSSAttrInfoDict::AttrInfo  HTTPSessionInterface::sAttributes[] = 
-{   
-	/*fields:   fAttrName, fFuncPtr, fAttrDataType, fAttrPermission */
+QTSSAttrInfoDict::AttrInfo HTTPSessionInterface::sAttributes[] =
+{
+    /*fields:   fAttrName, fFuncPtr, fAttrDataType, fAttrPermission */
     /* 0 */ { "EasyHTTPSesIndex",            NULL,          qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe },
-    /* 1 */ { "EasyHTTPSesLocalAddr",       SetupParams,    qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    /* 2 */ { "EasyHTTPSesLocalAddrStr",    SetupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    /* 3 */ { "EasyHTTPSesLocalDNS",        SetupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    /* 4 */ { "EasyHTTPSesRemoteAddr",      SetupParams,    qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    /* 5 */ { "EasyHTTPSesRemoteAddrStr",   SetupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 1 */ { "EasyHTTPSesLocalAddr",       setupParams,    qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 2 */ { "EasyHTTPSesLocalAddrStr",    setupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 3 */ { "EasyHTTPSesLocalDNS",        setupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 4 */ { "EasyHTTPSesRemoteAddr",      setupParams,    qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 5 */ { "EasyHTTPSesRemoteAddrStr",   setupParams,    qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
     /* 6 */ { "EasyHTTPSesEventCntxt",      NULL,           qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe },
     /* 7 */ { "EasyHTTPSesType",            NULL,           qtssAttrDataTypeUInt32,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite },
     /* 8 */ { "EasyHTTPSesSerial",			NULL,           qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite },
-    
+
     /* 9 */ { "EasyHTTPSessionID",			NULL,			qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe  },
-    /* 10 */{ "EasyHTTPSesLocalPort",       SetupParams,    qtssAttrDataTypeUInt16,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    /* 11 */{ "EasyHTTPSesRemotePort",      SetupParams,    qtssAttrDataTypeUInt16,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
-    
-	/* 12 */{ "EasyHTTPSesContentBody",		NULL,           qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite },
-	/* 13 */{ "EasyHTTPSesContentBodyOffset",NULL,          qtssAttrDataTypeUInt32,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite }
+    /* 10 */{ "EasyHTTPSesLocalPort",       setupParams,    qtssAttrDataTypeUInt16,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+    /* 11 */{ "EasyHTTPSesRemotePort",      setupParams,    qtssAttrDataTypeUInt16,     qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeCacheable },
+
+    /* 12 */{ "EasyHTTPSesContentBody",		NULL,           qtssAttrDataTypeCharArray,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite },
+    /* 13 */{ "EasyHTTPSesContentBodyOffset",NULL,          qtssAttrDataTypeUInt32,  qtssAttrModeRead | qtssAttrModePreempSafe | qtssAttrModeWrite }
 };
 
 
-void    HTTPSessionInterface::Initialize()
+void HTTPSessionInterface::Initialize()
 {
     for (UInt32 x = 0; x < EasyHTTPSesNumParams; x++)
+    {
         QTSSDictionaryMap::GetMap(QTSSDictionaryMap::kHTTPSessionDictIndex)->
             SetAttribute(x, sAttributes[x].fAttrName, sAttributes[x].fFuncPtr, sAttributes[x].fAttrDataType, sAttributes[x].fAttrPermission);
+    }
 }
 
 HTTPSessionInterface::HTTPSessionInterface()
-	: QTSSDictionary(QTSSDictionaryMap::GetMap(QTSSDictionaryMap::kHTTPSessionDictIndex)),
-	Task(),
-	fTimeoutTask(NULL, QTSServerInterface::GetServer()->GetPrefs()->GetSessionTimeoutInSecs() * 1000),
-	fInputStream(&fSocket),
-	fOutputStream(&fSocket, &fTimeoutTask),
-	fSessionMutex(),
-	fSocket(NULL, Socket::kNonBlockingSocketType),
-	fOutputSocketP(&fSocket),
-	fInputSocketP(&fSocket),
-	fSessionType(EasyHTTPSession),
-	//fTerminalType(0),
-	fLiveSession(true),
-	fObjectHolders(0),
-	fRequestBodyLen(-1),
-	fAuthenticated(false),
-	fRequestBody(NULL),//add
-	fCSeq(1)//add
+    : QTSSDictionary(QTSSDictionaryMap::GetMap(QTSSDictionaryMap::kHTTPSessionDictIndex)),
+    Task(),
+    fTimeoutTask(NULL, QTSServerInterface::GetServer()->GetPrefs()->GetSessionTimeoutInSecs() * 1000),
+    fInputStream(&fSocket),
+    fOutputStream(&fSocket, &fTimeoutTask),
+    fSessionMutex(),
+    fSocket(NULL, Socket::kNonBlockingSocketType),
+    fOutputSocketP(&fSocket),
+    fInputSocketP(&fSocket),
+    fSessionType(EasyHTTPSession),
+    //fTerminalType(0),
+    fLiveSession(true),
+    fObjectHolders(0),
+    fRequestBodyLen(-1),
+    fAuthenticated(false),
+    fRequestBody(NULL),//add
+    fCSeq(1)//add
 {
     fTimeoutTask.SetTask(this);
     fSocket.SetTask(this);
@@ -74,11 +76,11 @@ HTTPSessionInterface::HTTPSessionInterface()
 
     this->SetVal(EasyHTTPSesEventCntxt, &fOutputSocketP, sizeof(fOutputSocketP));
     this->SetVal(EasyHTTPSesType, &fSessionType, sizeof(fSessionType));
-	//this->SetEmptyVal(EasyHTTPSesSerial, &fSerial[0], EASY_MAX_SERIAL_LENGTH);
+    //this->SetEmptyVal(EasyHTTPSesSerial, &fSerial[0], EASY_MAX_SERIAL_LENGTH);
 
-	qtss_sprintf(fSessionID, "%s", EasyUtil::GetUUID().c_str());
-	this->SetValue(EasyHTTPSessionID, 0, fSessionID, ::strlen(fSessionID), QTSSDictionary::kDontObeyReadOnly);
-    
+    qtss_sprintf(fSessionID, "%s", EasyUtil::GetUUID().c_str());
+    this->SetValue(EasyHTTPSessionID, 0, fSessionID, ::strlen(fSessionID), QTSSDictionary::kDontObeyReadOnly);
+
     fInputStream.ShowMSG(QTSServerInterface::GetServer()->GetPrefs()->GetMSGDebugPrintfs());
     fOutputStream.ShowMSG(QTSServerInterface::GetServer()->GetPrefs()->GetMSGDebugPrintfs());
 }
@@ -86,55 +88,55 @@ HTTPSessionInterface::HTTPSessionInterface()
 HTTPSessionInterface::~HTTPSessionInterface()
 {
     // If the input socket is != output socket, the input socket was created dynamically
-    if (fInputSocketP != fOutputSocketP) 
+    if (fInputSocketP != fOutputSocketP)
         delete fInputSocketP;
-	
-	char remoteAddress[20] = {0};
-	StrPtrLen theIPAddressStr(remoteAddress,sizeof(remoteAddress));
-	QTSS_GetValue(this, EasyHTTPSesRemoteAddrStr, 0, static_cast<void*>(theIPAddressStr.Ptr), &theIPAddressStr.Len);
-	char msgStr[2048] = { 0 };
-	
-	switch(fSessionType)
-	{
-	case EasyCameraSession:
-		this->UnRegDevSession();
-		qtss_snprintf(msgStr, sizeof(msgStr), "EasyCameraSession offline from ip[%s], device_serial[%s]",remoteAddress, fDevice.serial_.c_str());
-		break;
-	case EasyNVRSession:
-		this->UnRegDevSession();
-		qtss_snprintf(msgStr, sizeof(msgStr), "EasyNVRSession offline from ip[%s]",remoteAddress);
-		break;
-	case EasyHTTPSession:
-		qtss_snprintf(msgStr, sizeof(msgStr), "EasyHTTPSession offline from ip[%s]",remoteAddress);
-		break;
-	default:
-		qtss_snprintf(msgStr, sizeof(msgStr), "Unknown session offline from ip[%s]",remoteAddress);
-		break;
-	}
-	QTSServerInterface::LogError(qtssMessageVerbosity, msgStr);
+
+    char remoteAddress[20] = { 0 };
+    StrPtrLen theIPAddressStr(remoteAddress, sizeof(remoteAddress));
+    QTSS_GetValue(this, EasyHTTPSesRemoteAddrStr, 0, static_cast<void*>(theIPAddressStr.Ptr), &theIPAddressStr.Len);
+    char msgStr[2048] = { 0 };
+
+    switch (fSessionType)
+    {
+    case EasyCameraSession:
+        this->UnRegDevSession();
+        qtss_snprintf(msgStr, sizeof(msgStr), "EasyCameraSession offline from ip[%s], device_serial[%s]", remoteAddress, fDevice.serial_.c_str());
+        break;
+    case EasyNVRSession:
+        this->UnRegDevSession();
+        qtss_snprintf(msgStr, sizeof(msgStr), "EasyNVRSession offline from ip[%s]", remoteAddress);
+        break;
+    case EasyHTTPSession:
+        qtss_snprintf(msgStr, sizeof(msgStr), "EasyHTTPSession offline from ip[%s]", remoteAddress);
+        break;
+    default:
+        qtss_snprintf(msgStr, sizeof(msgStr), "Unknown session offline from ip[%s]", remoteAddress);
+        break;
+    }
+    QTSServerInterface::LogError(qtssMessageVerbosity, msgStr);
 }
 
 void HTTPSessionInterface::DecrementObjectHolderCount()
 {
 
-//#if __Win32__
-//maybe don't need this special case but for now on Win32 we do it the old way since the killEvent code hasn't been verified on Windows.
+    //#if __Win32__
+    //maybe don't need this special case but for now on Win32 we do it the old way since the killEvent code hasn't been verified on Windows.
     this->Signal(Task::kReadEvent);//have the object wakeup in case it can go away.
     atomic_sub(&fObjectHolders, 1);
-//#else
-//    if (0 == atomic_sub(&fObjectHolders, 1))
-//        this->Signal(Task::kKillEvent);
-//#endif
+    //#else
+    //    if (0 == atomic_sub(&fObjectHolders, 1))
+    //        this->Signal(Task::kKillEvent);
+    //#endif
 
 }
 
 QTSS_Error HTTPSessionInterface::Write(void* inBuffer, UInt32 inLength,
-                                            UInt32* outLenWritten, UInt32 inFlags)
+    UInt32* outLenWritten, UInt32 inFlags)
 {
     UInt32 sendType = HTTPResponseStream::kDontBuffer;
     if ((inFlags & qtssWriteFlagsBufferData) != 0)
         sendType = HTTPResponseStream::kAlwaysBuffer;
-    
+
     iovec theVec[2];
     theVec[1].iov_base = static_cast<char*>(inBuffer);
     theVec[1].iov_len = inLength;
@@ -151,22 +153,22 @@ QTSS_Error HTTPSessionInterface::Read(void* ioBuffer, UInt32 inLength, UInt32* o
     //
     // Don't let callers of this function accidently creep past the end of the
     // request body.  If the request body size isn't known, fRequestBodyLen will be -1
-    
+
     if (fRequestBodyLen == 0)
         return QTSS_NoMoreData;
-        
+
     if ((fRequestBodyLen > 0) && (static_cast<SInt32>(inLength) > fRequestBodyLen))
         inLength = fRequestBodyLen;
-    
+
     UInt32 theLenRead = 0;
     QTSS_Error theErr = fInputStream.Read(ioBuffer, inLength, &theLenRead);
-    
+
     if (fRequestBodyLen >= 0)
         fRequestBodyLen -= theLenRead;
 
     if (outLenRead != NULL)
         *outLenRead = theLenRead;
-    
+
     return theErr;
 }
 
@@ -176,54 +178,54 @@ QTSS_Error HTTPSessionInterface::RequestEvent(QTSS_EventType inEventMask)
         fInputSocketP->RequestEvent(EV_RE);
     if (inEventMask & QTSS_WriteableEvent)
         fOutputSocketP->RequestEvent(EV_WR);
-        
+
     return QTSS_NoErr;
 }
 
-void    HTTPSessionInterface::SnarfInputSocket( HTTPSessionInterface* fromHTTPSession )
+void HTTPSessionInterface::snarfInputSocket(HTTPSessionInterface* fromHTTPSession)
 {
-    Assert( fromHTTPSession != NULL );
-    Assert( fromHTTPSession->fOutputSocketP != NULL );
-    
-    fInputStream.SnarfRetreat( fromHTTPSession->fInputStream );
+    Assert(fromHTTPSession != NULL);
+    Assert(fromHTTPSession->fOutputSocketP != NULL);
+
+    fInputStream.SnarfRetreat(fromHTTPSession->fInputStream);
 
     if (fInputSocketP == fOutputSocketP)
-        fInputSocketP = NEW TCPSocket( this, Socket::kNonBlockingSocketType );
+        fInputSocketP = NEW TCPSocket(this, Socket::kNonBlockingSocketType);
     else
         fInputSocketP->Cleanup();   // if this is a socket replacing an old socket, we need
                                     // to make sure the file descriptor gets closed
-    fInputSocketP->SnarfSocket( fromHTTPSession->fSocket );
-    
+    fInputSocketP->SnarfSocket(fromHTTPSession->fSocket);
+
     // fInputStream, meet your new input socket
-    fInputStream.AttachToSocket( fInputSocketP );
+    fInputStream.AttachToSocket(fInputSocketP);
 }
 
 
-void* HTTPSessionInterface::SetupParams(QTSSDictionary* inSession, UInt32* /*outLen*/)
+void* HTTPSessionInterface::setupParams(QTSSDictionary* inSession, UInt32* /*outLen*/)
 {
     HTTPSessionInterface* theSession = static_cast<HTTPSessionInterface*>(inSession);
- 
+
     theSession->fLocalAddr = theSession->fSocket.GetLocalAddr();
     theSession->fRemoteAddr = theSession->fSocket.GetRemoteAddr();
-    
+
     theSession->fLocalPort = theSession->fSocket.GetLocalPort();
     theSession->fRemotePort = theSession->fSocket.GetRemotePort();
-    
+
     StrPtrLen* theLocalAddrStr = theSession->fSocket.GetLocalAddrStr();
     StrPtrLen* theLocalDNSStr = theSession->fSocket.GetLocalDNSStr();
     StrPtrLen* theRemoteAddrStr = theSession->fSocket.GetRemoteAddrStr();
     if (theLocalAddrStr == NULL || theLocalDNSStr == NULL || theRemoteAddrStr == NULL)
     {    //the socket is bad most likely values are all 0. If the socket had an error we shouldn't even be here.
          //theLocalDNSStr is set to localAddr if it is unavailable, so it should be present at this point as well.
-         Assert(0);   //for debugging
-         return NULL; //nothing to set
+        Assert(0);   //for debugging
+        return NULL; //nothing to set
     }
     theSession->SetVal(EasyHTTPSesLocalAddr, &theSession->fLocalAddr, sizeof(theSession->fLocalAddr));
     theSession->SetVal(EasyHTTPSesLocalAddrStr, theLocalAddrStr->Ptr, theLocalAddrStr->Len);
     theSession->SetVal(EasyHTTPSesLocalDNS, theLocalDNSStr->Ptr, theLocalDNSStr->Len);
     theSession->SetVal(EasyHTTPSesRemoteAddr, &theSession->fRemoteAddr, sizeof(theSession->fRemoteAddr));
     theSession->SetVal(EasyHTTPSesRemoteAddrStr, theRemoteAddrStr->Ptr, theRemoteAddrStr->Len);
-    
+
     theSession->SetVal(EasyHTTPSesLocalPort, &theSession->fLocalPort, sizeof(theSession->fLocalPort));
     theSession->SetVal(EasyHTTPSesRemotePort, &theSession->fRemotePort, sizeof(theSession->fRemotePort));
     return NULL;
@@ -231,29 +233,29 @@ void* HTTPSessionInterface::SetupParams(QTSSDictionary* inSession, UInt32* /*out
 
 QTSS_Error HTTPSessionInterface::UpdateDevSnap(const char* inSnapTime, const char* inSnapJpg) const
 {
-	if(!fAuthenticated) return QTSS_NoErr;
-	return QTSS_NoErr;
+    if (!fAuthenticated) return QTSS_NoErr;
+    return QTSS_NoErr;
 }
 
 void HTTPSessionInterface::UnRegDevSession() const
 {
-	if (fAuthenticated)
-	{
-		char msgStr[512];
-		qtss_snprintf(msgStr, sizeof(msgStr), "Device unregister，Device_serial[%s]\n", fDevice.serial_.c_str());
-		QTSServerInterface::LogError(qtssMessageVerbosity, msgStr);
+    if (fAuthenticated)
+    {
+        char msgStr[512];
+        qtss_snprintf(msgStr, sizeof(msgStr), "Device unregister，Device_serial[%s]\n", fDevice.serial_.c_str());
+        QTSServerInterface::LogError(qtssMessageVerbosity, msgStr);
 
-		QTSServerInterface::GetServer()->GetDeviceSessionMap()->UnRegister(fDevice.serial_);//add
-		//在redis上删除设备
-		//QTSServerInterface::GetServer()->RedisDelDevName(fDevice.serial_.c_str());
+        QTSServerInterface::GetServer()->GetDeviceSessionMap()->UnRegister(fDevice.serial_);//add
+        //在redis上删除设备
+        //QTSServerInterface::GetServer()->RedisDelDevName(fDevice.serial_.c_str());
 
-		QTSS_RoleParams theParams;
-		theParams.StreamNameParams.inStreamName = const_cast<char *>(fDevice.serial_.c_str());
-		UInt32 numModules = QTSServerInterface::GetNumModulesInRole(QTSSModule::kRedisDelDevNameRole);
-		for ( UInt32 currentModule=0;currentModule < numModules; currentModule++)
-		{
-			QTSSModule* theModule = QTSServerInterface::GetModule(QTSSModule::kRedisDelDevNameRole, currentModule);
-			(void)theModule->CallDispatch(Easy_RedisDelDevName_Role, &theParams);
-		}
-	}
+        QTSS_RoleParams theParams;
+        theParams.StreamNameParams.inStreamName = const_cast<char *>(fDevice.serial_.c_str());
+        UInt32 numModules = QTSServerInterface::GetNumModulesInRole(QTSSModule::kRedisDelDevNameRole);
+        for (UInt32 currentModule = 0; currentModule < numModules; currentModule++)
+        {
+            QTSSModule* theModule = QTSServerInterface::GetModule(QTSSModule::kRedisDelDevNameRole, currentModule);
+            (void)theModule->CallDispatch(Easy_RedisDelDevName_Role, &theParams);
+        }
+    }
 }
--- EasyCMS/Server.tproj/HTTPSessionInterface.h
+++ EasyCMS/Server.tproj/HTTPSessionInterface.h
@@ -1,14 +1,14 @@
 /*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
+    Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+    Github: https://github.com/EasyDarwin
+    WEChat: EasyDarwin
+    Website: http://www.EasyDarwin.org
 */
 /*
     File:       HTTPSessionInterface.h
 
     Contains:   Presents an API for session-wide resources for modules to use.
-                Implements the CMS Session dictionary for QTSS API. 
+                Implements the CMS Session dictionary for QTSS API.
 */
 
 #ifndef __HTTPSESSIONINTERFACE_H__
@@ -38,16 +38,16 @@ public:
 
     //Initialize must be called right off the bat to initialize dictionary resources
     static void     Initialize();
-    
+
     HTTPSessionInterface();
     virtual ~HTTPSessionInterface();
-    
+
     //Is this session alive? If this returns false, clean up and begone as
     //fast as possible
-    Bool16 IsLiveSession()      { return fSocket.IsConnected() && fLiveSession; }
-    
+    Bool16 IsLiveSession() { return fSocket.IsConnected() && fLiveSession; }
+
     // Allows clients to refresh the timeout
-    void RefreshTimeout()       { fTimeoutTask.RefreshTimeout(); }
+    void RefreshTimeout() { fTimeoutTask.RefreshTimeout(); }
 
     // In order to facilitate sending out of band data on the RTSP connection,
     // other objects need to have direct pointer access to this object. But,
@@ -57,79 +57,85 @@ public:
     // false it may be wise to relinquish control of the session
     void IncrementObjectHolderCount() { (void)atomic_add(&fObjectHolders, 1); }
     void DecrementObjectHolderCount();
-    
+
     //Two main things are persistent through the course of a session, not
     //associated with any one request. The RequestStream (which can be used for
     //getting data from the client), and the socket. OOps, and the ResponseStream
-    HTTPRequestStream*  GetInputStream()    { return &fInputStream; }
-    HTTPResponseStream* GetOutputStream()   { return &fOutputStream; }
-    TCPSocket*          GetSocket()         { return &fSocket; }
-    OSMutex*            GetSessionMutex()   { return &fSessionMutex; }
-    
-    UInt32              GetSessionIndex()      { return fSessionIndex; }
-    
+    HTTPRequestStream*  GetInputStream() { return &fInputStream; }
+    HTTPResponseStream* GetOutputStream() { return &fOutputStream; }
+    TCPSocket*          GetSocket() { return &fSocket; }
+    OSMutex*            GetSessionMutex() { return &fSessionMutex; }
+
+    UInt32              GetSessionIndex() { return fSessionIndex; }
+
     // Request Body Length
     // This object can enforce a length of the request body to prevent callers
     // of Read() from overrunning the request body and going into the next request.
     // -1 is an unknown request body length. If the body length is unknown,
     // this object will do no length enforcement. 
-    void                SetRequestBodyLength(SInt32 inLength)   { fRequestBodyLen = inLength; }
-    SInt32              GetRemainingReqBodyLen()                { return fRequestBodyLen; }
+    void                SetRequestBodyLength(SInt32 inLength) { fRequestBodyLen = inLength; }
+    SInt32              GetRemainingReqBodyLen() { return fRequestBodyLen; }
 
-	QTSS_Error UpdateDevSnap(const char* inSnapTime, const char* inSnapJpg) const;
-	void UnRegDevSession() const;
-    
+    QTSS_Error UpdateDevSnap(const char* inSnapTime, const char* inSnapJpg) const;
+    void UnRegDevSession() const;
+
     // QTSS STREAM FUNCTIONS
-    
+
     // Allows non-buffered writes to the client. These will flow control.
-    
+
     //THE FIRST ENTRY OF THE IOVEC MUST BE BLANK!!!
     virtual QTSS_Error WriteV(iovec* inVec, UInt32 inNumVectors, UInt32 inTotalLength, UInt32* outLenWritten);
     virtual QTSS_Error Write(void* inBuffer, UInt32 inLength, UInt32* outLenWritten, UInt32 inFlags);
     virtual QTSS_Error Read(void* ioBuffer, UInt32 inLength, UInt32* outLenRead);
     virtual QTSS_Error RequestEvent(QTSS_EventType inEventMask);
 
+    strDevice *GetDeviceInfo() { return &fDevice; }
+    UInt32 GetCSeq() { OSMutexLocker MutexLocker(&fMutexCSeq); return fCSeq++; }
+
     enum
     {
-        kMaxUserNameLen         = 32,
-        kMaxUserPasswordLen     = 32
+        kMaxUserNameLen = 32,
+        kMaxUserPasswordLen = 32
     };
 
-	void PushNVRMessage(EasyNVRMessage &msg) { fNVRMessageQueue.push_back(msg); }
+    void PushNVRMessage(EasyNVRMessage &msg) { fNVRMessageQueue.push_back(msg); }
 
 protected:
+    void        snarfInputSocket(HTTPSessionInterface* fromRTSPSession);
+    // Dictionary support Param retrieval function
+    static void*        setupParams(QTSSDictionary* inSession, UInt32* outLen);
+
     enum
     {
-        kFirstHTTPSessionID     = 1,    //UInt32
+        kFirstHTTPSessionID = 1,    //UInt32
     };
 
     char                fSessionID[QTSS_MAX_SESSION_ID_LENGTH];
 
-	//char				fSerial[EASY_MAX_SERIAL_LENGTH];
-	//StrPtrLen			fDevSerialPtr;
-	//string				fDevSerial;
-	//boost::mutex		fNVROperatorMutex;
-	//boost::mutex		fStreamReqCountMutex;
-	//boost::condition_variable fCond;
-	EasyNVRMessageQueue fNVRMessageQueue;
-	//OSRef				fDevRef;
+    //char				fSerial[EASY_MAX_SERIAL_LENGTH];
+    //StrPtrLen			fDevSerialPtr;
+    //string				fDevSerial;
+    //boost::mutex		fNVROperatorMutex;
+    //boost::mutex		fStreamReqCountMutex;
+    //boost::condition_variable fCond;
+    EasyNVRMessageQueue fNVRMessageQueue;
+    //OSRef				fDevRef;
 
     TimeoutTask         fTimeoutTask;//allows the session to be timed out
-    
+
     HTTPRequestStream   fInputStream;
     HTTPResponseStream  fOutputStream;
-    
+
     OSMutex             fSessionMutex;
 
     //+rt  socket we get from "accept()"
     TCPSocket           fSocket;
     TCPSocket*          fOutputSocketP;
     TCPSocket*          fInputSocketP;  // <-- usually same as fSocketP, unless we're HTTP Proxying
-    
-    void        SnarfInputSocket( HTTPSessionInterface* fromRTSPSession );
-    
+
+
     Easy_SessionType    fSessionType;	//普通socket,NVR,智能主机，摄像机
-	//UInt32				fTerminalType;	//终端类型ARM、PC、Android、IOS
+    //UInt32				fTerminalType;	//终端类型ARM、PC、Android、IOS
 
     Bool16              fLiveSession;
     unsigned int        fObjectHolders;
@@ -138,34 +144,27 @@ protected:
     UInt32              fLocalAddr;
     UInt32              fRemoteAddr;
     SInt32              fRequestBodyLen;
-    
+
     UInt16              fLocalPort;
     UInt16              fRemotePort;
-    
-	Bool16				fAuthenticated;
+
+    Bool16				fAuthenticated;
 
     static unsigned int		sSessionIndexCounter;
 
-    // Dictionary support Param retrieval function
-    static void*        SetupParams(QTSSDictionary* inSession, UInt32* outLen);
-    
     static QTSSAttrInfoDict::AttrInfo   sAttributes[];
-	
-	//add,紫光，start
-	#define CliStartStreamTimeout	30000//客户端开始流超时时间，单位为ms
-	#define CliSNapShotTimeout		30000//客户端抓拍超时时间，单位为ms  
-	#define SessionIDTimeout		30000//sessionID在redis上的存活时间，单位为ms
 
-	strDevice fDevice;//add,存储设备信息，仅当Session类型是设备时有效
-	
-	char* fRequestBody;//存储请求的数据部分
+    //add,紫光，start
+#define CliStartStreamTimeout	30000//客户端开始流超时时间，单位为ms
+#define CliSNapShotTimeout		30000//客户端抓拍超时时间，单位为ms  
+#define SessionIDTimeout		30000//sessionID在redis上的存活时间，单位为ms
 
-	OSMutex fMutexCSeq;//fCSeq互斥操作实现，因为可能多个线程同时fCSeq++,和MsgMap共同使用一个互斥量
-	UInt32 fCSeq;//当前Session向对方发送请求时，fCSeq每次加1
+    strDevice fDevice;//add,存储设备信息，仅当Session类型是设备时有效
 
-public:
-	strDevice *GetDeviceInfo(){return &fDevice;}
-	UInt32 GetCSeq(){OSMutexLocker MutexLocker(&fMutexCSeq);return fCSeq++;}
+    char* fRequestBody;//存储请求的数据部分
+
+    OSMutex fMutexCSeq;//fCSeq互斥操作实现，因为可能多个线程同时fCSeq++,和MsgMap共同使用一个互斥量
+    UInt32 fCSeq;//当前Session向对方发送请求时，fCSeq每次加1
 
 };
 #endif // __HTTPSESSIONINTERFACE_H__
