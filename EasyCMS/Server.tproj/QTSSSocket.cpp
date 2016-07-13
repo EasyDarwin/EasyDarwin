34189dc8a631ca0b3bb1dd2587cb1d225b090ce9
Alex <cai6811376@gmail.com>
2016-07-13 13:55:57 +0800

    Update QTSSSocket

--- EasyCMS/Server.tproj/QTSSSocket.cpp
+++ EasyCMS/Server.tproj/QTSSSocket.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,33 +18,33 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
-*/
-/*
-    File:       QTSSSocket.cpp
-    Contains:   A QTSS Stream object for a generic socket
-*/
+ /*
+	 Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+	 Github: https://github.com/EasyDarwin
+	 WEChat: EasyDarwin
+	 Website: http://www.EasyDarwin.org
+ */
+ /*
+	 File:       QTSSSocket.cpp
+	 Contains:   A QTSS Stream object for a generic socket
+ */
 
 #include "QTSSSocket.h"
 
-QTSS_Error  QTSSSocket::RequestEvent(QTSS_EventType inEventMask)
+QTSS_Error QTSSSocket::RequestEvent(QTSS_EventType inEventMask)
 {
-    int theMask = 0;
-    
-    if (inEventMask & QTSS_ReadableEvent)
-        theMask |= EV_RE;
-    if (inEventMask & QTSS_WriteableEvent)
-        theMask |= EV_WR;
-        
-    fEventContext.SetTask(this->GetTask());
-    fEventContext.RequestEvent(theMask);
-    return QTSS_NoErr;
+	int theMask = 0;
+
+	if (inEventMask & QTSS_ReadableEvent)
+		theMask |= EV_RE;
+	if (inEventMask & QTSS_WriteableEvent)
+		theMask |= EV_WR;
+
+	fEventContext.SetTask(this->GetTask());
+	fEventContext.RequestEvent(theMask);
+	return QTSS_NoErr;
 }
