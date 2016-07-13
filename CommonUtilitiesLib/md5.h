d53a2f84cf2f44b459e532532b78174daca1117a
Alex <cai6811376@gmail.com>
2016-07-13 18:03:25 +0800

    Update md5

--- CommonUtilitiesLib/md5.h
+++ CommonUtilitiesLib/md5.h
@@ -11,7 +11,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -19,39 +19,39 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
-/* MD5.H - header file for MD5.C
- */
+ /* MD5.H - header file for MD5.C
+  */
 
-/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
+  /* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+  rights reserved.
 
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
+  License to copy and use this software is granted provided that it
+  is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+  Algorithm" in all material mentioning or referencing this software
+  or this function.
 
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
+  License is also granted to make and use derivative works provided
+  that such works are identified as "derived from the RSA Data
+  Security, Inc. MD5 Message-Digest Algorithm" in all material
+  mentioning or referencing the derived work.
 
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
+  RSA Data Security, Inc. makes no representations concerning either
+  the merchantability of this software or the suitability of this
+  software for any particular purpose. It is provided "as is"
+  without express or implied warranty of any kind.
 
-These notices must be retained in any copies of any part of this
-documentation and/or software.
- */
+  These notices must be retained in any copies of any part of this
+  documentation and/or software.
+   */
 
-/*
- Note: Renamed the functions to avoid having same symbols in
- the linked-in frameworks.
- It is a hack to work around the problem.
- */
+   /*
+	Note: Renamed the functions to avoid having same symbols in
+	the linked-in frameworks.
+	It is a hack to work around the problem.
+	*/
 
 #ifndef _MD5_H_
 #define _MD5_H_
@@ -62,16 +62,16 @@ documentation and/or software.
 extern "C" {
 #endif
 
-/* MD5 context. */
-typedef struct {
-  UInt32 state[4];                                   /* state (ABCD) */
-  UInt32 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-  unsigned char buffer[64];                         /* input buffer */
-} MD5_CTX;
+	/* MD5 context. */
+	typedef struct {
+		UInt32 state[4];                                   /* state (ABCD) */
+		UInt32 count[2];        /* number of bits, modulo 2^64 (lsb first) */
+		unsigned char buffer[64];                         /* input buffer */
+	} MD5_CTX;
 
-void MD5_Init(MD5_CTX *context);
-void MD5_Update(MD5_CTX *context, unsigned char *input, unsigned int inputLen);
-void MD5_Final(unsigned char digest[16], MD5_CTX *context);
+	void MD5_Init(MD5_CTX* context);
+	void MD5_Update(MD5_CTX* context, unsigned char* input, unsigned int inputLen);
+	void MD5_Final(unsigned char digest[16], MD5_CTX* context);
 
 #ifdef __cplusplus
 }
@@ -79,7 +79,7 @@ void MD5_Final(unsigned char digest[16], MD5_CTX *context);
 
 #endif
 
- 
+
 
 
 
