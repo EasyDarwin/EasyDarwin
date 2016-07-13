66afeac400be4a34243d18f76b302eecc85427b7
Alex <cai6811376@gmail.com>
2016-07-13 11:39:58 +0800

    Update OSRefTableEx

--- CommonUtilitiesLib/OSRefTableEx.cpp
+++ CommonUtilitiesLib/OSRefTableEx.cpp
@@ -1,71 +1,76 @@
 /*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
+    Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+    Github: https://github.com/EasyDarwin
+    WEChat: EasyDarwin
+    Website: http://www.EasyDarwin.org
 */
 
 #include"OSRefTableEx.h"
 #include <errno.h>
 
-OSRefTableEx::OSRefEx * OSRefTableEx::Resolve(const string& keystring)//引用对象，存在返回指针，否则返回NULL
+OSRefTableEx::OSRefEx * OSRefTableEx::Resolve(const string& key)//引用对象，存在返回指针，否则返回NULL
 {
-	OSMutexLocker locker(&m_Mutex);
-	if (m_Map.find(keystring) == m_Map.end())//不存在
-		return NULL;
-	m_Map[keystring]->AddRef(1);
-	return m_Map[keystring];
+    OSMutexLocker locker(&m_Mutex);
+    if (m_Map.find(key) == m_Map.end())//不存在
+        return NULL;
+    m_Map[key]->AddRef(1);
+
+    return m_Map[key];
 }
 
-OS_Error OSRefTableEx::Release(const string& keystring)//释放引用
+OS_Error OSRefTableEx::Release(const string& key)//释放引用
 {
-	OSMutexLocker locker(&m_Mutex);
-	if (m_Map.find(keystring) == m_Map.end())//不存在
-		return EPERM;
-	//make sure to wakeup anyone who may be waiting for this resource to be released
-	m_Map[keystring]->AddRef(-1);//减少引用
-	m_Map[keystring]->GetCondPtr()->Signal();
-	return OS_NoErr;
+    OSMutexLocker locker(&m_Mutex);
+    if (m_Map.find(key) == m_Map.end())//不存在
+        return EPERM;
+    //make sure to wakeup anyone who may be waiting for this resource to be released
+    m_Map[key]->AddRef(-1);//减少引用
+    m_Map[key]->GetCondPtr()->Signal();
+
+    return OS_NoErr;
 }
 
-OS_Error OSRefTableEx::Register(const string& keystring, void* pobject)//加入到map中
+OS_Error OSRefTableEx::Register(const string& key, void* pObject)//加入到map中
 {
-	Assert(pobject != NULL);
-	if (pobject == NULL)
-		return EPERM;
+    Assert(pObject != NULL);
+    if (pObject == NULL)
+        return EPERM;
 
-	OSMutexLocker locker(&m_Mutex);//互斥
-	if (m_Map.find(keystring) != m_Map.end())//已经出现，拒绝重复的key
-		return EPERM;
-	OSRefTableEx::OSRefEx *RefTemp = new OSRefTableEx::OSRefEx(pobject);//生成value，在此new，在UnRegister里delete
-	m_Map[keystring] = RefTemp;//加入到map中
-	return OS_NoErr;
+    OSMutexLocker locker(&m_Mutex);//互斥
+    if (m_Map.find(key) != m_Map.end())//已经出现，拒绝重复的key
+        return EPERM;
+    OSRefEx *RefTemp = new OSRefEx(pObject);//生成value，在此new，在UnRegister里delete
+    m_Map[key] = RefTemp;//加入到map中
+
+    return OS_NoErr;
 }
 
-OS_Error OSRefTableEx::UnRegister(const string& keystring)//从map中移除，必须引用计数为0才允许移除
+OS_Error OSRefTableEx::UnRegister(const string& key)//从map中移除，必须引用计数为0才允许移除
 {
-	OSMutexLocker locker(&m_Mutex);
-	if (m_Map.find(keystring) == m_Map.end())//不存在当前key
-		return EPERM;
-	//make sure that no one else is using the object
-	while (m_Map[keystring]->GetRefNum() > 0)
-		m_Map[keystring]->GetCondPtr()->Wait(&m_Mutex);
+    OSMutexLocker locker(&m_Mutex);
+    if (m_Map.find(key) == m_Map.end())//不存在当前key
+        return EPERM;
+    //make sure that no one else is using the object
+    while (m_Map[key]->GetRefNum() > 0)
+        m_Map[key]->GetCondPtr()->Wait(&m_Mutex);
 
-	delete m_Map[keystring];//释放
-	m_Map.erase(keystring);//移除
-	return OS_NoErr;
+    delete m_Map[key];//释放
+    m_Map.erase(key);//移除
+
+    return OS_NoErr;
 }
 
-OS_Error OSRefTableEx::TryUnRegister(const string& keystring)
+OS_Error OSRefTableEx::TryUnRegister(const string& key)
 {
-	OSMutexLocker locker(&m_Mutex);
-	if (m_Map.find(keystring) == m_Map.end())//不存在当前key
-		return EPERM;
-	if (m_Map[keystring]->GetRefNum() > 0)
-		return EPERM;
-	// At this point, this is guarenteed not to block, because
-	// we've already checked that the refCount is low.
-	delete m_Map[keystring];//释放
-	m_Map.erase(keystring);//移除
-	return OS_NoErr;
+    OSMutexLocker locker(&m_Mutex);
+    if (m_Map.find(key) == m_Map.end())//不存在当前key
+        return EPERM;
+    if (m_Map[key]->GetRefNum() > 0)
+        return EPERM;
+    // At this point, this is guarenteed not to block, because
+    // we've already checked that the refCount is low.
+    delete m_Map[key];//释放
+    m_Map.erase(key);//移除
+
+    return OS_NoErr;
 }
--- CommonUtilitiesLib/OSRefTableEx.h
+++ CommonUtilitiesLib/OSRefTableEx.h
@@ -1,6 +1,6 @@
-
 #ifndef _OSREFTABLEEX_H_
 #define _OSREFTABLEEX_H_
+
 //因为darwin的引用表不允许相同的key，但他的字符串hash函数又不能做到这一点。因此考虑使用STL中的map进行代替。
 //注意这个类是后来加上的，不是Darwin自带的
 #include<map>
@@ -9,38 +9,40 @@ using namespace std;
 
 #include "OSCond.h"
 #include "OSHeaders.h"
+
 class OSRefTableEx
 {
 public:
-	class OSRefEx
-	{
-	private:
-		void *mp_Object;//引用的对象或其余数据
-		int m_Count;//当前引用对象计数，只有为0时才允许对象销毁
-		OSCond  m_Cond;//to block threads waiting for this ref.
-	public:
-		OSRefEx() :mp_Object(NULL), m_Count(0) {}
-		OSRefEx(void * pobject) :mp_Object(pobject), m_Count(0) {}
-	public:
-		void *GetObjectPtr() const { return mp_Object; }
-		int GetRefNum() const { return m_Count; }
-		OSCond *GetCondPtr() { return &m_Cond; }
-		void AddRef(int num) { m_Count += num; }
-	};
-private:
-	map<string, OSRefTableEx::OSRefEx*> m_Map;//以string为key，以OSRefEx为value
-	OSMutex         m_Mutex;//提供对map的互斥操作
-public:
-	OSRefEx *    Resolve(const string& keystring);//引用对象
-	OS_Error     Release(const string& keystring);//释放引用
-	OS_Error     Register(const string& keystring, void* pobject);//加入到map中
-	OS_Error     UnRegister(const string& keystring);//从map中移除
+    class OSRefEx
+    {
 
-	OS_Error TryUnRegister(const string& keystring);//尝试移除，如果引用为0,则移除并返回true，否则返回false
-public:
-	int GetEleNumInMap() const { return m_Map.size(); }//获得map中的元素个数
-	OSMutex *GetMutex() { return &m_Mutex; }//给外面提供互斥接口
-	map<string, OSRefTableEx::OSRefEx*> *GetMap() { return &m_Map; }
+    public:
+        OSRefEx() : mp_Object(NULL), m_Count(0) {}
+        OSRefEx(void* pobject) : mp_Object(pobject), m_Count(0) {}
+        void* GetObjectPtr() const { return mp_Object; }
+        int GetRefNum() const { return m_Count; }
+        OSCond* GetCondPtr() { return &m_Cond; }
+        void AddRef(int num) { m_Count += num; }
+
+    private:
+        void* mp_Object;//引用的对象或其余数据
+        int m_Count;//当前引用对象计数，只有为0时才允许对象销毁
+        OSCond  m_Cond;//to block threads waiting for this ref.
+    };
+
+    OSRefEx*     Resolve(const string& key);//引用对象
+    OS_Error     Release(const string& key);//释放引用
+    OS_Error     Register(const string& key, void* pObject);//加入到map中
+    OS_Error     UnRegister(const string& key);//从map中移除
+
+    OS_Error TryUnRegister(const string& key);//尝试移除，如果引用为0,则移除并返回true，否则返回false
+    int GetEleNumInMap() const { return m_Map.size(); }//获得map中的元素个数
+    OSMutex *GetMutex() { return &m_Mutex; }//给外面提供互斥接口
+    map<string, OSRefEx*> *GetMap() { return &m_Map; }
+
+private:
+    map<string, OSRefEx*> m_Map;//以string为key，以OSRefEx为value
+    OSMutex         m_Mutex;//提供对map的互斥操作
 };
 typedef map<string, OSRefTableEx::OSRefEx*> OSHashMap;
 typedef map<string, OSRefTableEx::OSRefEx*>::iterator OSRefIt;
@@ -48,16 +50,14 @@ typedef map<string, OSRefTableEx::OSRefEx*>::iterator OSRefIt;
 class OSRefReleaserEx
 {
 public:
+    OSRefReleaserEx(OSRefTableEx* pTable, const string& key) : fOSRefTable(pTable), fOSRefKey(key) {}
+    ~OSRefReleaserEx() { fOSRefTable->Release(fOSRefKey); }
 
-	OSRefReleaserEx(OSRefTableEx* inTable, const string& inKeyString) : fOSRefTable(inTable), fOSRefKey(inKeyString) {}
-	~OSRefReleaserEx() { fOSRefTable->Release(fOSRefKey); }
-
-	string GetRefKey() const { return fOSRefKey; }
+    string GetRefKey() const { return fOSRefKey; }
 
 private:
-
-	OSRefTableEx*	fOSRefTable;
-	string			fOSRefKey;
+    OSRefTableEx*	fOSRefTable;
+    string			fOSRefKey;
 };
 
 #endif _OSREFTABLEEX_H_
