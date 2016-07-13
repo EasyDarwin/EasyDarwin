1d5bd5497d4c191f28839b87c4dd6a66e66d5d2a
Alex <cai6811376@gmail.com>
2016-07-13 19:54:41 +0800

    Update ResizeableStringFormatter

--- CommonUtilitiesLib/ResizeableStringFormatter.cpp
+++ CommonUtilitiesLib/ResizeableStringFormatter.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,37 +18,37 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       ResizeableStringFormatter.cpp
+ /*
+	 File:       ResizeableStringFormatter.cpp
 
-    Contains:   Implements object defined in ResizeableStringFormatter.h
-    
-    
-*/
+	 Contains:   Implements object defined in ResizeableStringFormatter.h
+
+
+ */
 
 #include "ResizeableStringFormatter.h"
 #include "OSMemory.h"
 
 Bool16 ResizeableStringFormatter::BufferIsFull(char* inBuffer, UInt32 inBufferLen)
 {
-    //allocate a buffer twice as big as the old one, and copy over the contents
-    UInt32 theNewBufferSize = this->GetTotalBufferSize() * 2;
-    if (theNewBufferSize == 0)
-        theNewBufferSize = 64;
-        
-    char* theNewBuffer = NEW char[theNewBufferSize];
-    ::memcpy(theNewBuffer, inBuffer, inBufferLen);
+	//allocate a buffer twice as big as the old one, and copy over the contents
+	UInt32 theNewBufferSize = this->GetTotalBufferSize() * 2;
+	if (theNewBufferSize == 0)
+		theNewBufferSize = 64;
 
-    //if the old buffer was dynamically allocated also, we'd better delete it.
-    if (inBuffer != fOriginalBuffer)
-        delete [] inBuffer;
-    
-    fStartPut = theNewBuffer;
-    fCurrentPut = theNewBuffer + inBufferLen;
-    fEndPut = theNewBuffer + theNewBufferSize;
+	char* theNewBuffer = NEW char[theNewBufferSize];
+	::memcpy(theNewBuffer, inBuffer, inBufferLen);
+
+	//if the old buffer was dynamically allocated also, we'd better delete it.
+	if (inBuffer != fOriginalBuffer)
+		delete[] inBuffer;
+
+	fStartPut = theNewBuffer;
+	fCurrentPut = theNewBuffer + inBufferLen;
+	fEndPut = theNewBuffer + theNewBufferSize;
 	return true;
 }
--- CommonUtilitiesLib/ResizeableStringFormatter.h
+++ CommonUtilitiesLib/ResizeableStringFormatter.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,20 +18,20 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       ResizeableStringFormatter.h
+ /*
+	 File:       ResizeableStringFormatter.h
 
-    Contains:   Derived from StringFormatter, this transparently grows the
-                output buffer if the original buffer is too small to hold all
-                the data being placed in it
-    
-    
+	 Contains:   Derived from StringFormatter, this transparently grows the
+				 output buffer if the original buffer is too small to hold all
+				 the data being placed in it
 
-*/
+
+
+ */
 
 #ifndef __RESIZEABLE_STRING_FORMATTER_H__
 #define __RESIZEABLE_STRING_FORMATTER_H__
@@ -40,24 +40,24 @@
 
 class ResizeableStringFormatter : public StringFormatter
 {
-    public:
-        // Pass in inBuffer=NULL and inBufSize=0 to dynamically allocate the initial buffer.
-        ResizeableStringFormatter(char* inBuffer = NULL, UInt32 inBufSize = 0)
-            : StringFormatter(inBuffer, inBufSize), fOriginalBuffer(inBuffer) {}
-        
-        //If we've been forced to increase the buffer size, fStartPut WILL be a dynamically allocated
-        //buffer, and it WON'T be equal to fOriginalBuffer (obviously).
-        virtual ~ResizeableStringFormatter() {  if (fStartPut != fOriginalBuffer) delete [] fStartPut; }
+public:
+	// Pass in inBuffer=NULL and inBufSize=0 to dynamically allocate the initial buffer.
+	ResizeableStringFormatter(char* inBuffer = NULL, UInt32 inBufSize = 0)
+		: StringFormatter(inBuffer, inBufSize), fOriginalBuffer(inBuffer) {}
 
-    private:
-        
-        // This function will get called by StringFormatter if the current
-        // output buffer is full. This object allocates a buffer that's twice
-        // as big as the old one.
-        virtual Bool16    BufferIsFull(char* inBuffer, UInt32 inBufferLen);
-        
-        char*           fOriginalBuffer;
-        
+	//If we've been forced to increase the buffer size, fStartPut WILL be a dynamically allocated
+	//buffer, and it WON'T be equal to fOriginalBuffer (obviously).
+	virtual ~ResizeableStringFormatter() { if (fStartPut != fOriginalBuffer) delete[] fStartPut; }
+
+private:
+
+	// This function will get called by StringFormatter if the current
+	// output buffer is full. This object allocates a buffer that's twice
+	// as big as the old one.
+	virtual Bool16    BufferIsFull(char* inBuffer, UInt32 inBufferLen);
+
+	char*           fOriginalBuffer;
+
 };
 
 #endif //__RESIZEABLE_STRING_FORMATTER_H__
