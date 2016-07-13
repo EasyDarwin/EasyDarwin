240b821c7e3ca32d87f466661ee93dae6954ecb6
Alex <cai6811376@gmail.com>
2016-07-13 12:14:49 +0800

    Update QTSSErrorLogModule

--- EasyCMS/Server.tproj/QTSSErrorLogModule.h
+++ EasyCMS/Server.tproj/QTSSErrorLogModule.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,24 +18,24 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
-	Github: https://github.com/EasyDarwin
-	WEChat: EasyDarwin
-	Website: http://www.EasyDarwin.org
-*/
-/*
-    File:       QTSSErrorLogModule.h
+ /*
+     Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+     Github: https://github.com/EasyDarwin
+     WEChat: EasyDarwin
+     Website: http://www.EasyDarwin.org
+ */
+ /*
+     File:       QTSSErrorLogModule.h
 
-    Contains:   A module that uses QTSSRollingLog to write error messages to a file
+     Contains:   A module that uses QTSSRollingLog to write error messages to a file
 
 
 
-*/
+ */
 
 #ifndef __QTSS_ERROR_LOG_MODULE_H__
 #define __QTSS_ERROR_LOG_MODULE_H__
