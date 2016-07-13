cfbd491c0280dda8d8eb98e32636f92eda955851
Alex <cai6811376@gmail.com>
2016-07-13 19:58:04 +0800

    Update sdpCache

--- CommonUtilitiesLib/sdpCache.cpp
+++ CommonUtilitiesLib/sdpCache.cpp
@@ -2,85 +2,86 @@
 #include <map>
 #include <string>
 #include <time.h>
+
 using namespace std;
+
 CSdpCache* CSdpCache::cache = NULL;
 
 typedef struct sdpCache_Tag
 {
-    unsigned long long date;
-    string context;
-}SdpCache;
-static map<string,SdpCache> sdpmap;
+	unsigned long long date;
+	string context;
+} SdpCache;
+
+static map<string, SdpCache> sdpmap;
+
 CSdpCache* CSdpCache::GetInstance()
 {
-    if(cache == NULL)
-    {
-        cache = new CSdpCache();
-    }
-    return cache;
+	if (cache == NULL)
+	{
+		cache = new CSdpCache();
+	}
+	return cache;
 }
 
-void CSdpCache::setSdpMap(char* path,char* context)
+void CSdpCache::setSdpMap(char* path, char* context)
 {
-    if(path == NULL || context == NULL)
-    {
-        return ;
-    }
-    SdpCache cache = {0};
-    cache.date = time(NULL);
-    cache.context = string(context);
+	if (path == NULL || context == NULL)
+	{
+		return;
+	}
+	SdpCache cache = { 0 };
+	cache.date = time(NULL);
+	cache.context = string(context);
 
-    sdpmap[string(path)] = cache;
+	sdpmap[string(path)] = cache;
 }
-    
+
 char* CSdpCache::getSdpMap(char* path)
 {
-    map<string,SdpCache>::iterator it = sdpmap.find(string(path));
-    if(it == sdpmap.end())
-    {
-        return NULL;
-    }
-    
-    return (char*)it->second.context.c_str();
+	map<string, SdpCache>::iterator it = sdpmap.find(string(path));
+	if (it == sdpmap.end())
+	{
+		return NULL;
+	}
+
+	return (char*)it->second.context.c_str();
 }
 
 bool CSdpCache::eraseSdpMap(char* path)
 {
-    map<string,SdpCache>::iterator it = sdpmap.find(string(path));
-    if(it == sdpmap.end())
-    {
-        return true;
-    }
-    sdpmap.erase(it);
-    return true;
+	map<string, SdpCache>::iterator it = sdpmap.find(string(path));
+	if (it == sdpmap.end())
+	{
+		return true;
+	}
+	sdpmap.erase(it);
+	return true;
 }
 
 unsigned long long CSdpCache::getSdpCacheDate(char *path)
 {
-    unsigned long long date = 0;
-    int length = 0;
-    map<string,SdpCache>::iterator it = sdpmap.find(string(path));
-    if(it == sdpmap.end())
-    {
-        return NULL;
-    }
-    
-    date = it->second.date;
-    return date;
+	unsigned long long date = 0;
+	int length = 0;
+	map<string, SdpCache>::iterator it = sdpmap.find(string(path));
+	if (it == sdpmap.end())
+	{
+		return NULL;
+	}
+
+	date = it->second.date;
+	return date;
 }
 
-int CSdpCache::getSdpCacheLen(char *path)
+int CSdpCache::getSdpCacheLen(char* path)
 {
-    int length = 0;
-    map<string,SdpCache>::iterator it = sdpmap.find(string(path));
-    if(it == sdpmap.end())
-    {
-        return 0;
-    }
-    
-    length = it->second.context.size();
-    return length;
+	int length = 0;
+	map<string, SdpCache>::iterator it = sdpmap.find(string(path));
+	if (it == sdpmap.end())
+	{
+		return 0;
+	}
+
+	length = it->second.context.size();
+	return length;
 }
-
-
-
--- CommonUtilitiesLib/sdpCache.h
+++ CommonUtilitiesLib/sdpCache.h
@@ -1,29 +1,29 @@
 #ifndef __SDPCACHE_H__
 #define __SDPCACHE_H__
+
 #include <stdio.h>
+
 class CSdpCache
 {
 private:
 	static CSdpCache* cache;
 	CSdpCache()
-	{
-
-	}
+	{}
 public:
 	~CSdpCache()
 	{}
 
 	static CSdpCache* GetInstance();
 
-	void setSdpMap(char* path,char* context);
-		
+	void setSdpMap(char* path, char* context);
+
 	char* getSdpMap(char* path);
 
 	bool eraseSdpMap(char* path);
 
-	unsigned long long getSdpCacheDate(char *path);
+	unsigned long long getSdpCacheDate(char* path);
 
-	int getSdpCacheLen(char *path);
+	int getSdpCacheLen(char* path);
 
 };
 #endif
