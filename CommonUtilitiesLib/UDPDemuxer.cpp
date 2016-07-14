eb303db372d706ed01076c1e10d332aad51abccb
Alex <cai6811376@gmail.com>
2016-07-14 12:22:32 +0800

    Update UDPDemuxer

--- CommonUtilitiesLib/UDPDemuxer.cpp
+++ CommonUtilitiesLib/UDPDemuxer.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,18 +18,18 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       UDPDemuxer.cpp
+ /*
+	 File:       UDPDemuxer.cpp
 
-    Contains:   Implements objects defined in UDPDemuxer.h
+	 Contains:   Implements objects defined in UDPDemuxer.h
 
-    
 
-*/
+
+ */
 
 #include "UDPDemuxer.h"
 
@@ -37,35 +37,35 @@
 
 
 OS_Error UDPDemuxer::RegisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
-                                        UDPDemuxerTask *inTaskP)
+	UDPDemuxerTask* inTaskP)
 {
-    Assert(NULL != inTaskP);
-    OSMutexLocker locker(&fMutex);
-    if (this->GetTask(inRemoteAddr, inRemotePort) != NULL)
-        return EPERM;
-    inTaskP->Set(inRemoteAddr, inRemotePort);
-    fHashTable.Add(inTaskP);
-    return OS_NoErr;
+	Assert(NULL != inTaskP);
+	OSMutexLocker locker(&fMutex);
+	if (this->GetTask(inRemoteAddr, inRemotePort) != NULL)
+		return EPERM;
+	inTaskP->set(inRemoteAddr, inRemotePort);
+	fHashTable.Add(inTaskP);
+	return OS_NoErr;
 }
 
 OS_Error UDPDemuxer::UnregisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
-                                            UDPDemuxerTask *inTaskP)
+	UDPDemuxerTask* inTaskP)
 {
-    OSMutexLocker locker(&fMutex);
-    //remove by executing a lookup based on key information
-    UDPDemuxerTask* theTask = this->GetTask(inRemoteAddr, inRemotePort);
+	OSMutexLocker locker(&fMutex);
+	//remove by executing a lookup based on key information
+	UDPDemuxerTask* theTask = this->GetTask(inRemoteAddr, inRemotePort);
 
-    if ((NULL != theTask) && (theTask == inTaskP))
-    {
-        fHashTable.Remove(theTask);
-        return OS_NoErr;
-    }
-    else
-        return EPERM;
+	if ((NULL != theTask) && (theTask == inTaskP))
+	{
+		fHashTable.Remove(theTask);
+		return OS_NoErr;
+	}
+	else
+		return EPERM;
 }
 
 UDPDemuxerTask* UDPDemuxer::GetTask(UInt32 inRemoteAddr, UInt16 inRemotePort)
 {
-    UDPDemuxerKey theKey(inRemoteAddr, inRemotePort);
-    return fHashTable.Map(&theKey);
+	UDPDemuxerKey theKey(inRemoteAddr, inRemotePort);
+	return fHashTable.Map(&theKey);
 }
--- CommonUtilitiesLib/UDPDemuxer.h
+++ CommonUtilitiesLib/UDPDemuxer.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       UDPDemuxer.h
+ /*
+	 File:       UDPDemuxer.h
 
-    Contains:   Provides a "Listener" socket for UDP. Blocks on a local IP & port,
-                waiting for data. When it gets data, it passes it off to a UDPDemuxerTask
-                object depending on where it came from.
+	 Contains:   Provides a "Listener" socket for UDP. Blocks on a local IP & port,
+				 waiting for data. When it gets data, it passes it off to a UDPDemuxerTask
+				 object depending on where it came from.
 
-    
-*/
+
+ */
 
 #ifndef __UDPDEMUXER_H__
 #define __UDPDEMUXER_H__
@@ -48,86 +48,91 @@ class UDPDemuxerKey;
 
 class UDPDemuxerUtils
 {
-    private:
-    
-        static UInt32 ComputeHashValue(UInt32 inRemoteAddr, UInt16 inRemotePort)
-            { return ((inRemoteAddr << 16) + inRemotePort); }
-            
-    friend class UDPDemuxerTask;
-    friend class UDPDemuxerKey;
+private:
+
+	static UInt32 ComputeHashValue(UInt32 inRemoteAddr, UInt16 inRemotePort)
+	{
+		return ((inRemoteAddr << 16) + inRemotePort);
+	}
+
+	friend class UDPDemuxerTask;
+	friend class UDPDemuxerKey;
 };
 
 class UDPDemuxerTask
 {
-    public:
-    
-        UDPDemuxerTask()
-            :   fRemoteAddr(0), fRemotePort(0),
-                fHashValue(0), fNextHashEntry(NULL) {}
-        virtual ~UDPDemuxerTask() {}
-        
-        UInt32  GetRemoteAddr() { return fRemoteAddr; }
-        
-    private:
+public:
 
-        void Set(UInt32 inRemoteAddr, UInt16 inRemotePort)
-            {   fRemoteAddr = inRemoteAddr; fRemotePort = inRemotePort;
-                fHashValue = UDPDemuxerUtils::ComputeHashValue(fRemoteAddr, fRemotePort);
-            }
-        
-        //key values
-        UInt32 fRemoteAddr;
-        UInt16 fRemotePort;
-        
-        //precomputed for performance
-        UInt32 fHashValue;
-        
-        UDPDemuxerTask  *fNextHashEntry;
+	UDPDemuxerTask()
+		: fRemoteAddr(0), fRemotePort(0),
+		fHashValue(0), fNextHashEntry(NULL) {}
+	virtual ~UDPDemuxerTask() {}
 
-        friend class UDPDemuxerKey;
-        friend class UDPDemuxer;
-        friend class OSHashTable<UDPDemuxerTask,UDPDemuxerKey>;
+	UInt32  GetRemoteAddr() { return fRemoteAddr; }
+
+private:
+
+	void set(UInt32 inRemoteAddr, UInt16 inRemotePort)
+	{
+		fRemoteAddr = inRemoteAddr; fRemotePort = inRemotePort;
+		fHashValue = UDPDemuxerUtils::ComputeHashValue(fRemoteAddr, fRemotePort);
+	}
+
+	//key values
+	UInt32 fRemoteAddr;
+	UInt16 fRemotePort;
+
+	//precomputed for performance
+	UInt32 fHashValue;
+
+	UDPDemuxerTask  *fNextHashEntry;
+
+	friend class UDPDemuxerKey;
+	friend class UDPDemuxer;
+	friend class OSHashTable<UDPDemuxerTask, UDPDemuxerKey>;
 };
 
 
 
 class UDPDemuxerKey
 {
-    private:
+private:
 
-        //CONSTRUCTOR / DESTRUCTOR:
-        UDPDemuxerKey(UInt32 inRemoteAddr, UInt16 inRemotePort)
-            :   fRemoteAddr(inRemoteAddr), fRemotePort(inRemotePort)
-                { fHashValue = UDPDemuxerUtils::ComputeHashValue(inRemoteAddr, inRemotePort); }
-                
-        ~UDPDemuxerKey() {}
-        
-        
-    private:
+	//CONSTRUCTOR / DESTRUCTOR:
+	UDPDemuxerKey(UInt32 inRemoteAddr, UInt16 inRemotePort)
+		: fRemoteAddr(inRemoteAddr), fRemotePort(inRemotePort)
+	{
+		fHashValue = UDPDemuxerUtils::ComputeHashValue(inRemoteAddr, inRemotePort);
+	}
 
-        //PRIVATE ACCESSORS:    
-        UInt32      GetHashKey()        { return fHashValue; }
+	~UDPDemuxerKey() {}
 
-        //these functions are only used by the hash table itself. This constructor
-        //will break the "Set" functions.
-        UDPDemuxerKey(UDPDemuxerTask *elem) :   fRemoteAddr(elem->fRemoteAddr),
-                                                fRemotePort(elem->fRemotePort), 
-                                                fHashValue(elem->fHashValue) {}
-                                            
-        friend int operator ==(const UDPDemuxerKey &key1, const UDPDemuxerKey &key2) {
-            if ((key1.fRemoteAddr == key2.fRemoteAddr) &&
-                (key1.fRemotePort == key2.fRemotePort))
-                return true;
-            return false;
-        }
-        
-        //data:
-        UInt32 fRemoteAddr;
-        UInt16 fRemotePort;
-        UInt32  fHashValue;
 
-        friend class OSHashTable<UDPDemuxerTask,UDPDemuxerKey>;
-        friend class UDPDemuxer;
+private:
+
+	//PRIVATE ACCESSORS:    
+	UInt32      getHashKey() { return fHashValue; }
+
+	//these functions are only used by the hash table itself. This constructor
+	//will break the "Set" functions.
+	UDPDemuxerKey(UDPDemuxerTask *elem) : fRemoteAddr(elem->fRemoteAddr),
+		fRemotePort(elem->fRemotePort),
+		fHashValue(elem->fHashValue) {}
+
+	friend int operator ==(const UDPDemuxerKey &key1, const UDPDemuxerKey &key2) {
+		if ((key1.fRemoteAddr == key2.fRemoteAddr) &&
+			(key1.fRemotePort == key2.fRemotePort))
+			return true;
+		return false;
+	}
+
+	//data:
+	UInt32 fRemoteAddr;
+	UInt16 fRemotePort;
+	UInt32  fHashValue;
+
+	friend class OSHashTable<UDPDemuxerTask, UDPDemuxerKey>;
+	friend class UDPDemuxer;
 };
 
 //CLASSES USED ONLY IN IMPLEMENTATION
@@ -135,40 +140,42 @@ typedef OSHashTable<UDPDemuxerTask, UDPDemuxerKey> UDPDemuxerHashTable;
 
 class UDPDemuxer
 {
-    public:
+public:
 
-        UDPDemuxer() : fHashTable(kMaxHashTableSize), fMutex() {}
-        ~UDPDemuxer() {}
+	UDPDemuxer() : fHashTable(kMaxHashTableSize), fMutex() {}
+	~UDPDemuxer() {}
 
-        //These functions grab the mutex and are therefore premptive safe
-        
-        // Return values: OS_NoErr, or EPERM if there is already a task registered
-        // with this address combination
-        OS_Error RegisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
-                                        UDPDemuxerTask *inTaskP);
+	//These functions grab the mutex and are therefore premptive safe
 
-        // Return values: OS_NoErr, or EPERM if this task / address combination
-        // is not registered
-        OS_Error UnregisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
-                                        UDPDemuxerTask *inTaskP);
-        
-        //Assumes that parent has grabbed the mutex!
-        UDPDemuxerTask* GetTask(UInt32 inRemoteAddr, UInt16 inRemotePort);
+	// Return values: OS_NoErr, or EPERM if there is already a task registered
+	// with this address combination
+	OS_Error RegisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
+		UDPDemuxerTask *inTaskP);
 
-        Bool16  AddrInMap(UInt32 inRemoteAddr, UInt16 inRemotePort)
-                    { return (this->GetTask(inRemoteAddr, inRemotePort) != NULL); }
-                    
-        OSMutex*                GetMutex()      { return &fMutex; }
-        UDPDemuxerHashTable*    GetHashTable()  { return &fHashTable; }
-        
-    private:
-    
-        enum
-        {
-            kMaxHashTableSize = 2747//is this prime? it should be... //UInt32
-        };
-        UDPDemuxerHashTable fHashTable;
-        OSMutex             fMutex;//this data structure is shared!
+	// Return values: OS_NoErr, or EPERM if this task / address combination
+	// is not registered
+	OS_Error UnregisterTask(UInt32 inRemoteAddr, UInt16 inRemotePort,
+		UDPDemuxerTask *inTaskP);
+
+	//Assumes that parent has grabbed the mutex!
+	UDPDemuxerTask* GetTask(UInt32 inRemoteAddr, UInt16 inRemotePort);
+
+	Bool16  AddrInMap(UInt32 inRemoteAddr, UInt16 inRemotePort)
+	{
+		return (this->GetTask(inRemoteAddr, inRemotePort) != NULL);
+	}
+
+	OSMutex*                GetMutex() { return &fMutex; }
+	UDPDemuxerHashTable*    GetHashTable() { return &fHashTable; }
+
+private:
+
+	enum
+	{
+		kMaxHashTableSize = 2747//is this prime? it should be... //UInt32
+	};
+	UDPDemuxerHashTable fHashTable;
+	OSMutex             fMutex;//this data structure is shared!
 };
 
 #endif // __UDPDEMUXER_H__
