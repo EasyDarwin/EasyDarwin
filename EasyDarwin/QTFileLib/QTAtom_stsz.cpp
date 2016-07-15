da2e3f64b9c178348d5744aa290347c8ef9c5efb
Alex <cai6811376@gmail.com>
2016-07-15 16:22:46 +0800

    Update QTAtom_stsz

--- EasyDarwin/QTFileLib/QTAtom_stsz.cpp
+++ EasyDarwin/QTFileLib/QTAtom_stsz.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stsz.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_stsz:
-//   The 'stsz' QTAtom class.
+ // $Id: QTAtom_stsz.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_stsz:
+ //   The 'stsz' QTAtom class.
 
 
-// -------------------------------------
-// Includes
-//
+ // -------------------------------------
+ // Includes
+ //
 #include <stdio.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -49,10 +49,10 @@
 // -------------------------------------
 // Constants
 //
-const int       stszPos_VersionFlags        =  0;
-const int       stszPos_SampleSize          =  4;
-const int       stszPos_NumEntries          =  8;
-const int       stszPos_SampleTable         = 12;
+const int       stszPos_VersionFlags = 0;
+const int       stszPos_SampleSize = 4;
+const int       stszPos_NumEntries = 8;
+const int       stszPos_SampleTable = 12;
 
 
 
@@ -68,18 +68,18 @@ const int       stszPos_SampleTable         = 12;
 // Constructors and destructors
 //
 QTAtom_stsz::QTAtom_stsz(QTFile * File, QTFile::AtomTOCEntry * TOCEntry, Bool16 Debug, Bool16 DeepDebug)
-    : QTAtom(File, TOCEntry, Debug, DeepDebug),
-      fCommonSampleSize(0),
-      fNumEntries(0), fSampleSizeTable(NULL), fTable(NULL)
+	: QTAtom(File, TOCEntry, Debug, DeepDebug),
+	fCommonSampleSize(0),
+	fNumEntries(0), fSampleSizeTable(NULL), fTable(NULL)
 {
 }
 
-QTAtom_stsz::~QTAtom_stsz(void)
+QTAtom_stsz::~QTAtom_stsz()
 {
-    //
-    // Free our variables.
-    if( fSampleSizeTable != NULL )
-        delete[] fSampleSizeTable;
+	//
+	// Free our variables.
+	if (fSampleSizeTable != NULL)
+		delete[] fSampleSizeTable;
 }
 
 
@@ -87,119 +87,119 @@ QTAtom_stsz::~QTAtom_stsz(void)
 // -------------------------------------
 // Initialization functions
 //
-Bool16 QTAtom_stsz::Initialize(void)
+Bool16 QTAtom_stsz::Initialize()
 {
-    // Temporary vars
-    UInt32      tempInt32;
+	// Temporary vars
+	UInt32      tempInt32;
 
 
-    //
-    // Parse this atom's fields.
-    ReadInt32(stszPos_VersionFlags, &tempInt32);
-    fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
-    fFlags = tempInt32 & 0x00ffffff;
+	//
+	// Parse this atom's fields.
+	ReadInt32(stszPos_VersionFlags, &tempInt32);
+	fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
+	fFlags = tempInt32 & 0x00ffffff;
 
-    ReadInt32(stszPos_SampleSize, &fCommonSampleSize);
-    
-    //
-    // We don't need to read in the table (it doesn't exist anyway) if the
-    // SampleSize field is non-zero.
-    if( fCommonSampleSize != 0 )
-        return true;
+	ReadInt32(stszPos_SampleSize, &fCommonSampleSize);
+
+	//
+	// We don't need to read in the table (it doesn't exist anyway) if the
+	// SampleSize field is non-zero.
+	if (fCommonSampleSize != 0)
+		return true;
 
 
-    //
-    // Build the table..
-    ReadInt32(stszPos_NumEntries, &fNumEntries);
+	//
+	// Build the table..
+	ReadInt32(stszPos_NumEntries, &fNumEntries);
 
-    //
-    // Validate the size of the sample table.
-    if( (UInt32)(fNumEntries * 4) != (fTOCEntry.AtomDataLength - 12) )
-        return false;
+	//
+	// Validate the size of the sample table.
+	if ((UInt32)(fNumEntries * 4) != (fTOCEntry.AtomDataLength - 12))
+		return false;
 
-    //
-    // Read in the sample size table.
-    fSampleSizeTable = NEW char[(fNumEntries * 4) + 1];
-    if( fSampleSizeTable == NULL )
-        return false;
-    
-    if( ((PointerSizedInt)fSampleSizeTable & (PointerSizedInt)0x3) == 0)
-        fTable = (UInt32 *)fSampleSizeTable;
-    else
-        fTable = (UInt32 *)(((PointerSizedInt)fSampleSizeTable + 4) & ~((PointerSizedInt)0x3));
-    
-    ReadBytes(stszPos_SampleTable, (char *)fTable, fNumEntries * 4);
+	//
+	// Read in the sample size table.
+	fSampleSizeTable = NEW char[(fNumEntries * 4) + 1];
+	if (fSampleSizeTable == NULL)
+		return false;
 
-    //
-    // This atom has been successfully read in.
-    return true;
+	if (((PointerSizedInt)fSampleSizeTable & (PointerSizedInt)0x3) == 0)
+		fTable = (UInt32 *)fSampleSizeTable;
+	else
+		fTable = (UInt32 *)(((PointerSizedInt)fSampleSizeTable + 4) & ~((PointerSizedInt)0x3));
+
+	ReadBytes(stszPos_SampleTable, (char *)fTable, fNumEntries * 4);
+
+	//
+	// This atom has been successfully read in.
+	return true;
 }
 
 Bool16 QTAtom_stsz::SampleRangeSize(UInt32 firstSampleNumber, UInt32 lastSampleNumber, UInt32 *sizePtr)
-{   
-    Bool16 result = false;
-    
+{
+	Bool16 result = false;
 
-    do 
-    {
-        if (lastSampleNumber < firstSampleNumber) 
-        {
-//          qtss_printf("QTAtom_stsz::SampleRangeSize (lastSampleNumber %" _S32BITARG_ " < firstSampleNumber %" _S32BITARG_ ") \n",lastSampleNumber, firstSampleNumber);
-            break;
-        }
-        
-        if(fCommonSampleSize) 
-        { 
-//          qtss_printf("QTAtom_stsz::SampleRangeSize fCommonSampleSize %" _S32BITARG_ " firstSampleNumber %" _S32BITARG_ " lastSampleNumber %" _S32BITARG_ " *sizePtr %" _S32BITARG_ "\n",fCommonSampleSize,firstSampleNumber,lastSampleNumber,*sizePtr);
-            if( sizePtr != NULL ) 
-                *sizePtr = fCommonSampleSize * (lastSampleNumber - firstSampleNumber + 1) ; 
-                
-            result =  true; 
-            break;
-        } 
-                                    
-        if(firstSampleNumber && lastSampleNumber && (lastSampleNumber<=fNumEntries) && (firstSampleNumber <= fNumEntries) ) 
-        {
-            if( sizePtr != NULL ) 
-            {   *sizePtr = 0;
-                
-                for (UInt32 sampleNumber = firstSampleNumber; sampleNumber <= lastSampleNumber; sampleNumber++ ) 
-                    *sizePtr += ntohl(fTable[sampleNumber-1]);
-                
-            }
-            result =  true; 
-            break;
-        }
-    
-    } 
-    while (false);
-                            
-    return result; 
+
+	do
+	{
+		if (lastSampleNumber < firstSampleNumber)
+		{
+			//          qtss_printf("QTAtom_stsz::SampleRangeSize (lastSampleNumber %" _S32BITARG_ " < firstSampleNumber %" _S32BITARG_ ") \n",lastSampleNumber, firstSampleNumber);
+			break;
+		}
+
+		if (fCommonSampleSize)
+		{
+			//          qtss_printf("QTAtom_stsz::SampleRangeSize fCommonSampleSize %" _S32BITARG_ " firstSampleNumber %" _S32BITARG_ " lastSampleNumber %" _S32BITARG_ " *sizePtr %" _S32BITARG_ "\n",fCommonSampleSize,firstSampleNumber,lastSampleNumber,*sizePtr);
+			if (sizePtr != NULL)
+				*sizePtr = fCommonSampleSize * (lastSampleNumber - firstSampleNumber + 1);
+
+			result = true;
+			break;
+		}
+
+		if (firstSampleNumber && lastSampleNumber && (lastSampleNumber <= fNumEntries) && (firstSampleNumber <= fNumEntries))
+		{
+			if (sizePtr != NULL)
+			{
+				*sizePtr = 0;
+
+				for (UInt32 sampleNumber = firstSampleNumber; sampleNumber <= lastSampleNumber; sampleNumber++)
+					*sizePtr += ntohl(fTable[sampleNumber - 1]);
+
+			}
+			result = true;
+			break;
+		}
+
+	} while (false);
+
+	return result;
 }
 
 // -------------------------------------
 // Debugging functions
 //
-void QTAtom_stsz::DumpAtom(void)
+void QTAtom_stsz::DumpAtom()
 {
-    DEBUG_PRINT(("QTAtom_stsz::DumpAtom - Dumping atom.\n"));
-    DEBUG_PRINT(("QTAtom_stsz::DumpAtom - ..Number of sample size entries: %" _S32BITARG_ "\n", fNumEntries));
+	DEBUG_PRINT(("QTAtom_stsz::DumpAtom - Dumping atom.\n"));
+	DEBUG_PRINT(("QTAtom_stsz::DumpAtom - ..Number of sample size entries: %" _S32BITARG_ "\n", fNumEntries));
 }
 
-void QTAtom_stsz::DumpTable(void)
+void QTAtom_stsz::DumpTable()
 {
-    //
-    // Print out a header.
-    qtss_printf("-- Sample Size table -----------------------------------------------------------\n");
-    qtss_printf("\n");
-    qtss_printf("  Sample Num   SampleSize\n");
-    qtss_printf("  ----------   ----------\n");
-    
-    //
-    // Print the table.
-    for( UInt32 CurEntry = 1; CurEntry <= fNumEntries; CurEntry++ ) {
-        //
-        // Print out a listing.
-        qtss_printf("  %10"   _U32BITARG_   " : %10"   _U32BITARG_   "\n", CurEntry, fTable[CurEntry-1]);
-    }
+	//
+	// Print out a header.
+	qtss_printf("-- Sample Size table -----------------------------------------------------------\n");
+	qtss_printf("\n");
+	qtss_printf("  Sample Num   SampleSize\n");
+	qtss_printf("  ----------   ----------\n");
+
+	//
+	// Print the table.
+	for (UInt32 CurEntry = 1; CurEntry <= fNumEntries; CurEntry++) {
+		//
+		// Print out a listing.
+		qtss_printf("  %10" _U32BITARG_ " : %10" _U32BITARG_ "\n", CurEntry, fTable[CurEntry - 1]);
+	}
 }
--- EasyDarwin/QTFileLib/QTAtom_stsz.h
+++ EasyDarwin/QTFileLib/QTAtom_stsz.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,14 +18,14 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stsz.h,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_stsz:
-//   The 'stsz' QTAtom class.
+ // $Id: QTAtom_stsz.h,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_stsz:
+ //   The 'stsz' QTAtom class.
 
 #ifndef QTAtom_stsz_H
 #define QTAtom_stsz_H
@@ -42,51 +42,55 @@
 class QTAtom_stsz : public QTAtom {
 
 public:
-    //
-    // Constructors and destructor.
-                        QTAtom_stsz(QTFile * File, QTFile::AtomTOCEntry * Atom,
-                               Bool16 Debug = false, Bool16 DeepDebug = false);
-    virtual             ~QTAtom_stsz(void);
+	//
+	// Constructors and destructor.
+	QTAtom_stsz(QTFile * File, QTFile::AtomTOCEntry * Atom,
+		Bool16 Debug = false, Bool16 DeepDebug = false);
+	virtual             ~QTAtom_stsz();
 
 
-    //
-    // Initialization functions.
-    virtual Bool16      Initialize(void);
-    
-    //
-    // Accessors.
-    inline  Bool16      SampleSize(UInt32 SampleNumber, UInt32 *Size = NULL) \
-                            {   if(fCommonSampleSize) { \
-                                    if( Size != NULL ) \
-                                        *Size = fCommonSampleSize; \
-                                    return true; \
-                                } else if(SampleNumber && (SampleNumber<=fNumEntries)) { \
-                                    if( Size != NULL ) \
-                                        *Size = ntohl(fTable[SampleNumber-1]); \
-                                    return true; \
-                                } else \
-                                    return false; \
-                            };
+	//
+	// Initialization functions.
+	virtual Bool16      Initialize();
 
-    Bool16      SampleRangeSize(UInt32 firstSampleNumber, UInt32 lastSampleNumber, UInt32 *sizePtr);
-    
-    //
-    // Debugging functions.
-    virtual void        DumpAtom(void);
-    virtual void        DumpTable(void);
+	//
+	// Accessors.
+	inline  Bool16      SampleSize(UInt32 SampleNumber, UInt32 *Size = NULL) \
+	{   if (fCommonSampleSize) {
+		\
+			if (Size != NULL) \
+				*Size = fCommonSampleSize; \
+				return true; \
+	}
+	else if (SampleNumber && (SampleNumber <= fNumEntries)) {
+		\
+			if (Size != NULL) \
+				*Size = ntohl(fTable[SampleNumber - 1]); \
+				return true; \
+	}
+	else \
+		return false; \
+	};
 
-    inline  UInt32      GetNumEntries() {return fNumEntries;}
-    inline  UInt32      GetCommonSampleSize() {return fCommonSampleSize;}
+	Bool16      SampleRangeSize(UInt32 firstSampleNumber, UInt32 lastSampleNumber, UInt32 *sizePtr);
+
+	//
+	// Debugging functions.
+	virtual void        DumpAtom();
+	virtual void        DumpTable();
+
+	inline  UInt32      GetNumEntries() { return fNumEntries; }
+	inline  UInt32      GetCommonSampleSize() { return fCommonSampleSize; }
 
 protected:
-    //
-    // Protected member variables.
-    UInt8       fVersion;
-    UInt32      fFlags; // 24 bits in the low 3 bytes
-    UInt32      fCommonSampleSize;
-    UInt32      fNumEntries;
-    char        *fSampleSizeTable;
-    UInt32      *fTable; // longword-aligned version of the above
+	//
+	// Protected member variables.
+	UInt8       fVersion;
+	UInt32      fFlags; // 24 bits in the low 3 bytes
+	UInt32      fCommonSampleSize;
+	UInt32      fNumEntries;
+	char        *fSampleSizeTable;
+	UInt32      *fTable; // longword-aligned version of the above
 };
 
 #endif // QTAtom_stsz_H
