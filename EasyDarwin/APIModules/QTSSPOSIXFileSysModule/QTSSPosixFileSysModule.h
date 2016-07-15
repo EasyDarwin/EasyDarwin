0c45ba6fd8c3afe093daa3b51b69173c5b9a053a
Alex <cai6811376@gmail.com>
2016-07-15 18:19:34 +0800

    Update EasyDarwin QTSSPosixFileSysModule

--- EasyDarwin/APIModules/QTSSPOSIXFileSysModule/QTSSPosixFileSysModule.h
+++ EasyDarwin/APIModules/QTSSPOSIXFileSysModule/QTSSPosixFileSysModule.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,32 +18,31 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       QTSSPosixFileSysModule.h
+ /*
+	 File:       QTSSPosixFileSysModule.h
 
-    Contains:   
-    
-    Written By: Denis Serenyi
+	 Contains:
 
-    
+	 Written By: Denis Serenyi
 
-*/
+
+
+ */
 
 #ifndef __QTSSPOSIXFILESYSMODULE_H__
 #define __QTSSPOSIXFILESYSMODULE_H__
 
 #include "QTSS.h"
-#include "QTSS_Private.h"   // This module MUST be compiled directly into the server!
-                            // This is because it uses the server's private internal event
-                            // mechanism for doing async file I/O
+ // This is because it uses the server's private internal event
+ // mechanism for doing async file I/O
 
 extern "C"
 {
-    EXPORT QTSS_Error QTSSPosixFileSysModule_Main(void* inPrivateArgs);
+	EXPORT QTSS_Error QTSSPosixFileSysModule_Main(void* inPrivateArgs);
 }
 
 #endif // __QTSSPOSIXFILESYSMODULE_H__
