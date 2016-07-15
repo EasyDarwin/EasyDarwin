7e6dcdf9d67ef840c25ec2e9246d10d3d9f09117
Alex <cai6811376@gmail.com>
2016-07-15 16:19:25 +0800

    Update QTAtom_stsd

--- EasyDarwin/QTFileLib/QTAtom_stsd.cpp
+++ EasyDarwin/QTFileLib/QTAtom_stsd.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stsd.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_stsd:
-//   The 'stsd' QTAtom class.
+ // $Id: QTAtom_stsd.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_stsd:
+ //   The 'stsd' QTAtom class.
 
 
-// -------------------------------------
-// Includes
-//
+ // -------------------------------------
+ // Includes
+ //
 #include <stdio.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -50,13 +50,13 @@
 // -------------------------------------
 // Constants
 //
-const int       stsdPos_VersionFlags        =  0;
-const int       stsdPos_NumEntries          =  4;
-const int       stsdPos_SampleTable         =  8;
+const int       stsdPos_VersionFlags = 0;
+const int       stsdPos_NumEntries = 4;
+const int       stsdPos_SampleTable = 8;
 
-const int       stsdDescPos_Size            =  0;
-const int       stsdDescPos_DataFormat      =  4;
-const int       stsdDescPos_Index           = 14;
+const int       stsdDescPos_Size = 0;
+const int       stsdDescPos_DataFormat = 4;
+const int       stsdDescPos_Index = 14;
 
 
 
@@ -72,19 +72,19 @@ const int       stsdDescPos_Index           = 14;
 // Constructors and destructors
 //
 QTAtom_stsd::QTAtom_stsd(QTFile * File, QTFile::AtomTOCEntry * TOCEntry, Bool16 Debug, Bool16 DeepDebug)
-    : QTAtom(File, TOCEntry, Debug, DeepDebug),
-      fNumEntries(0), fSampleDescriptionTable(NULL), fTable(NULL)
+	: QTAtom(File, TOCEntry, Debug, DeepDebug),
+	fNumEntries(0), fSampleDescriptionTable(NULL), fTable(NULL)
 {
 }
 
-QTAtom_stsd::~QTAtom_stsd(void)
+QTAtom_stsd::~QTAtom_stsd()
 {
-    //
-    // Free our variables.
-    if( fSampleDescriptionTable != NULL )
-        delete[] fSampleDescriptionTable;
-    if( fTable != NULL )
-        delete[] fTable;
+	//
+	// Free our variables.
+	if (fSampleDescriptionTable != NULL)
+		delete[] fSampleDescriptionTable;
+	if (fTable != NULL)
+		delete[] fTable;
 }
 
 
@@ -92,65 +92,66 @@ QTAtom_stsd::~QTAtom_stsd(void)
 // -------------------------------------
 // Initialization functions
 //
-Bool16 QTAtom_stsd::Initialize(void)
+Bool16 QTAtom_stsd::Initialize()
 {
-    // Temporary vars
-    UInt32      tempInt32;
+	// Temporary vars
+	UInt32      tempInt32;
 
-    // General vars
-    char        *pSampleDescriptionTable;
+	// General vars
+	char        *pSampleDescriptionTable;
 
 
-    //
-    // Parse this atom's fields.
-    ReadInt32(stsdPos_VersionFlags, &tempInt32);
-    fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
-    fFlags = tempInt32 & 0x00ffffff;
+	//
+	// Parse this atom's fields.
+	ReadInt32(stsdPos_VersionFlags, &tempInt32);
+	fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
+	fFlags = tempInt32 & 0x00ffffff;
 
-    ReadInt32(stsdPos_NumEntries, &fNumEntries);
+	ReadInt32(stsdPos_NumEntries, &fNumEntries);
 
-    //
-    // Read in all of the sample descriptions.
-    if( fNumEntries > 0 ) {
-        //
-        // Allocate our description tables.
-        UInt64 tableSize = fTOCEntry.AtomDataLength - 8;
+	//
+	// Read in all of the sample descriptions.
+	if (fNumEntries > 0) {
+		//
+		// Allocate our description tables.
+		UInt64 tableSize = fTOCEntry.AtomDataLength - 8;
 		Assert(tableSize < kSInt32_Max);
-        fSampleDescriptionTable = NEW char[ (SInt32) tableSize];
-        if( fSampleDescriptionTable == NULL )
-            return false;
-        
-        fTable = NEW char *[fNumEntries];
-        if( fTable == NULL )
-            return false;
-        
-        //
-        // Read in the sample description table.
-        ReadBytes(stsdPos_SampleTable, fSampleDescriptionTable, (UInt32) tableSize);
+		fSampleDescriptionTable = NEW char[(SInt32)tableSize];
+		if (fSampleDescriptionTable == NULL)
+			return false;
 
-        //
-        // Read them all in..
-        pSampleDescriptionTable = fSampleDescriptionTable;
-        char        *maxSampleDescriptionPtr = pSampleDescriptionTable + tableSize;
-        for( UInt32 CurDesc = 0; CurDesc < fNumEntries; CurDesc++ ) {
-            //
-            // Associate this entry in our Table with the actual location of
-            // this sample description.
-            fTable[CurDesc] = pSampleDescriptionTable;
-            
-            //
-            // Skip over this mini-atom.
-            memcpy(&tempInt32, pSampleDescriptionTable, 4);
-            pSampleDescriptionTable += ntohl(tempInt32);
-            if (pSampleDescriptionTable > maxSampleDescriptionPtr)
-            {   return false;
-            }
-        }
-    }
+		fTable = NEW char *[fNumEntries];
+		if (fTable == NULL)
+			return false;
 
-    //
-    // This atom has been successfully read in.
-    return true;
+		//
+		// Read in the sample description table.
+		ReadBytes(stsdPos_SampleTable, fSampleDescriptionTable, (UInt32)tableSize);
+
+		//
+		// Read them all in..
+		pSampleDescriptionTable = fSampleDescriptionTable;
+		char        *maxSampleDescriptionPtr = pSampleDescriptionTable + tableSize;
+		for (UInt32 CurDesc = 0; CurDesc < fNumEntries; CurDesc++) {
+			//
+			// Associate this entry in our Table with the actual location of
+			// this sample description.
+			fTable[CurDesc] = pSampleDescriptionTable;
+
+			//
+			// Skip over this mini-atom.
+			memcpy(&tempInt32, pSampleDescriptionTable, 4);
+			pSampleDescriptionTable += ntohl(tempInt32);
+			if (pSampleDescriptionTable > maxSampleDescriptionPtr)
+			{
+				return false;
+			}
+		}
+	}
+
+	//
+	// This atom has been successfully read in.
+	return true;
 }
 
 
@@ -160,54 +161,54 @@ Bool16 QTAtom_stsd::Initialize(void)
 //
 Bool16 QTAtom_stsd::FindSampleDescription(OSType DataFormat, char ** Buffer, UInt32 * Length)
 {
-    // Temporary vars
-    UInt32      tempInt32;
-    
-    
-    //
-    // Go through all of the sample descriptions, looking for the requested
-    // entry.
-    for( UInt32 CurDesc = 0; CurDesc < fNumEntries; CurDesc++ ) {
-        //
-        // Get this entry's data format.
-        memcpy(&tempInt32, fTable[CurDesc] + stsdDescPos_DataFormat, 4);
-        tempInt32 = ntohl(tempInt32);
+	// Temporary vars
+	UInt32      tempInt32;
 
-        //
-        // Skip this entry if it does not match.
-        if( DataFormat != tempInt32 )
-            continue;
-        
-        //
-        // We found a match; return it.
-        *Buffer = fTable[CurDesc];
 
-        memcpy(&tempInt32, fTable[CurDesc] + stsdDescPos_Size, 4);
-        *Length = ntohl(tempInt32);
+	//
+	// Go through all of the sample descriptions, looking for the requested
+	// entry.
+	for (UInt32 CurDesc = 0; CurDesc < fNumEntries; CurDesc++) {
+		//
+		// Get this entry's data format.
+		memcpy(&tempInt32, fTable[CurDesc] + stsdDescPos_DataFormat, 4);
+		tempInt32 = ntohl(tempInt32);
 
-        return true;
-    }
-    
-    //
-    // No match was found.
-    return false;
+		//
+		// Skip this entry if it does not match.
+		if (DataFormat != tempInt32)
+			continue;
+
+		//
+		// We found a match; return it.
+		*Buffer = fTable[CurDesc];
+
+		memcpy(&tempInt32, fTable[CurDesc] + stsdDescPos_Size, 4);
+		*Length = ntohl(tempInt32);
+
+		return true;
+	}
+
+	//
+	// No match was found.
+	return false;
 }
 
 UInt16 QTAtom_stsd::SampleDescriptionToDataReference(UInt32 SampleDescriptionID)
 {
-    // Temporary vars
-    UInt16      tempInt16;
-    
-    
-    //
-    // Validate our arguments.
-    if( SampleDescriptionID > fNumEntries )
-        return 1;
-    
-    //
-    // Find and return the given sample's data reference index.
-    memcpy(&tempInt16, fTable[SampleDescriptionID - 1] + stsdDescPos_Index, 2);
-    return ntohs(tempInt16);
+	// Temporary vars
+	UInt16      tempInt16;
+
+
+	//
+	// Validate our arguments.
+	if (SampleDescriptionID > fNumEntries)
+		return 1;
+
+	//
+	// Find and return the given sample's data reference index.
+	memcpy(&tempInt16, fTable[SampleDescriptionID - 1] + stsdDescPos_Index, 2);
+	return ntohs(tempInt16);
 }
 
 
@@ -215,8 +216,8 @@ UInt16 QTAtom_stsd::SampleDescriptionToDataReference(UInt32 SampleDescriptionID)
 // -------------------------------------
 // Debugging functions
 //
-void QTAtom_stsd::DumpAtom(void)
+void QTAtom_stsd::DumpAtom()
 {
-    DEBUG_PRINT(("QTAtom_stsd::DumpAtom - Dumping atom.\n"));
-    DEBUG_PRINT(("QTAtom_stsd::DumpAtom - ..Number of sample description entries: %" _S32BITARG_ "\n", fNumEntries));
+	DEBUG_PRINT(("QTAtom_stsd::DumpAtom - Dumping atom.\n"));
+	DEBUG_PRINT(("QTAtom_stsd::DumpAtom - ..Number of sample description entries: %" _S32BITARG_ "\n", fNumEntries));
 }
--- EasyDarwin/QTFileLib/QTAtom_stsd.h
+++ EasyDarwin/QTFileLib/QTAtom_stsd.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,14 +18,14 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stsd.h,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_stsd:
-//   The 'stsd' QTAtom class.
+ // $Id: QTAtom_stsd.h,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_stsd:
+ //   The 'stsd' QTAtom class.
 
 #ifndef QTAtom_stsd_H
 #define QTAtom_stsd_H
@@ -42,38 +42,38 @@
 class QTAtom_stsd : public QTAtom {
 
 public:
-    //
-    // Constructors and destructor.
-                        QTAtom_stsd(QTFile * File, QTFile::AtomTOCEntry * Atom,
-                               Bool16 Debug = false, Bool16 DeepDebug = false);
-    virtual             ~QTAtom_stsd(void);
+	//
+	// Constructors and destructor.
+	QTAtom_stsd(QTFile * File, QTFile::AtomTOCEntry * Atom,
+		Bool16 Debug = false, Bool16 DeepDebug = false);
+	virtual             ~QTAtom_stsd();
 
 
-    //
-    // Initialization functions.
-    virtual Bool16      Initialize(void);
-    
-    //
-    // Accessors.
-    Bool16      FindSampleDescription(OSType DataFormat, char ** Buffer, UInt32 * Length);
-    UInt16      SampleDescriptionToDataReference(UInt32 SampleDescriptionID);
+	//
+	// Initialization functions.
+	virtual Bool16      Initialize();
+
+	//
+	// Accessors.
+	Bool16      FindSampleDescription(OSType DataFormat, char ** Buffer, UInt32 * Length);
+	UInt16      SampleDescriptionToDataReference(UInt32 SampleDescriptionID);
 
 
-    //
-    // Debugging functions.
-    virtual void        DumpAtom(void);
+	//
+	// Debugging functions.
+	virtual void        DumpAtom();
 
 
 protected:
-    //
-    // Protected member variables.
-    UInt8       fVersion;
-    UInt32      fFlags; // 24 bits in the low 3 bytes
+	//
+	// Protected member variables.
+	UInt8       fVersion;
+	UInt32      fFlags; // 24 bits in the low 3 bytes
 
-    UInt32      fNumEntries;
-    char        *fSampleDescriptionTable;
-    char        **fTable; // each entry points to the start of a sample
-                          // description entry in the above memory area
+	UInt32      fNumEntries;
+	char        *fSampleDescriptionTable;
+	char        **fTable; // each entry points to the start of a sample
+						  // description entry in the above memory area
 };
 
 #endif // QTAtom_stsd_H
