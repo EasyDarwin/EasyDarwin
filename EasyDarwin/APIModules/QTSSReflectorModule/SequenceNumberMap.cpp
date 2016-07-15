d062e3f99439a0fd55af757bd587adba346bca1b
Alex <cai6811376@gmail.com>
2016-07-15 17:58:33 +0800

    Update EasyDarwin SequenceNumberMap

--- EasyDarwin/APIModules/QTSSReflectorModule/SequenceNumberMap.cpp
+++ EasyDarwin/APIModules/QTSSReflectorModule/SequenceNumberMap.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       SequenceNumberMap.cpp
+ /*
+	 File:       SequenceNumberMap.cpp
 
-    Contains:   Implements object defined in SequenceNumberMap.h.
-                    
-    
-    
+	 Contains:   Implements object defined in SequenceNumberMap.h.
 
-*/
+
+
+
+ */
 
 #include <string.h>
 #include "MyAssert.h"
@@ -39,115 +39,115 @@
 #include "SequenceNumberMap.h"
 
 SequenceNumberMap::SequenceNumberMap(UInt32 inSlidingWindowSize)
-:   fSlidingWindow(NULL),
-    fWindowSize( (SInt32) inSlidingWindowSize),
-    fNegativeWindowSize( (SInt32) inSlidingWindowSize - (SInt32) (2 * inSlidingWindowSize)),
-    fHighestSeqIndex(0),
-    fHighestSeqNumber(0)
+	: fSlidingWindow(NULL),
+	fWindowSize((SInt32)inSlidingWindowSize),
+	fNegativeWindowSize((SInt32)inSlidingWindowSize - (SInt32)(2 * inSlidingWindowSize)),
+	fHighestSeqIndex(0),
+	fHighestSeqNumber(0)
 {
-    Assert(fNegativeWindowSize < 0);
-    Assert(fWindowSize < 32768);//AddSequenceNumber makes this assumption
-    
+	Assert(fNegativeWindowSize < 0);
+	Assert(fWindowSize < 32768);//AddSequenceNumber makes this assumption
+
 }
 
 Bool16 SequenceNumberMap::AddSequenceNumber(UInt16 inSeqNumber)
 {
-    // Returns whether sequence number has already been added.
-    
-    //Check to see if object has been initialized
-    if (fSlidingWindow == NULL)
-    {
-        fSlidingWindow = NEW Bool16[fWindowSize + 1];
-        ::memset(fSlidingWindow, 0, fWindowSize * sizeof(Bool16));
-        fHighestSeqIndex = 0;
-        fHighestSeqNumber = inSeqNumber;
-    }
+	// Returns whether sequence number has already been added.
 
-    // First check to see if this sequence number is so far below the highest sequence number
-    // we can't even put it in the sliding window.
+	//Check to see if object has been initialized
+	if (fSlidingWindow == NULL)
+	{
+		fSlidingWindow = NEW Bool16[fWindowSize + 1];
+		::memset(fSlidingWindow, 0, fWindowSize * sizeof(Bool16));
+		fHighestSeqIndex = 0;
+		fHighestSeqNumber = inSeqNumber;
+	}
 
-    SInt16 theWindowOffset = inSeqNumber - fHighestSeqNumber;
-    
-    if (theWindowOffset < fNegativeWindowSize)
-        return false;//We don't know, but for safety, assume we haven't seen it.
-        
-    // If this seq # is higher thn the highest previous, set the highest to be this
-    // new sequence number, and zero out our sliding window as we go.
-    
-    while (theWindowOffset > 0)
-    {
-        fHighestSeqNumber++;
+	// First check to see if this sequence number is so far below the highest sequence number
+	// we can't even put it in the sliding window.
 
-        fHighestSeqIndex++;
-        if (fHighestSeqIndex == fWindowSize)
-            fHighestSeqIndex = 0;
-        fSlidingWindow[fHighestSeqIndex] = false;
-            
-        theWindowOffset--;
-    }
+	SInt16 theWindowOffset = inSeqNumber - fHighestSeqNumber;
 
-    // Find the right entry in the sliding window for this sequence number, taking
-    // into account that we may need to wrap.
-    
-    SInt32 theWindowIndex = fHighestSeqIndex + theWindowOffset;
-    if (theWindowIndex < 0)
-        theWindowIndex += fWindowSize;
-        
-    Assert(theWindowIndex >= 0);
-    Assert(theWindowIndex < fWindowSize);
-        
-    // Turn this index on, return whether it was already turned on.
-    Bool16 alreadyAdded = fSlidingWindow[theWindowIndex];
-    fSlidingWindow[theWindowIndex] = true;
+	if (theWindowOffset < fNegativeWindowSize)
+		return false;//We don't know, but for safety, assume we haven't seen it.
+
+	// If this seq # is higher thn the highest previous, set the highest to be this
+	// new sequence number, and zero out our sliding window as we go.
+
+	while (theWindowOffset > 0)
+	{
+		fHighestSeqNumber++;
+
+		fHighestSeqIndex++;
+		if (fHighestSeqIndex == fWindowSize)
+			fHighestSeqIndex = 0;
+		fSlidingWindow[fHighestSeqIndex] = false;
+
+		theWindowOffset--;
+	}
+
+	// Find the right entry in the sliding window for this sequence number, taking
+	// into account that we may need to wrap.
+
+	SInt32 theWindowIndex = fHighestSeqIndex + theWindowOffset;
+	if (theWindowIndex < 0)
+		theWindowIndex += fWindowSize;
+
+	Assert(theWindowIndex >= 0);
+	Assert(theWindowIndex < fWindowSize);
+
+	// Turn this index on, return whether it was already turned on.
+	Bool16 alreadyAdded = fSlidingWindow[theWindowIndex];
+	fSlidingWindow[theWindowIndex] = true;
 #if SEQUENCENUMBERMAPTESTING
-    //if (alreadyAdded)
-    //  qtss_printf("Found a duplicate seq num. Num = %d\n", inSeqNumber);
+	//if (alreadyAdded)
+	//  qtss_printf("Found a duplicate seq num. Num = %d\n", inSeqNumber);
 #endif
-    return alreadyAdded;
+	return alreadyAdded;
 }
 
 #if SEQUENCENUMBERMAPTESTING
 void SequenceNumberMap::Test()
 {
-    SequenceNumberMap theMap1;
-    Bool16 retval = theMap1.AddSequenceNumber(64674);
-    Assert(retval == false);
-    
-    retval = theMap1.AddSequenceNumber(64582);
-    Assert(retval == false);
-    
-    retval = theMap1.AddSequenceNumber(64777);
-    Assert(retval == false);
-    
-    retval = theMap1.AddSequenceNumber(64582);
-    Assert(retval == TRUE);
-    
-    retval = theMap1.AddSequenceNumber(64674);
-    Assert(retval == TRUE);
-    
-    retval = theMap1.AddSequenceNumber(1);
-    Assert(retval == FALSE);
+	SequenceNumberMap theMap1;
+	Bool16 retval = theMap1.AddSequenceNumber(64674);
+	Assert(retval == false);
 
-    retval = theMap1.AddSequenceNumber(65500);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(64582);
+	Assert(retval == false);
 
-    retval = theMap1.AddSequenceNumber(65500);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(64777);
+	Assert(retval == false);
 
-    retval = theMap1.AddSequenceNumber(32768);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(64582);
+	Assert(retval == TRUE);
 
-    retval = theMap1.AddSequenceNumber(1024);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(64674);
+	Assert(retval == TRUE);
 
-    retval = theMap1.AddSequenceNumber(32757);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(1);
+	Assert(retval == FALSE);
 
-    retval = theMap1.AddSequenceNumber(32799);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(65500);
+	Assert(retval == FALSE);
 
-    retval = theMap1.AddSequenceNumber(32768);
-    Assert(retval == FALSE);
+	retval = theMap1.AddSequenceNumber(65500);
+	Assert(retval == FALSE);
+
+	retval = theMap1.AddSequenceNumber(32768);
+	Assert(retval == FALSE);
+
+	retval = theMap1.AddSequenceNumber(1024);
+	Assert(retval == FALSE);
+
+	retval = theMap1.AddSequenceNumber(32757);
+	Assert(retval == FALSE);
+
+	retval = theMap1.AddSequenceNumber(32799);
+	Assert(retval == FALSE);
+
+	retval = theMap1.AddSequenceNumber(32768);
+	Assert(retval == FALSE);
 
 }
 #endif
--- EasyDarwin/APIModules/QTSSReflectorModule/SequenceNumberMap.h
+++ EasyDarwin/APIModules/QTSSReflectorModule/SequenceNumberMap.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,20 +18,20 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       SequenceNumberMap.h
+ /*
+	 File:       SequenceNumberMap.h
 
-    Contains:   Data structure for keeping track of duplicate sequence numbers.
-                Useful for removing duplicate packets from an RTP stream.
-                    
+	 Contains:   Data structure for keeping track of duplicate sequence numbers.
+				 Useful for removing duplicate packets from an RTP stream.
 
-    
 
-*/
+
+
+ */
 
 #ifndef _SEQUENCE_NUMBER_MAP_H_
 #define _SEQUENCE_NUMBER_MAP_H_
@@ -42,32 +42,32 @@
 
 class SequenceNumberMap
 {
-    public:
-    
-        enum
-        {
-            kDefaultSlidingWindowSize = 256
-        };
-        
-        SequenceNumberMap(UInt32 inSlidingWindowSize = kDefaultSlidingWindowSize);
-        ~SequenceNumberMap() { delete [] fSlidingWindow; }
-        
-        // Returns whether this sequence number was already added or not.
-        Bool16  AddSequenceNumber(UInt16 inSeqNumber);
-        
+public:
+
+	enum
+	{
+		kDefaultSlidingWindowSize = 256
+	};
+
+	SequenceNumberMap(UInt32 inSlidingWindowSize = kDefaultSlidingWindowSize);
+	~SequenceNumberMap() { delete[] fSlidingWindow; }
+
+	// Returns whether this sequence number was already added or not.
+	Bool16  AddSequenceNumber(UInt16 inSeqNumber);
+
 #if SEQUENCENUMBERMAPTESTING
-        static void Test();
+	static void Test();
 #endif
-        
-    private:
-        
-        Bool16*         fSlidingWindow;
 
-        const SInt32    fWindowSize;
-        const SInt32    fNegativeWindowSize;
+private:
 
-        UInt16          fHighestSeqIndex;
-        UInt16          fHighestSeqNumber;
+	Bool16*         fSlidingWindow;
+
+	const SInt32    fWindowSize;
+	const SInt32    fNegativeWindowSize;
+
+	UInt16          fHighestSeqIndex;
+	UInt16          fHighestSeqNumber;
 };
 
 
