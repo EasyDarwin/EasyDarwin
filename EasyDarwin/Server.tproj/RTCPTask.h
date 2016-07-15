1445f6b348035e34ed859b43c74042170b8c02c2
Alex <cai6811376@gmail.com>
2016-07-15 19:16:36 +0800

    Update EasyDarwin RTCPTask

--- EasyDarwin/Server.tproj/RTCPTask.h
+++ EasyDarwin/Server.tproj/RTCPTask.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,18 +18,18 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       RTCPTask.h
+ /*
+	 File:       RTCPTask.h
 
-    Contains:   A task object that processes all incoming RTCP packets
-                for the server, and passes each one onto the task for
-                which it belongs. 
+	 Contains:   A task object that processes all incoming RTCP packets
+				 for the server, and passes each one onto the task for
+				 which it belongs.
 
-*/
+ */
 
 #ifndef __RTCP_TASK_H__
 #define __RTCP_TASK_H__
@@ -38,14 +38,14 @@
 
 class RTCPTask : public Task
 {
-    public:
-        //This task handles all incoming RTCP data. It just polls, so make sure
-        //to start the polling process by signalling a start event.
-        RTCPTask() : Task() {this->SetTaskName("RTCPTask"); this->Signal(Task::kStartEvent); }
-        virtual ~RTCPTask() {}
-    
-    private:
-        virtual SInt64 Run();
+public:
+	//This task handles all incoming RTCP data. It just polls, so make sure
+	//to start the polling process by signalling a start event.
+	RTCPTask() : Task() { this->SetTaskName("RTCPTask"); this->Signal(Task::kStartEvent); }
+	virtual ~RTCPTask() {}
+
+private:
+	virtual SInt64 Run();
 };
 
 #endif //__RTCP_TASK_H__
