5404a91577fa28ce50c5d3b665f4212d227fc3ba
Alex <cai6811376@gmail.com>
2016-07-15 16:20:38 +0800

    Update QTAtom_stss

--- EasyDarwin/QTFileLib/QTAtom_stss.cpp
+++ EasyDarwin/QTFileLib/QTAtom_stss.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stss.cpp,v 1.2 2006/03/29 00:47:00 murata Exp $
-//
-// QTAtom_stss:
-//   The 'stss' QTAtom class.
+ // $Id: QTAtom_stss.cpp,v 1.2 2006/03/29 00:47:00 murata Exp $
+ //
+ // QTAtom_stss:
+ //   The 'stss' QTAtom class.
 
 
-// -------------------------------------
-// Includes
-//
+ // -------------------------------------
+ // Includes
+ //
 #include <stdio.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -49,9 +49,9 @@
 // -------------------------------------
 // Constants
 //
-const int       stssPos_VersionFlags        =  0;
-const int       stssPos_NumEntries          =  4;
-const int       stssPos_SampleTable         =  8;
+const int       stssPos_VersionFlags = 0;
+const int       stssPos_NumEntries = 4;
+const int       stssPos_SampleTable = 8;
 
 
 
@@ -67,21 +67,21 @@ const int       stssPos_SampleTable         =  8;
 // Constructors and destructors
 //
 QTAtom_stss::QTAtom_stss(QTFile * File, QTFile::AtomTOCEntry * TOCEntry, Bool16 Debug, Bool16 DeepDebug)
-    : QTAtom(File, TOCEntry, Debug, DeepDebug),
-      fNumEntries(0), fSyncSampleTable(NULL), fTable(NULL), fTableSize(0)
+	: QTAtom(File, TOCEntry, Debug, DeepDebug),
+	fNumEntries(0), fSyncSampleTable(NULL), fTable(NULL), fTableSize(0)
 {
 }
 
-QTAtom_stss::~QTAtom_stss(void)
+QTAtom_stss::~QTAtom_stss()
 {
-    //
-    // Free our variables.
+	//
+	// Free our variables.
 #if MMAP_TABLES
-    if( fSyncSampleTable != NULL )
-        this->UnMap(fSyncSampleTable, fTableSize);
+	if (fSyncSampleTable != NULL)
+		this->UnMap(fSyncSampleTable, fTableSize);
 #else
-    if( fSyncSampleTable != NULL )
-        delete[] fSyncSampleTable;
+	if (fSyncSampleTable != NULL)
+		delete[] fSyncSampleTable;
 #endif
 }
 
@@ -90,75 +90,75 @@ QTAtom_stss::~QTAtom_stss(void)
 // -------------------------------------
 // Initialization functions
 //
-Bool16 QTAtom_stss::Initialize(void)
+Bool16 QTAtom_stss::Initialize()
 {
-    Bool16      initSucceeds = false;
-    UInt32      tempInt32;
+	Bool16      initSucceeds = false;
+	UInt32      tempInt32;
 
 
-    //
-    // Parse this atom's fields.
-    initSucceeds = ReadInt32(stssPos_VersionFlags, &tempInt32);
-    fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
-    fFlags = tempInt32 & 0x00ffffff;
+	//
+	// Parse this atom's fields.
+	initSucceeds = ReadInt32(stssPos_VersionFlags, &tempInt32);
+	fVersion = (UInt8)((tempInt32 >> 24) & 0x000000ff);
+	fFlags = tempInt32 & 0x00ffffff;
 
-    if ( initSucceeds )
-    {   
-        initSucceeds = ReadInt32(stssPos_NumEntries, &fNumEntries);
+	if (initSucceeds)
+	{
+		initSucceeds = ReadInt32(stssPos_NumEntries, &fNumEntries);
 
-        //
-        // Validate the size of the sample table.
-        if( (UInt32)(fNumEntries * 4) != (fTOCEntry.AtomDataLength - 8) )
-            return false;
+		//
+		// Validate the size of the sample table.
+		if ((UInt32)(fNumEntries * 4) != (fTOCEntry.AtomDataLength - 8))
+			return false;
 
-        
+
 #if 0// MMAP_TABLES needs fixing should be page aligned and maybe on a 64bit system the whole file should be mapped.
-    fTableSize = (fNumEntries * 4);
-    fSyncSampleTable = this->MemMap(stssPos_SampleTable, fTableSize);
-    fTable = (UInt32 *) fSyncSampleTable;
-    if (fSyncSampleTable == NULL)
-        return false;
-        
+		fTableSize = (fNumEntries * 4);
+		fSyncSampleTable = this->MemMap(stssPos_SampleTable, fTableSize);
+		fTable = (UInt32 *)fSyncSampleTable;
+		if (fSyncSampleTable == NULL)
+			return false;
+
 #else
-        //
-        // Read in the sync sample table.
-        fSyncSampleTable = NEW char[(fNumEntries * 4) + 1];
-        if( fSyncSampleTable == NULL )
-            return false;
-        
-        if( ((PointerSizedInt)fSyncSampleTable & (PointerSizedInt)0x3) == 0)
-            fTable = (UInt32 *)fSyncSampleTable;
-        else
-            fTable = (UInt32 *)(((PointerSizedInt)fSyncSampleTable + 4) & ~((PointerSizedInt)0x3));
-        
-        initSucceeds = ReadBytes(stssPos_SampleTable, (char *)fTable, fNumEntries * 4);
- 
+		//
+		// Read in the sync sample table.
+		fSyncSampleTable = NEW char[(fNumEntries * 4) + 1];
+		if (fSyncSampleTable == NULL)
+			return false;
+
+		if (((PointerSizedInt)fSyncSampleTable & (PointerSizedInt)0x3) == 0)
+			fTable = (UInt32 *)fSyncSampleTable;
+		else
+			fTable = (UInt32 *)(((PointerSizedInt)fSyncSampleTable + 4) & ~((PointerSizedInt)0x3));
+
+		initSucceeds = ReadBytes(stssPos_SampleTable, (char *)fTable, fNumEntries * 4);
+
 #endif
 
-        if ( initSucceeds )
-        {
-            // This atom has been successfully read in.
-            // sample offsets are in network byte order on disk, convert them to host order
-            UInt32      sampleIndex = 0;
-            
-            // convert each sample to host order
-            // NOTE - most other Atoms handle byte order conversions in
-            // the accessor function.  For efficiency reasons it's converted
-            // to host order here for sync samples.
-            
-            for ( sampleIndex = 0; sampleIndex < fNumEntries; sampleIndex++ )
-            {
-                fTable[sampleIndex] = ntohl( fTable[sampleIndex] );
-            
-            }
-        
-        }
+		if (initSucceeds)
+		{
+			// This atom has been successfully read in.
+			// sample offsets are in network byte order on disk, convert them to host order
+			UInt32      sampleIndex = 0;
+
+			// convert each sample to host order
+			// NOTE - most other Atoms handle byte order conversions in
+			// the accessor function.  For efficiency reasons it's converted
+			// to host order here for sync samples.
+
+			for (sampleIndex = 0; sampleIndex < fNumEntries; sampleIndex++)
+			{
+				fTable[sampleIndex] = ntohl(fTable[sampleIndex]);
+
+			}
+
+		}
 
 
 
-    }
-    
-    return initSucceeds;
+	}
+
+	return initSucceeds;
 }
 
 
@@ -168,64 +168,64 @@ Bool16 QTAtom_stss::Initialize(void)
 //
 void QTAtom_stss::PreviousSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber)
 {
-    //
-    // We assume that we won't find an answer
-    *SyncSampleNumber = SampleNumber;
-    
-    //
-    // Scan the table until we find a sample number greater than our current
-    // sample number; then return that.
-    for( UInt32 CurEntry = 0; CurEntry < fNumEntries; CurEntry++ ) {
-        //
-        // Take this entry if it is before (or equal to) our current entry.
-        if( fTable[CurEntry] <= SampleNumber )
-            *SyncSampleNumber = fTable[CurEntry];
-    }
+	//
+	// We assume that we won't find an answer
+	*SyncSampleNumber = SampleNumber;
+
+	//
+	// Scan the table until we find a sample number greater than our current
+	// sample number; then return that.
+	for (UInt32 CurEntry = 0; CurEntry < fNumEntries; CurEntry++) {
+		//
+		// Take this entry if it is before (or equal to) our current entry.
+		if (fTable[CurEntry] <= SampleNumber)
+			*SyncSampleNumber = fTable[CurEntry];
+	}
 }
 
 void QTAtom_stss::NextSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber)
 {
-    //
-    // We assume that we won't find an answer
-    *SyncSampleNumber = SampleNumber + 1;
-    
-    //
-    // Scan the table until we find a sample number greater than our current
-    // sample number; then return that.
-    for( UInt32 CurEntry = 0; CurEntry < fNumEntries; CurEntry++ ) {
-        //
-        // Take this entry if it is greater than our current entry.
-        if( fTable[CurEntry] > SampleNumber ) {
-            *SyncSampleNumber = fTable[CurEntry];
-            break;
-        }
-    }
+	//
+	// We assume that we won't find an answer
+	*SyncSampleNumber = SampleNumber + 1;
+
+	//
+	// Scan the table until we find a sample number greater than our current
+	// sample number; then return that.
+	for (UInt32 CurEntry = 0; CurEntry < fNumEntries; CurEntry++) {
+		//
+		// Take this entry if it is greater than our current entry.
+		if (fTable[CurEntry] > SampleNumber) {
+			*SyncSampleNumber = fTable[CurEntry];
+			break;
+		}
+	}
 }
 
 
 // -------------------------------------
 // Debugging functions
 //
-void QTAtom_stss::DumpAtom(void)
+void QTAtom_stss::DumpAtom()
 {
-    DEBUG_PRINT(("QTAtom_stss::DumpAtom - Dumping atom.\n"));
-    DEBUG_PRINT(("QTAtom_stss::DumpAtom - ..Number of sync sample entries: %" _S32BITARG_ "\n", fNumEntries));
+	DEBUG_PRINT(("QTAtom_stss::DumpAtom - Dumping atom.\n"));
+	DEBUG_PRINT(("QTAtom_stss::DumpAtom - ..Number of sync sample entries: %" _S32BITARG_ "\n", fNumEntries));
 }
 
-void QTAtom_stss::DumpTable(void)
+void QTAtom_stss::DumpTable()
 {
-    //
-    // Print out a header.
-    qtss_printf("-- Sync Sample table -----------------------------------------------------------\n");
-    qtss_printf("\n");
-    qtss_printf("  Entry Num.   Sample Num\n");
-    qtss_printf("  ----------   ----------\n");
-    
-    //
-    // Print the table.
-    for( UInt32 CurEntry = 1; CurEntry <= fNumEntries; CurEntry++ ) {
-        //
-        // Print out a listing.
-        qtss_printf("  %10"   _U32BITARG_   " : %10"   _U32BITARG_   "\n", CurEntry, fTable[CurEntry-1]);
-    }
+	//
+	// Print out a header.
+	qtss_printf("-- Sync Sample table -----------------------------------------------------------\n");
+	qtss_printf("\n");
+	qtss_printf("  Entry Num.   Sample Num\n");
+	qtss_printf("  ----------   ----------\n");
+
+	//
+	// Print the table.
+	for (UInt32 CurEntry = 1; CurEntry <= fNumEntries; CurEntry++) {
+		//
+		// Print out a listing.
+		qtss_printf("  %10"   _U32BITARG_   " : %10"   _U32BITARG_   "\n", CurEntry, fTable[CurEntry - 1]);
+	}
 }
--- EasyDarwin/QTFileLib/QTAtom_stss.h
+++ EasyDarwin/QTFileLib/QTAtom_stss.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,14 +18,14 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_stss.h,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_stss:
-//   The 'stss' QTAtom class.
+ // $Id: QTAtom_stss.h,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_stss:
+ //   The 'stss' QTAtom class.
 
 #ifndef QTAtom_stss_H
 #define QTAtom_stss_H
@@ -43,51 +43,51 @@
 class QTAtom_stss : public QTAtom {
 
 public:
-    //
-    // Constructors and destructor.
-                        QTAtom_stss(QTFile * File, QTFile::AtomTOCEntry * Atom,
-                               Bool16 Debug = false, Bool16 DeepDebug = false);
-    virtual             ~QTAtom_stss(void);
+	//
+	// Constructors and destructor.
+	QTAtom_stss(QTFile * File, QTFile::AtomTOCEntry * Atom,
+		Bool16 Debug = false, Bool16 DeepDebug = false);
+	virtual             ~QTAtom_stss();
 
 
-    //
-    // Initialization functions.
-    virtual Bool16      Initialize(void);
-    
-    //
-    // Accessors.
-            void        PreviousSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber);
-            void        NextSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber);
-            inline Bool16       IsSyncSample(UInt32 SampleNumber, UInt32 inCursor)
-            {
-                Assert(inCursor <= fNumEntries);
-                for (UInt32 curEntry = inCursor; curEntry < fNumEntries; curEntry++)
-                {
-                    if (fTable[curEntry] == SampleNumber)
-                        return true;
-                    else if (fTable[curEntry] > SampleNumber)
-                        return false;
-                }
-                return false;
-            }
+	//
+	// Initialization functions.
+	virtual Bool16      Initialize();
+
+	//
+	// Accessors.
+	void        PreviousSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber);
+	void        NextSyncSample(UInt32 SampleNumber, UInt32 *SyncSampleNumber);
+	inline Bool16       IsSyncSample(UInt32 SampleNumber, UInt32 inCursor)
+	{
+		Assert(inCursor <= fNumEntries);
+		for (UInt32 curEntry = inCursor; curEntry < fNumEntries; curEntry++)
+		{
+			if (fTable[curEntry] == SampleNumber)
+				return true;
+			else if (fTable[curEntry] > SampleNumber)
+				return false;
+		}
+		return false;
+	}
 
 
-    //
-    // Debugging functions.
-    virtual void        DumpAtom(void);
-    virtual void        DumpTable(void);
+	//
+	// Debugging functions.
+	virtual void        DumpAtom();
+	virtual void        DumpTable();
 
 
 protected:
-    //
-    // Protected member variables.
-    UInt8       fVersion;
-    UInt32      fFlags; // 24 bits in the low 3 bytes
+	//
+	// Protected member variables.
+	UInt8       fVersion;
+	UInt32      fFlags; // 24 bits in the low 3 bytes
 
-    UInt32      fNumEntries;
-    char        *fSyncSampleTable;
-    UInt32      *fTable; // longword-aligned version of the above
-    UInt32      fTableSize;
+	UInt32      fNumEntries;
+	char        *fSyncSampleTable;
+	UInt32      *fTable; // longword-aligned version of the above
+	UInt32      fTableSize;
 };
 
 #endif // QTAtom_stss_H
