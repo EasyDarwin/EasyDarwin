21ea3654c1be41c9f68ec349dfde1bf95c9e9d44
Alex <cai6811376@gmail.com>
2016-07-15 16:16:54 +0800

    Update QTAtom_stco

--- EasyDarwin/QTFileLib/QTAtom_stco.h
+++ EasyDarwin/QTFileLib/QTAtom_stco.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,13 +18,13 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-//
-// QTAtom_stco:
-//   The 'stco' QTAtom class.
+ //
+ // QTAtom_stco:
+ //   The 'stco' QTAtom class.
 
 #ifndef QTAtom_stco_H
 #define QTAtom_stco_H
@@ -44,52 +44,52 @@
 class QTAtom_stco : public QTAtom {
 
 public:
-    //
-    // Constructors and destructor.
-                        QTAtom_stco(QTFile * File, QTFile::AtomTOCEntry * Atom,
-                        UInt16 offSetSize = 4, Bool16 Debug = false, Bool16 DeepDebug = false);
-    virtual             ~QTAtom_stco(void);
+	//
+	// Constructors and destructor.
+	QTAtom_stco(QTFile * File, QTFile::AtomTOCEntry * Atom,
+		UInt16 offSetSize = 4, Bool16 Debug = false, Bool16 DeepDebug = false);
+	virtual             ~QTAtom_stco();
 
 
-    //
-    // Initialization functions.
-    virtual Bool16      Initialize(void);
-    
-    //
-    // Accessors.
-                            
-    inline  Bool16      ChunkOffset(UInt32 ChunkNumber, UInt64 *Offset = NULL)
-                        {
-                            if (Offset && ChunkNumber && (ChunkNumber<=fNumEntries)) 
-                            {
-                                if (4 == fOffSetSize)
-                                    *Offset = (UInt64) ntohl( ( (UInt32 *) fTable)[ChunkNumber-1]);
-                                else
-                                    *Offset = (UInt64) QTAtom::NTOH64( ( (UInt64 *) fTable)[ChunkNumber-1]);
-                                        
-                                return true;
-                            } 
-                            
-                            return false; 
-                        }
+	//
+	// Initialization functions.
+	virtual Bool16      Initialize();
+
+	//
+	// Accessors.
+
+	inline  Bool16      ChunkOffset(UInt32 ChunkNumber, UInt64 *Offset = NULL)
+	{
+		if (Offset && ChunkNumber && (ChunkNumber <= fNumEntries))
+		{
+			if (4 == fOffSetSize)
+				*Offset = (UInt64)ntohl(((UInt32 *)fTable)[ChunkNumber - 1]);
+			else
+				*Offset = (UInt64)QTAtom::NTOH64(((UInt64 *)fTable)[ChunkNumber - 1]);
+
+			return true;
+		}
+
+		return false;
+	}
 
 
-    //
-    // Debugging functions.
-    virtual void        DumpAtom(void);
-    virtual void        DumpTable(void);
+	//
+	// Debugging functions.
+	virtual void        DumpAtom();
+	virtual void        DumpTable();
 
 
 protected:
-    //
-    // Protected member variables.
-    UInt8       fVersion;
-    UInt32      fFlags; // 24 bits in the low 3 bytes
+	//
+	// Protected member variables.
+	UInt8       fVersion;
+	UInt32      fFlags; // 24 bits in the low 3 bytes
 
-    UInt32      fNumEntries;
-    UInt16      fOffSetSize;
-    char        *fChunkOffsetTable;
-    void        *fTable; // longword-aligned version of the above
+	UInt32      fNumEntries;
+	UInt16      fOffSetSize;
+	char        *fChunkOffsetTable;
+	void        *fTable; // longword-aligned version of the above
 };
 
 #endif // QTAtom_stco_H
