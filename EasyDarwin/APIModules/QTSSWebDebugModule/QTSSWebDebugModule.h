37b11af3a3f17dfd54bade940ec77a80675ad4b4
Alex <cai6811376@gmail.com>
2016-07-15 18:20:41 +0800

    Update EasyDarwin QTSSWebDebugModule

--- EasyDarwin/APIModules/QTSSWebDebugModule/QTSSWebDebugModule.h
+++ EasyDarwin/APIModules/QTSSWebDebugModule/QTSSWebDebugModule.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,21 +18,21 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       QTSSWebDebugModule.h
+ /*
+	 File:       QTSSWebDebugModule.h
 
-    Contains:   A module that uses the debugging information available in the server
-                to present a web page containing that information. Uses the Filter
-                module feature of the QTSS API.
+	 Contains:   A module that uses the debugging information available in the server
+				 to present a web page containing that information. Uses the Filter
+				 module feature of the QTSS API.
 
 
-    
 
-*/
+
+ */
 
 #ifndef __QTSSWEBDEBUGMODULE_H__
 #define __QTSSWEBDEBUGMODULE_H__
@@ -41,7 +41,7 @@
 
 extern "C"
 {
-    EXPORT QTSS_Error QTSSWebDebugModule_Main(void* inPrivateArgs);
+	EXPORT QTSS_Error QTSSWebDebugModule_Main(void* inPrivateArgs);
 }
 
 #endif //__QTSSWEBDEBUGMODULE_H__
