1feb710379a4b406f00409c66d288840320e8864
Alex <cai6811376@gmail.com>
2016-07-15 18:15:14 +0800

    Update EasyDarwin QTSSAccessModule

--- EasyDarwin/APIModules/QTSSAccessModule/QTSSAccessModule.h
+++ EasyDarwin/APIModules/QTSSAccessModule/QTSSAccessModule.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,18 +18,18 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/* 
-    File:       QTSSAccessModule.h
+ /*
+	 File:       QTSSAccessModule.h
 
-    Contains:   Module that handles authentication and authorization independent of the file system
-                    
-    
+	 Contains:   Module that handles authentication and authorization independent of the file system
 
-*/
+
+
+ */
 
 #ifndef _QTSSACCESSMODULE_H_
 #define _QTSSACCESSMODULE_H_
@@ -38,7 +38,7 @@
 
 extern "C"
 {
-    EXPORT QTSS_Error QTSSAccessModule_Main(void* inPrivateArgs);
+	EXPORT QTSS_Error QTSSAccessModule_Main(void* inPrivateArgs);
 }
 
 #endif //_QTSSACCESSMODULE_H_
