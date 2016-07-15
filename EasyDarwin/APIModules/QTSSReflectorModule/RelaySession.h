b7c6d6bfc78738d68c7e33b0905e8a757246ae33
Alex <cai6811376@gmail.com>
2016-07-15 17:54:24 +0800

    Update EasyDarwin RelaySession

--- EasyDarwin/APIModules/QTSSReflectorModule/RelaySession.h
+++ EasyDarwin/APIModules/QTSSReflectorModule/RelaySession.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,72 +18,71 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       RelaySession.h
+ /*
+	 File:       RelaySession.h
 
-    Contains:   Subclass of ReflectorSession. It has two static
-                attributes (QTSSRelayModule Attributes object
-                and the ReflectorSession attribute ID)  
+	 Contains:   Subclass of ReflectorSession. It has two static
+				 attributes (QTSSRelayModule Attributes object
+				 and the ReflectorSession attribute ID)
 
-*/
+ */
+
+#ifndef _RELAY_SESSION_
+#define _RELAY_SESSION_
 
 #include "QTSS.h"
 #include "ReflectorSession.h"
 #include "StrPtrLen.h"
 #include "SourceInfo.h"
-#include "RTSPSourceInfo.h"
-
-#ifndef _RELAY_SESSION_
-#define _RELAY_SESSION_
 
 class RelaySession : public ReflectorSession
 {
-    public:
-        
-        // Call Register in the Relay Module's Register Role
-        static void Register();
-        
-        //
-        // Initialize
-        // Call Initialize in the Relay Module's Initialize Role
-        static void Initialize(QTSS_Object inAttrObject);
-        
-        RelaySession(StrPtrLen* inSourceID, SourceInfo* inInfo = NULL):ReflectorSession(inSourceID, inInfo){};
-        ~RelaySession();
-         
-        QTSS_Error SetupRelaySession(SourceInfo* inInfo);
-        
-        QTSS_Object GetRelaySessionObject() { return fRelaySessionObject; }
-        static QTSS_AttributeID     sRelayOutputObject;
+public:
 
-        static char         sRelayUserAgent[20];
-                
-    private:
-        
-        QTSS_Object                 fRelaySessionObject;
-        
-        // gets set in the initialize method
-        static QTSS_Object          relayModuleAttributesObject;
-        
-        static QTSS_ObjectType      qtssRelaySessionObjectType;
-        
-        static QTSS_AttributeID     sRelaySessionObjectID;
-        static QTSS_AttributeID     sRelayName;
-        static QTSS_AttributeID     sSourceType;
-        static QTSS_AttributeID     sSourceIPAddr;
-        static QTSS_AttributeID     sSourceInIPAddr;
-        static QTSS_AttributeID     sSourceUDPPorts;
-        static QTSS_AttributeID     sSourceRTSPPort;
-        static QTSS_AttributeID     sSourceURL;
-        static QTSS_AttributeID     sSourceUsername;
-        static QTSS_AttributeID     sSourcePassword;
-        static QTSS_AttributeID     sSourceTTL;
-                
-                
+	// Call Register in the Relay Module's Register Role
+	static void Register();
+
+	//
+	// Initialize
+	// Call Initialize in the Relay Module's Initialize Role
+	static void Initialize(QTSS_Object inAttrObject);
+
+	RelaySession(StrPtrLen* inSourceID, SourceInfo* inInfo = NULL) :ReflectorSession(inSourceID, inInfo) {};
+	~RelaySession();
+
+	QTSS_Error SetupRelaySession(SourceInfo* inInfo);
+
+	QTSS_Object GetRelaySessionObject() { return fRelaySessionObject; }
+	static QTSS_AttributeID     sRelayOutputObject;
+
+	static char         sRelayUserAgent[20];
+
+private:
+
+	QTSS_Object                 fRelaySessionObject;
+
+	// gets set in the initialize method
+	static QTSS_Object          relayModuleAttributesObject;
+
+	static QTSS_ObjectType      qtssRelaySessionObjectType;
+
+	static QTSS_AttributeID     sRelaySessionObjectID;
+	static QTSS_AttributeID     sRelayName;
+	static QTSS_AttributeID     sSourceType;
+	static QTSS_AttributeID     sSourceIPAddr;
+	static QTSS_AttributeID     sSourceInIPAddr;
+	static QTSS_AttributeID     sSourceUDPPorts;
+	static QTSS_AttributeID     sSourceRTSPPort;
+	static QTSS_AttributeID     sSourceURL;
+	static QTSS_AttributeID     sSourceUsername;
+	static QTSS_AttributeID     sSourcePassword;
+	static QTSS_AttributeID     sSourceTTL;
+
+
 };
 
 #endif
