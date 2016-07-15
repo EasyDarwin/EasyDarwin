b566112f87c511bc3cb88fa8337388eeca8de55b
Alex <cai6811376@gmail.com>
2016-07-15 16:28:27 +0800

    Update QTFile

--- EasyDarwin/QTFileLib/QTFile.h
+++ EasyDarwin/QTFileLib/QTFile.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,14 +18,14 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTFile.h,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTFile:
-//   The central point of control for a file in the QuickTime File Format.
+ // $Id: QTFile.h,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTFile:
+ //   The central point of control for a file in the QuickTime File Format.
 
 #ifndef QTFile_H
 #define QTFile_H
@@ -51,165 +51,165 @@ class QTTrack;
 class QTFile {
 
 public:
-    //
-    // Class constants
-    
-    
-    //
-    // Class error codes
-    enum ErrorCode {
-        errNoError                  = 0,
-        errFileNotFound             = 1,
-        errInvalidQuickTimeFile     = 2,
-        errInternalError            = 100
-    };
+	//
+	// Class constants
 
 
-    //
-    // Class typedefs.
-    struct AtomTOCEntry {
-        // TOC id (used to compare TOCs)
-        UInt32          TOCID;
+	//
+	// Class error codes
+	enum ErrorCode {
+		errNoError = 0,
+		errFileNotFound = 1,
+		errInvalidQuickTimeFile = 2,
+		errInternalError = 100
+	};
 
-        // Atom information
-        OSType          AtomType, beAtomType; // be = Big Endian
 
-        UInt64          AtomDataPos;
-        UInt64          AtomDataLength;
-        UInt32          AtomHeaderSize;
+	//
+	// Class typedefs.
+	struct AtomTOCEntry {
+		// TOC id (used to compare TOCs)
+		UInt32          TOCID;
 
-        // TOC pointers
-        AtomTOCEntry    *NextOrdAtom;
+		// Atom information
+		OSType          AtomType, beAtomType; // be = Big Endian
 
-        AtomTOCEntry    *PrevAtom, *NextAtom;
-        AtomTOCEntry    *Parent, *FirstChild;
-    };
+		UInt64          AtomDataPos;
+		UInt64          AtomDataLength;
+		UInt32          AtomHeaderSize;
 
-    struct TrackListEntry {
-        // Track information
-        UInt32          TrackID;
-        QTTrack         *Track;
-        Bool16          IsHintTrack;
+		// TOC pointers
+		AtomTOCEntry    *NextOrdAtom;
 
-        // List pointers
-        TrackListEntry  *NextTrack;
-    };
+		AtomTOCEntry    *PrevAtom, *NextAtom;
+		AtomTOCEntry    *Parent, *FirstChild;
+	};
+
+	struct TrackListEntry {
+		// Track information
+		UInt32          TrackID;
+		QTTrack         *Track;
+		Bool16          IsHintTrack;
+
+		// List pointers
+		TrackListEntry  *NextTrack;
+	};
 
 
 public:
-    //
-    // Constructors and destructor.
-                        QTFile(Bool16 Debug = false, Bool16 DeepDebug = false);
-    virtual             ~QTFile(void);
+	//
+	// Constructors and destructor.
+	QTFile(Bool16 Debug = false, Bool16 DeepDebug = false);
+	virtual             ~QTFile();
 
 
-    //
-    // Open a movie file and generate the atom table of contents.
-            ErrorCode   Open(const char * MoviePath);
-            
-            OSMutex*    GetMutex() { return fReadMutex; }
+	//
+	// Open a movie file and generate the atom table of contents.
+	ErrorCode   Open(const char * MoviePath);
 
-    //
-    // Table of Contents functions.
-            Bool16      FindTOCEntry(const char * AtomPath,
-                                     AtomTOCEntry **TOCEntry,
-                                     AtomTOCEntry *LastFoundTOCEntry = NULL);
-    
-    //
-    // Track List functions
-    inline  UInt32      GetNumTracks(void) { return fNumTracks; }
-            Bool16      NextTrack(QTTrack **Track, QTTrack *LastFoundTrack = NULL);
-            Bool16      FindTrack(UInt32 TrackID, QTTrack **Track);
-            Bool16      IsHintTrack(QTTrack *Track);
-    
-    //
-    // Accessors
-    inline  char *      GetMoviePath(void) { return fMoviePath; }
-            Float64     GetTimeScale(void);
-            Float64     GetDurationInSeconds(void);
-            SInt64      GetModDate();
-            // Returns the mod date as a RFC 1123 formatted string
-            char*       GetModDateStr();
-    //
-    // Read functions.
-            Bool16      Read(UInt64 Offset, char * const Buffer, UInt32 Length, QTFile_FileControlBlock * FCB = NULL);
-    
+	OSMutex*    GetMutex() { return fReadMutex; }
 
-            void        AllocateBuffers(UInt32 inUnitSizeInK, UInt32 inBufferInc, UInt32 inBufferSize, UInt32 inMaxBitRateBuffSizeInBlocks, UInt32 inBitrate);
+	//
+	// Table of Contents functions.
+	Bool16      FindTOCEntry(const char * AtomPath,
+		AtomTOCEntry **TOCEntry,
+		AtomTOCEntry *LastFoundTOCEntry = NULL);
+
+	//
+	// Track List functions
+	inline  UInt32      GetNumTracks() { return fNumTracks; }
+	Bool16      NextTrack(QTTrack **Track, QTTrack *LastFoundTrack = NULL);
+	Bool16      FindTrack(UInt32 TrackID, QTTrack **Track);
+	Bool16      IsHintTrack(QTTrack *Track);
+
+	//
+	// Accessors
+	inline  char *      GetMoviePath() { return fMoviePath; }
+	Float64     GetTimeScale(void);
+	Float64     GetDurationInSeconds();
+	SInt64      GetModDate();
+	// Returns the mod date as a RFC 1123 formatted string
+	char*       GetModDateStr();
+	//
+	// Read functions.
+	Bool16      Read(UInt64 Offset, char * const Buffer, UInt32 Length, QTFile_FileControlBlock * FCB = NULL);
+
+
+	void        AllocateBuffers(UInt32 inUnitSizeInK, UInt32 inBufferInc, UInt32 inBufferSize, UInt32 inMaxBitRateBuffSizeInBlocks, UInt32 inBitrate);
 #if DSS_USE_API_CALLBACKS
-            void        IncBufferUserCount() {if (fOSFileSourceFD != NULL) fOSFileSourceFD->IncMaxBuffers();}
-            void        DecBufferUserCount() {if (fOSFileSourceFD != NULL) fOSFileSourceFD->DecMaxBuffers();}
+	void        IncBufferUserCount() { if (fOSFileSourceFD != NULL) fOSFileSourceFD->IncMaxBuffers(); }
+	void        DecBufferUserCount() { if (fOSFileSourceFD != NULL) fOSFileSourceFD->DecMaxBuffers(); }
 #else
-            void        IncBufferUserCount() {fMovieFD.IncMaxBuffers();}
-            void        DecBufferUserCount() {fMovieFD.DecMaxBuffers();}
+	void        IncBufferUserCount() { fMovieFD.IncMaxBuffers(); }
+	void        DecBufferUserCount() { fMovieFD.DecMaxBuffers(); }
 #endif
 
-    inline Bool16       ValidTOC();
-            
-            
-            char*      MapFileToMem(UInt64 offset, UInt32 length);
-            
-            int         UnmapMem(char *memPtr, UInt32 length);
+	inline Bool16       ValidTOC();
 
-    //
-    // Debugging functions.
-            void        DumpAtomTOC(void);
+
+	char*      MapFileToMem(UInt64 offset, UInt32 length);
+
+	int         UnmapMem(char *memPtr, UInt32 length);
+
+	//
+	// Debugging functions.
+	void        DumpAtomTOC();
 
 protected:
-    //
-    // Protected member functions.
-            Bool16      GenerateAtomTOC(void);
-    
-    //
-    // Protected member variables.
-    Bool16              fDebug, fDeepDebug;
+	//
+	// Protected member functions.
+	Bool16      GenerateAtomTOC();
 
-    UInt32              fNextTOCID;
+	//
+	// Protected member variables.
+	Bool16              fDebug, fDeepDebug;
+
+	UInt32              fNextTOCID;
 #if DSS_USE_API_CALLBACKS
-    QTSS_Object         fMovieFD;
-    OSFileSource        *fOSFileSourceFD;
+	QTSS_Object         fMovieFD;
+	OSFileSource        *fOSFileSourceFD;
 #else
-    OSFileSource        fMovieFD;
+	OSFileSource        fMovieFD;
 #endif
-    Bool16              fCacheBuffersSet;
-    
-    DateBuffer          fModDateBuffer;
-    char                *fMoviePath;
+	Bool16              fCacheBuffersSet;
 
-    AtomTOCEntry        *fTOC, *fTOCOrdHead, *fTOCOrdTail;
-    
-    UInt32              fNumTracks;
-    TrackListEntry      *fFirstTrack, *fLastTrack;
+	DateBuffer          fModDateBuffer;
+	char                *fMoviePath;
 
-    QTAtom_mvhd         *fMovieHeaderAtom;
-    
-    OSMutex             *fReadMutex;
-    int                  fFile;
-                        
+	AtomTOCEntry        *fTOC, *fTOCOrdHead, *fTOCOrdTail;
+
+	UInt32              fNumTracks;
+	TrackListEntry      *fFirstTrack, *fLastTrack;
+
+	QTAtom_mvhd         *fMovieHeaderAtom;
+
+	OSMutex             *fReadMutex;
+	int                  fFile;
+
 };
 
 Bool16 QTFile::ValidTOC()
 {
-    UInt64 theLength = 0;
-    UInt64 thePos = 0;
+	UInt64 theLength = 0;
+	UInt64 thePos = 0;
 
 #if DSS_USE_API_CALLBACKS
-    UInt32 theDataLen = sizeof(UInt64);
-    (void)QTSS_GetValue(fMovieFD, qtssFlObjLength, 0, (void*)&theLength, &theDataLen);
-    (void)QTSS_GetValue(fMovieFD, qtssFlObjPosition, 0, (void*)&thePos, &theDataLen);
-//  qtss_printf("GenerateAtomTOC failed CurPos=%"_64BITARG_"u < Length=%"_64BITARG_"u\n", CurPos, theLength);
+	UInt32 theDataLen = sizeof(UInt64);
+	(void)QTSS_GetValue(fMovieFD, qtssFlObjLength, 0, (void*)&theLength, &theDataLen);
+	(void)QTSS_GetValue(fMovieFD, qtssFlObjPosition, 0, (void*)&thePos, &theDataLen);
+	//  qtss_printf("GenerateAtomTOC failed CurPos=%"_64BITARG_"u < Length=%"_64BITARG_"u\n", CurPos, theLength);
 #else
-    theLength = fMovieFD.GetLength();
-    thePos = fMovieFD.GetCurOffset();
+	theLength = fMovieFD.GetLength();
+	thePos = fMovieFD.GetCurOffset();
 #endif
 
-    if (thePos < theLength) // failure pos not at end of file
-    {  
-//      qtss_printf("GenerateAtomTOC failed CurPos=%"_64BITARG_"u < Length=%"_64BITARG_"u\n", CurPos, theLength);
-        return false;
-    }
-    
-    return true;
+	if (thePos < theLength) // failure pos not at end of file
+	{
+		//      qtss_printf("GenerateAtomTOC failed CurPos=%"_64BITARG_"u < Length=%"_64BITARG_"u\n", CurPos, theLength);
+		return false;
+	}
+
+	return true;
 }
 
 #endif // QTFile_H
