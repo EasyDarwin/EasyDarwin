8927b1ed24f193ad6cd16bad5037fd05584a2a7f
Alex <cai6811376@gmail.com>
2016-07-15 17:52:20 +0800

    Update EasyDarwin RelaySDPSourceInfo

--- EasyDarwin/APIModules/QTSSReflectorModule/RelaySDPSourceInfo.h
+++ EasyDarwin/APIModules/QTSSReflectorModule/RelaySDPSourceInfo.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       RelaySDPSourceInfo.h
+ /*
+	 File:       RelaySDPSourceInfo.h
 
-    Contains:   This object takes input SDP data, and uses it to support the SourceInfo
-                API. It looks for the x-qt-relay lines put in by some broadcasters,
-                and uses that information to construct OutputInfo objects.
+	 Contains:   This object takes input SDP data, and uses it to support the SourceInfo
+				 API. It looks for the x-qt-relay lines put in by some broadcasters,
+				 and uses that information to construct OutputInfo objects.
 
 
-*/
+ */
 
 #ifndef __RELAY_SDP_SOURCE_INFO_H__
 #define __RELAY_SDP_SOURCE_INFO_H__
@@ -40,15 +40,15 @@
 
 class RelaySDPSourceInfo : public SourceInfo
 {
-    public:
-    
-        // Reads in the SDP data from this file, and builds up the SourceInfo structures
-        RelaySDPSourceInfo(StrPtrLen* inSDPData) { Parse(inSDPData); }
-        virtual ~RelaySDPSourceInfo();
-        
-    private:
-        
-        void    Parse(StrPtrLen* inSDPData);
+public:
+
+	// Reads in the SDP data from this file, and builds up the SourceInfo structures
+	RelaySDPSourceInfo(StrPtrLen* inSDPData) { Parse(inSDPData); }
+	virtual ~RelaySDPSourceInfo();
+
+private:
+
+	void    Parse(StrPtrLen* inSDPData);
 };
 #endif // __SDP_SOURCE_INFO_H__
 
