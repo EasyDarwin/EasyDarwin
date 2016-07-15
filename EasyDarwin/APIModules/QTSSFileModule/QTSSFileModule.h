e5925e141d40d4d23cd327119b88b17010c7f803
Alex <cai6811376@gmail.com>
2016-07-15 18:16:32 +0800

    Update EasyDarwin QTSSFileModule

--- EasyDarwin/APIModules/QTSSFileModule/QTSSFileModule.h
+++ EasyDarwin/APIModules/QTSSFileModule/QTSSFileModule.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,18 +18,18 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       QTSSFileModule.h
+ /*
+	 File:       QTSSFileModule.h
 
-    Contains:   Content source module that uses the QTFileLib to serve Hinted QuickTime
-                files to clients. 
-                    
+	 Contains:   Content source module that uses the QTFileLib to serve Hinted QuickTime
+				 files to clients.
 
-*/
+
+ */
 
 #ifndef _RTPFILEMODULE_H_
 #define _RTPFILEMODULE_H_
@@ -38,7 +38,7 @@
 
 extern "C"
 {
-    EXPORT QTSS_Error QTSSFileModule_Main(void* inPrivateArgs);
+	EXPORT QTSS_Error QTSSFileModule_Main(void* inPrivateArgs);
 }
 
 #endif //_RTPFILEMODULE_H_
