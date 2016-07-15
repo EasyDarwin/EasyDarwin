ee0a758b22ffbbca6f6f6d4327516c319666fbaf
Alex <cai6811376@gmail.com>
2016-07-15 16:26:45 +0800

    Update QTAtom_tref

--- EasyDarwin/QTFileLib/QTAtom_tref.cpp
+++ EasyDarwin/QTFileLib/QTAtom_tref.cpp
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,19 +18,19 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-// $Id: QTAtom_tref.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
-//
-// QTAtom_tref:
-//   The 'tref' QTAtom class.
+ // $Id: QTAtom_tref.cpp,v 1.1 2006/01/05 13:20:36 murata Exp $
+ //
+ // QTAtom_tref:
+ //   The 'tref' QTAtom class.
 
 
-// -------------------------------------
-// Includes
-//
+ // -------------------------------------
+ // Includes
+ //
 #include <stdio.h>
 #include <stdlib.h>
 #include "SafeStdLib.h"
@@ -44,7 +44,7 @@
 // -------------------------------------
 // Constants
 //
-const int       trefPos_SampleTable         =  0;
+const int       trefPos_SampleTable = 0;
 
 
 
@@ -60,17 +60,17 @@ const int       trefPos_SampleTable         =  0;
 // Constructors and destructors
 //
 QTAtom_tref::QTAtom_tref(QTFile * File, QTFile::AtomTOCEntry * TOCEntry, Bool16 Debug, Bool16 DeepDebug)
-    : QTAtom(File, TOCEntry, Debug, DeepDebug),
-      fNumEntries(0), fTrackReferenceTable(NULL), fTable(NULL)
+	: QTAtom(File, TOCEntry, Debug, DeepDebug),
+	fNumEntries(0), fTrackReferenceTable(NULL), fTable(NULL)
 {
 }
 
-QTAtom_tref::~QTAtom_tref(void)
+QTAtom_tref::~QTAtom_tref()
 {
-    //
-    // Free our variables.
-    if( fTrackReferenceTable != NULL )
-        delete[] fTrackReferenceTable;
+	//
+	// Free our variables.
+	if (fTrackReferenceTable != NULL)
+		delete[] fTrackReferenceTable;
 }
 
 
@@ -78,28 +78,28 @@ QTAtom_tref::~QTAtom_tref(void)
 // -------------------------------------
 // Initialization functions
 //
-Bool16 QTAtom_tref::Initialize(void)
+Bool16 QTAtom_tref::Initialize()
 {
-    //
-    // Compute the size of the sample table.
-    fNumEntries = fTOCEntry.AtomDataLength / 4;
+	//
+	// Compute the size of the sample table.
+	fNumEntries = fTOCEntry.AtomDataLength / 4;
 
-    //
-    // Read in the track reference table.
-    fTrackReferenceTable = NEW char[ (SInt32) ((fNumEntries * 4) + 1)];
-    if( fTrackReferenceTable == NULL )
-        return false;
-    
-    if( ((PointerSizedInt)fTrackReferenceTable & (PointerSizedInt)0x3) == 0)
-        fTable = (UInt32 *)fTrackReferenceTable;
-    else
-        fTable = (UInt32 *)(((PointerSizedInt)fTrackReferenceTable + 4) & ~((PointerSizedInt)0x3));
-    
-    ReadBytes(trefPos_SampleTable, (char *)fTable, (UInt32) (fNumEntries * 4) );
+	//
+	// Read in the track reference table.
+	fTrackReferenceTable = NEW char[(SInt32)((fNumEntries * 4) + 1)];
+	if (fTrackReferenceTable == NULL)
+		return false;
 
-    //
-    // This atom has been successfully read in.
-    return true;
+	if (((PointerSizedInt)fTrackReferenceTable & (PointerSizedInt)0x3) == 0)
+		fTable = (UInt32 *)fTrackReferenceTable;
+	else
+		fTable = (UInt32 *)(((PointerSizedInt)fTrackReferenceTable + 4) & ~((PointerSizedInt)0x3));
+
+	ReadBytes(trefPos_SampleTable, (char *)fTable, (UInt32)(fNumEntries * 4));
+
+	//
+	// This atom has been successfully read in.
+	return true;
 }
 
 
@@ -107,8 +107,8 @@ Bool16 QTAtom_tref::Initialize(void)
 // -------------------------------------
 // Debugging functions
 //
-void QTAtom_tref::DumpAtom(void)
+void QTAtom_tref::DumpAtom()
 {
-    DEBUG_PRINT(("QTAtom_tref::DumpAtom - Dumping atom.\n"));
-    DEBUG_PRINT(("QTAtom_tref::DumpAtom - ..Number of track reference entries: %"   _U32BITARG_   "\n", (UInt32) fNumEntries));
+	DEBUG_PRINT(("QTAtom_tref::DumpAtom - Dumping atom.\n"));
+	DEBUG_PRINT(("QTAtom_tref::DumpAtom - ..Number of track reference entries: %"   _U32BITARG_   "\n", (UInt32)fNumEntries));
 }
