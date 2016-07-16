7c083b10f87b51f78c60b51bc4df7a91c71dd32b
Alex <cai6811376@gmail.com>
2016-07-16 13:56:09 +0800

    Update FilePrefsSource

--- EasyDarwin/PrefsSourceLib/FilePrefsSource.h
+++ EasyDarwin/PrefsSourceLib/FilePrefsSource.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,21 +18,21 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       FilePrefsSource.h
+ /*
+	 File:       FilePrefsSource.h
 
-    Contains:   Implements the PrefsSource interface, getting the prefs from a file.
+	 Contains:   Implements the PrefsSource interface, getting the prefs from a file.
 
-    Written by: Chris LeCroy
+	 Written by: Chris LeCroy
 
 
-    
 
-*/
+
+ */
 
 #ifndef __FILEPREFSSOURCE_H__
 #define __FILEPREFSSOURCE_H__
@@ -44,33 +44,33 @@ class KeyValuePair; //only used in the implementation
 
 class FilePrefsSource : public PrefsSource
 {
-    public:
-    
-        FilePrefsSource( Bool16 allowDuplicates = false );
-        virtual ~FilePrefsSource(); 
-    
-        virtual int     GetValue(const char* inKey, char* ioValue);
-        virtual int     GetValueByIndex(const char* inKey, UInt32 inIndex, char* ioValue);
+public:
 
-        // Allows caller to iterate over all the values in the file.
-        char*           GetValueAtIndex(UInt32 inIndex);
-        char*           GetKeyAtIndex(UInt32 inIndex);
-        UInt32          GetNumKeys() { return fNumKeys; }
-        
-        int InitFromConfigFile(const char* configFilePath);
-        void WriteToConfigFile(const char* configFilePath);
+	FilePrefsSource(Bool16 allowDuplicates = false);
+	virtual ~FilePrefsSource();
 
-        void SetValue(const char* inKey, const char* inValue);
-        void DeleteValue(const char* inKey);
+	virtual int     GetValue(const char* inKey, char* ioValue);
+	virtual int     GetValueByIndex(const char* inKey, UInt32 inIndex, char* ioValue);
 
-    private:
-    
-        static Bool16 FilePrefsConfigSetter( const char* paramName, const char* paramValue[], void* userData );
-        
-        KeyValuePair*   FindValue(const char* inKey, char* ioValue, UInt32 index = 0);
-        KeyValuePair*   fKeyValueList;
-        UInt32          fNumKeys;
-        Bool16 fAllowDuplicates;
+	// Allows caller to iterate over all the values in the file.
+	char*           GetValueAtIndex(UInt32 inIndex);
+	char*           GetKeyAtIndex(UInt32 inIndex);
+	UInt32          GetNumKeys() { return fNumKeys; }
+
+	int InitFromConfigFile(const char* configFilePath);
+	void WriteToConfigFile(const char* configFilePath);
+
+	void SetValue(const char* inKey, const char* inValue);
+	void DeleteValue(const char* inKey);
+
+private:
+
+	static Bool16 FilePrefsConfigSetter(const char* paramName, const char* paramValue[], void* userData);
+
+	KeyValuePair*   FindValue(const char* inKey, char* ioValue, UInt32 index = 0);
+	KeyValuePair*   fKeyValueList;
+	UInt32          fNumKeys;
+	Bool16 fAllowDuplicates;
 };
 
 #endif //__FILEPREFSSOURCE_H__
