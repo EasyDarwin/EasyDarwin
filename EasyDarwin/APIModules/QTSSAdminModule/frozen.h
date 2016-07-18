d995de359feed141a472b67760286a0758fced6d
Alex <cai6811376@gmail.com>
2016-07-18 13:46:54 +0800

    Update EasyDarwin frozen

--- EasyDarwin/APIModules/QTSSAdminModule/frozen.h
+++ EasyDarwin/APIModules/QTSSAdminModule/frozen.h
@@ -24,41 +24,42 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#include <stdarg.h>
 
-	enum json_type {
-		JSON_TYPE_EOF = 0,      /* End of parsed tokens marker */
-		JSON_TYPE_STRING = 1,
-		JSON_TYPE_NUMBER = 2,
-		JSON_TYPE_OBJECT = 3,
-		JSON_TYPE_TRUE = 4,
-		JSON_TYPE_FALSE = 5,
-		JSON_TYPE_NULL = 6,
-		JSON_TYPE_ARRAY = 7
-	};
+enum json_type {
+  JSON_TYPE_EOF     = 0,      /* End of parsed tokens marker */
+  JSON_TYPE_STRING  = 1,
+  JSON_TYPE_NUMBER  = 2,
+  JSON_TYPE_OBJECT  = 3,
+  JSON_TYPE_TRUE    = 4,
+  JSON_TYPE_FALSE   = 5,
+  JSON_TYPE_NULL    = 6,
+  JSON_TYPE_ARRAY   = 7
+};
 
-	struct json_token {
-		const char *ptr;      /* Points to the beginning of the token */
-		int len;              /* Token length */
-		int num_desc;         /* For arrays and object, total number of descendants */
-		enum json_type type;  /* Type of the token, possible values above */
-	};
+struct json_token {
+  const char *ptr;      /* Points to the beginning of the token */
+  int len;              /* Token length */
+  int num_desc;         /* For arrays and object, total number of descendants */
+  enum json_type type;  /* Type of the token, possible values above */
+};
 
-	/* Error codes */
+/* Error codes */
 #define JSON_STRING_INVALID           -1
 #define JSON_STRING_INCOMPLETE        -2
 #define JSON_TOKEN_ARRAY_TOO_SMALL    -3
 
-	int parse_json(const char *json_string, int json_string_length,
-		struct json_token *tokens_array, int size_of_tokens_array);
-	struct json_token *parse_json2(const char *json_string, int string_length);
-	struct json_token *find_json_token(struct json_token *toks, const char *path);
+int parse_json(const char *json_string, int json_string_length,
+               struct json_token *tokens_array, int size_of_tokens_array);
+struct json_token *parse_json2(const char *json_string, int string_length);
+struct json_token *find_json_token(struct json_token *toks, const char *path);
 
-	int json_emit_long(char *buf, int buf_len, long value);
-	int json_emit_double(char *buf, int buf_len, double value);
-	int json_emit_quoted_str(char *buf, int buf_len, const char *str, int len);
-	int json_emit_unquoted_str(char *buf, int buf_len, const char *str, int len);
-	int json_emit(char *buf, int buf_len, const char *fmt, ...);
-	int json_emit_va(char *buf, int buf_len, const char *fmt, va_list);
+int json_emit_long(char *buf, int buf_len, long value);
+int json_emit_double(char *buf, int buf_len, double value);
+int json_emit_quoted_str(char *buf, int buf_len, const char *str, int len);
+int json_emit_unquoted_str(char *buf, int buf_len, const char *str, int len);
+int json_emit(char *buf, int buf_len, const char *fmt, ...);
+int json_emit_va(char *buf, int buf_len, const char *fmt, va_list);
 
 #ifdef __cplusplus
 }
