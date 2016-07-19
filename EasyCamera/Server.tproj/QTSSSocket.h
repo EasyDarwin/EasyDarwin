1ba0d51409e34c70fc0aaeecc8cc10e9afa96788
Alex <cai6811376@gmail.com>
2016-07-19 16:29:26 +0800

    Update EasyCamera QTSSSocket

--- EasyCamera/Server.tproj/QTSSSocket.h
+++ EasyCamera/Server.tproj/QTSSSocket.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,17 +18,17 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       QTSSSocket.h
+ /*
+	 File:       QTSSSocket.h
 
-    Contains:   A QTSS Stream object for a generic socket
-    
-    Written By: Denis Serenyi
-*/
+	 Contains:   A QTSS Stream object for a generic socket
+
+	 Written By: Denis Serenyi
+ */
 
 #ifndef __QTSS_SOCKET_H__
 #define __QTSS_SOCKET_H__
@@ -40,18 +40,18 @@
 
 class QTSSSocket : public QTSSStream
 {
-    public:
+public:
 
-        QTSSSocket(int inFileDesc) : fEventContext(inFileDesc, Socket::GetEventThread()) {}
-        virtual ~QTSSSocket() {}
-        
-        //
-        // The only operation this stream supports is the requesting of events.
-        virtual QTSS_Error  RequestEvent(QTSS_EventType inEventMask);
-        
-    private:
-    
-        EventContext fEventContext;
+	QTSSSocket(int inFileDesc) : fEventContext(inFileDesc, Socket::GetEventThread()) {}
+	virtual ~QTSSSocket() {}
+
+	//
+	// The only operation this stream supports is the requesting of events.
+	virtual QTSS_Error  RequestEvent(QTSS_EventType inEventMask);
+
+private:
+
+	EventContext fEventContext;
 };
 
 #endif //__QTSS_SOCKET_H__
