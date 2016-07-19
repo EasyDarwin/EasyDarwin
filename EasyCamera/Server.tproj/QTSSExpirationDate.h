fabd51d48b419851023e8d0daf4761cbebdf9c51
Alex <cai6811376@gmail.com>
2016-07-19 16:25:20 +0800

    Update EasyCamera QTSSExpirationDate

--- EasyCamera/Server.tproj/QTSSExpirationDate.h
+++ EasyCamera/Server.tproj/QTSSExpirationDate.h
@@ -10,7 +10,7 @@
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,21 +18,21 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  */
-/*
-    File:       QTSSExpirationDate.h
+ /*
+	 File:       QTSSExpirationDate.h
 
-    Contains:   Routine that checks to see if software is expired.
+	 Contains:   Routine that checks to see if software is expired.
 
-    Written by: Denis Serenyi
+	 Written by: Denis Serenyi
 
-    Copyright:  © 1998 by Apple Computer, Inc., all rights reserved.
+	 Copyright:  ?1998 by Apple Computer, Inc., all rights reserved.
 
 
-*/
+ */
 
 #ifndef __QTSS_EXPIRATION_DATE_H__
 #define __QTSS_EXPIRATION_DATE_H__
@@ -41,19 +41,19 @@
 
 class QTSSExpirationDate
 {
-    public:
-    
-        //checks current time vs. hard coded time constant.
-        static Bool16   WillSoftwareExpire(){return sIsExpirationEnabled;}
-        static Bool16   IsSoftwareExpired();
-        static void PrintExpirationDate();
-        static void sPrintExpirationDate(char* ioExpireMessage);
-        
-    private:
-    
-        static Bool16 sIsExpirationEnabled;
-        static char*    sExpirationDate;
-        
+public:
+
+	//checks current time vs. hard coded time constant.
+	static Bool16   WillSoftwareExpire() { return sIsExpirationEnabled; }
+	static Bool16   IsSoftwareExpired();
+	static void PrintExpirationDate();
+	static void sPrintExpirationDate(char* ioExpireMessage);
+
+private:
+
+	static Bool16 sIsExpirationEnabled;
+	static char*    sExpirationDate;
+
 };
 
 #endif //__QTSS_EXPIRATION_DATE_H__
