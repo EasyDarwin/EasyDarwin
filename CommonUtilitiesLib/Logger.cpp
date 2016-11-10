a662bd9fdec89b442b641a732f0504d9b727eefa
Alex <cai6811376@gmail.com>
2016-11-10 16:45:44 +0800

    Update Logger thread-safe add std::call_once

--- CommonUtilitiesLib/Logger.cpp
+++ CommonUtilitiesLib/Logger.cpp
@@ -2,15 +2,19 @@
 
 std::shared_ptr<spdlog::logger> Logger::logger_ = nullptr;
 std::vector<spdlog::sink_ptr> Logger::sinks_;
+std::once_flag Logger::flag_;
 
 std::shared_ptr<spdlog::logger>& Logger::Instance()
 {
 	if (!logger_)
 	{
-		sinks_.push_back(std::make_shared<spdlog::sinks::wincolor_stdout_sink_st>());
-		//sinks_.push_back(std::make_shared<spdlog::sinks::daily_file_sink_st>("log", "txt", 23, 59));
-		logger_ = std::make_shared<spdlog::logger>("EasyDarwin", begin(sinks_), end(sinks_));
-		spdlog::register_logger(logger_);
+		std::call_once(flag_, [&]()
+		{
+			sinks_.push_back(std::make_shared<spdlog::sinks::wincolor_stdout_sink_st>());
+			//sinks_.push_back(std::make_shared<spdlog::sinks::daily_file_sink_st>("log", "txt", 23, 59));
+			logger_ = std::make_shared<spdlog::logger>("EasyNVR", begin(sinks_), end(sinks_));
+			spdlog::register_logger(logger_);			
+		});
 	}
 
 	return logger_;
--- CommonUtilitiesLib/Logger.h
+++ CommonUtilitiesLib/Logger.h
@@ -2,6 +2,7 @@
 #define __COMMONUTILITIESLIB_LOGGER__
 
 #include <spdlog/spdlog.h>
+#include <mutex>
 
 using namespace spdlog;
 
@@ -19,7 +20,7 @@ private:
 private:
 	static std::shared_ptr<spdlog::logger> logger_;
 	static std::vector<spdlog::sink_ptr> sinks_;
-
+	static std::once_flag flag_;
 };
 
 
