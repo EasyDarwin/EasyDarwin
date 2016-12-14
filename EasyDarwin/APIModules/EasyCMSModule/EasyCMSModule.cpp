40432059508a7d61e455e3fabe81b22c91ab09cd
Babosa <mysunpany@gmail.com>
2016-12-14 18:05:34 +0800

    EasyCMSModule remove ReflectorSessionCheckTask

--- EasyDarwin/APIModules/EasyCMSModule/EasyCMSModule.cpp
+++ EasyDarwin/APIModules/EasyCMSModule/EasyCMSModule.cpp
@@ -14,7 +14,6 @@
 #include "OSRef.h"
 #include "QTSServerInterface.h"
 #include "EasyCMSSession.h"
-#include "ReflectorSession.h"
 
 // STATIC DATA
 static QTSS_PrefsObject				sServerPrefs = nullptr;
@@ -26,47 +25,6 @@ static QTSS_Error EasyCMSModuleDispatch(QTSS_Role inRole, QTSS_RoleParamPtr inPa
 static QTSS_Error Register_EasyCMSModule(QTSS_Register_Params* inParams);
 static QTSS_Error Initialize_EasyCMSModule(QTSS_Initialize_Params* inParams);
 static QTSS_Error RereadPrefs_EasyCMSModule();
-
-class ReflectorSessionCheckTask : public Task
-{
-public:
-	ReflectorSessionCheckTask() : Task() { this->SetTaskName("ReflectorSessionCheckTask"); this->Signal(Task::kStartEvent); }
-	virtual ~ReflectorSessionCheckTask() {}
-
-private:
-	SInt64 Run() override;
-};
-
-static ReflectorSessionCheckTask* pTask = nullptr;
-
-SInt64 ReflectorSessionCheckTask::Run()
-{
-	auto reflectorSessionMap = QTSServerInterface::GetServer()->GetReflectorSessionMap();
-
-	OSMutexLocker locker(reflectorSessionMap->GetMutex());
-
-	SInt64 sNowTime = OS::Milliseconds();
-	for (OSRefHashTableIter theIter(reflectorSessionMap->GetHashTable()); !theIter.IsDone(); theIter.Next())
-	{
-		OSRef* theRef = theIter.GetCurrent();
-		ReflectorSession* theSession = static_cast<ReflectorSession*>(theRef->GetObject());
-
-		SInt64  sCreateTime = theSession->GetInitTimeMS();
-		if ((theSession->GetNumOutputs() == 0) && (sNowTime - sCreateTime >= 20 * 1000))
-		{
-			QTSS_RoleParams theParams;
-			theParams.easyFreeStreamParams.inStreamName = theSession->GetSourceID()->Ptr;
-			auto numModules = QTSServerInterface::GetNumModulesInRole(QTSSModule::kEasyCMSFreeStreamRole);
-			for (UInt32 currentModule = 0; currentModule < numModules; currentModule++)
-			{
-				qtss_printf("没有客户端观看当前转发媒体\n");
-				auto theModule = QTSServerInterface::GetModule(QTSSModule::kEasyCMSFreeStreamRole, currentModule);
-				(void)theModule->CallDispatch(Easy_CMSFreeStream_Role, &theParams);
-			}
-		}
-	}
-	return 30 * 1000;//30s
-}
 
 //向EasyCMS发送MSG_CS_FREE_STREAM_REQ
 static QTSS_Error FreeStream_EasyCMSModule(Easy_FreeStream_Params* inParams);
@@ -118,10 +76,8 @@ QTSS_Error Initialize_EasyCMSModule(QTSS_Initialize_Params* inParams)
 	sServer = inParams->inServer;
 	sEasyCMSModulePrefs = QTSSModuleUtils::GetModulePrefsObject(inParams->inModule);
 
-	//读取EasyCMSModule配置
 	RereadPrefs_EasyCMSModule();
 
-	pTask = new ReflectorSessionCheckTask();
 	return QTSS_NoErr;
 }
 
