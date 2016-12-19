d653c28df590fe03fd0f2b73ff32acdef10ce52a
Babosa <babosa@easydarwin.org>
2016-12-19 21:19:05 +0800

    /*
    	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
    	Github: https://github.com/EasyDarwin
    	WEChat: EasyDarwin
    	Website: http://www.EasyDarwin.org
    */

--- EasyCMS/APIModules/EasyRedisModule/EasyRedisModule.cpp
+++ EasyCMS/APIModules/EasyRedisModule/EasyRedisModule.cpp
@@ -1,3 +1,9 @@
+/*
+	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.EasyDarwin.org
+*/
 #include "EasyRedisModule.h"
 
 #include "OSHeaders.h"
@@ -390,7 +396,7 @@ QTSS_Error RedisGetAssociatedDarwin(QTSS_GetAssociatedDarwin_Params* inParams)
 		}
 		string easydarwin("EasyDarwin:");
 		easydarwin += replyHmget->element[0]->str;
-		freeReplyObject(replyHmget);		
+		freeReplyObject(replyHmget);
 
 		strTemp = Format("hmget %s", easydarwin + " IP HTTP RTSP");
 
--- EasyCMS/APIModules/EasyRedisModule/EasyRedisModule.h
+++ EasyCMS/APIModules/EasyRedisModule/EasyRedisModule.h
@@ -1,3 +1,9 @@
+/*
+	Copyleft (c) 2012-2016 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.EasyDarwin.org
+*/
 #ifndef __EASYREDISMODULE__H__
 #define __EASYREDISMODULE__H__
 
