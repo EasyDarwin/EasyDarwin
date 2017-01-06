f268444b674abb37de7418ed5e3ac0470399cc8f
Alex <cai6811376@gmail.com>
2017-01-06 14:18:57 +0800

    [EasyCMS]remove bitmap linux

--- EasyCMS/Server.tproj/HTTPSession.cpp
+++ EasyCMS/Server.tproj/HTTPSession.cpp
@@ -29,30 +29,6 @@
 
 #if __solaris__ || __linux__ || __sgi__	|| __hpux__
 #include <crypt.h>
-
-typedef struct BITMAPFILEHEADER
-{
-	u_int16_t bfType;
-	u_int32_t bfSize;
-	u_int16_t bfReserved1;
-	u_int16_t bfReserved2;
-	u_int32_t bfOffBits;
-} BITMAPFILEHEADER;
-
-typedef struct BITMAPINFOHEADER
-{
-	u_int32_t biSize;
-	u_int32_t biWidth;
-	u_int32_t biHeight;
-	u_int16_t biPlanes;
-	u_int16_t biBitCount;
-	u_int32_t biCompression;
-	u_int32_t biSizeImage;
-	u_int32_t biXPelsPerMeter;
-	u_int32_t biYPelsPerMeter;
-	u_int32_t biClrUsed;
-	u_int32_t biClrImportant;
-} BITMAPINFODEADER;
 #endif
 
 using namespace std;
