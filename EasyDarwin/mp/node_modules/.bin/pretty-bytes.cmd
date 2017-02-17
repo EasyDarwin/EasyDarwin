6001333deef5e94783c5b8b50d7a9ab92cd3f7a1
Babosa <babosa@easydarwin.org>
2017-02-17 16:50:46 +0800

    EasyDarwin remove mp

--- EasyDarwin/mp/node_modules/.bin/pretty-bytes.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\pretty-bytes\cli.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\pretty-bytes\cli.js" %*
-)
--- EasyDarwin/mp/node_modules/grunt/node_modules/.bin/grunt.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\grunt-cli\bin\grunt" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\grunt-cli\bin\grunt" %*
-)
